1e535f3a43aab3426e4050423fd775f0522c0044
Jonathan Olson <olsonsjc@gmail.com>
2013-01-06 20:59:43 -0700

    adding update to rectangle

--- src/js/scene/nodes/Rectangle.js
+++ src/js/scene/nodes/Rectangle.js
@@ -8,21 +8,31 @@ phet.scene.nodes = phet.scene.nodes || {};
     phet.scene.nodes.Rectangle = function ( args ) {
         phet.scene.Node.call( this );
         
-        this.args = args;
-        this.x = args.x;
-        this.y = args.y;
-        this.width = args.width;
-        this.height = args.height;
+        this.args = {};
         
-        // TODO: handle strokes and fills in a better way
-        this.stroke = args.stroke;
-        this.fill = args.fill;
+        this.update( args );
     };
 
     var Rectangle = phet.scene.nodes.Rectangle;
+    
+    Rectangle.parameters = [ 'x', 'y', 'width', 'height', 'stroke', 'fill' ];
 
     Rectangle.prototype = Object.create( phet.scene.Node.prototype );
     Rectangle.prototype.constructor = Rectangle;
+    
+    Rectangle.prototype.update = function( args ) {
+        // copy values into args
+        for( var key in args ) {
+            this.args[key] = args[key];
+        }
+        
+        this.x = args.x === undefined ? this.x : args.x;
+        this.y = args.y === undefined ? this.y : args.y;
+        this.width = args.width === undefined ? this.width : args.width;
+        this.height = args.height === undefined ? this.height : args.height;
+        this.stroke = args.stroke === undefined ? this.stroke : args.stroke;
+        this.fill = args.fill === undefined ? this.fill : args.fill;
+    };
     
     Rectangle.prototype.renderSelf = function ( state ) {
         if( state.isCanvasState() ) {
