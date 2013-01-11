85904e9f72cf987d983afa5124b6e56d1743e632
Jonathan Olson <olsonsjc@gmail.com>
2013-01-10 20:52:59 -0800

    added echos to Windows batch build file

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@
 setlocal enableextensions enabledelayedexpansion
 
 :: rebuild concatenated.js from the file list by appending
+echo Building concatenated.js
 del concatenated.js
 for /f %%k in (build/file-list.txt) DO (
 	:: call out to the inside of our loop. it's a separate file because of how the for loop is expanded. see http://www.robvanderwoude.com/for.php (section 7)
@@ -12,7 +13,9 @@ for /f %%k in (build/file-list.txt) DO (
 )
 
 :: build phet-scene.js
+echo Building phet-scene.js
 java -jar bin/closure-compiler.jar --compilation_level WHITESPACE_ONLY --formatting PRETTY_PRINT --js concatenated.js --js_output_file phet-scene.js
 
 :: build phet-scene-min.js
+echo Building phet-scene-min.js
 java -jar bin/closure-compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js concatenated.js --js_output_file phet-scene-min.js
