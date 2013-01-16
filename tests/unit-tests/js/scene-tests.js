a76472fd06e745d1153421f7286205134d4dd022
Jonathan Olson <olsonsjc@gmail.com>
2013-01-16 12:36:24 -0800

    Adding Object.defineProperty test

--- tests/unit-tests/js/scene-tests.js
+++ tests/unit-tests/js/scene-tests.js
@@ -206,6 +206,24 @@
     } );
     
     /*---------------------------------------------------------------------------*
+    * Miscellaneous HTML / JS
+    *----------------------------------------------------------------------------*/        
+    
+    module( 'Miscellaneous' );
+    
+    test( 'ES5 Object.defineProperty get/set', function() {
+        var ob = { _key: 5 };
+        Object.defineProperty( ob, 'key', {
+            enumerable: true,
+            configurable: true,
+            get: function() { return this._key; },
+            set: function( val ) { this._key = val; }
+        } );
+        ob.key += 1;
+        equal( ob._key, 6, 'incremented object value' );
+    } );
+    
+    /*---------------------------------------------------------------------------*
     * WebGL
     *----------------------------------------------------------------------------*/        
     
