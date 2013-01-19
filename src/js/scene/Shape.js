7378bd45062a5bf4abadbe7ceed8d207cb2d28d5
Jonathan Olson <olsonsjc@gmail.com>
2013-01-18 21:38:54 -0800

    Shape piece redesign

--- src/js/scene/Shape.js
+++ src/js/scene/Shape.js
@@ -3,6 +3,8 @@
 /**
  * Shape handling
  *
+ * TODO: add nonzero / evenodd support when browsers support it
+ *
  * @author Jonathan Olson
  */
 
@@ -24,34 +26,7 @@ phet.scene = phet.scene || {};
             this.addPiece( new Piece.Close() );
         }
     }
-    
     var Shape = phet.scene.Shape;
-    
-    // winding rule constants
-    Shape.FILL_NONZERO = 0;
-    Shape.FILL_EVENODD = 1;
-    
-    // piece types
-    Shape.PIECE_MOVE = 0;
-    Shape.PIECE_LINE = 1;
-    Shape.PIECE_CLOSE = 2;
-    Shape.PIECE_QUADRATIC = 3;
-    Shape.PIECE_CUBIC = 4;
-    Shape.PIECE_ELLIPSE = 5;
-    Shape.PIECE_ARC_TO = 6;
-    Shape.PIECE_ARC = 7;
-    Shape.PIECE_RECT = 8;
-    // TODO: simplify arc/ellipse/cubic/quadratic parts?
-    
-    // line caps
-    Shape.CAP_BUTT = 0;
-    Shape.CAP_ROUND = 1;
-    Shape.CAP_SQUARE = 2;
-    
-    // line joins
-    Shape.JOIN_ROUND = 0;
-    Shape.JOIN_BEVEL = 1;
-    Shape.JOIN_MITER = 2;
     
     Shape.LineStyles = function( args ) {
         if( args === undefined ) {
@@ -59,21 +34,17 @@ phet.scene = phet.scene || {};
         }
         return {
             lineWidth: args.lineWidth !== undefined ? args.lineWidth : 1,
-            lineCap: args.lineCap !== undefined ? args.lineCap : Shape.CAP_BUTT,
-            lineJoin: args.lineJoin !== undefined ? args.lineJoin : Shape.JOIN_MITER,
+            lineCap: args.lineCap !== undefined ? args.lineCap : 'butt', // butt, round, square
+            lineJoin: args.lineJoin !== undefined ? args.lineJoin : 'miter', // miter, round, bevel
             miterLimit: args.miterLimit !== undefined ? args.miterLimit : 10 // see https://svgwg.org/svg2-draft/painting.html for miterLimit computations
         };
     }
-    
-    // default canvas styles according to spec
-    Shape.DEFAULT_STYLES = new Shape.LineStyles();
+    var LineStyles = Shape.LineStyles;
     
     // Pieces should be considered immutable
-    Shape.Piece = function( type, points ) {
-        this.type = type;
+    Shape.Piece = function( points ) {
         this.points = points;
     };
-    
     var Piece = Shape.Piece;
     
     Piece.prototype = {
@@ -94,14 +65,14 @@ phet.scene = phet.scene || {};
     
     // TODO: make a canonical CLOSE?
     Piece.Close = function() {
-        Piece.call( this, Shape.PIECE_CLOSE, [] );
+        Piece.call( this, [] );
     };
     Piece.Close.prototype = Object.create( Piece.prototype );
     Piece.Close.prototype.constructor = Piece.Close;
     Piece.Close.prototype.writeToContext = function( context ) { context.closePath(); };
     
     Piece.MoveTo = function( point ) {
-        Piece.call( this, Shape.PIECE_MOVE, [ point ] );
+        Piece.call( this, [ point ] );
         this.point = point;
     };
     Piece.MoveTo.prototype = Object.create( Piece.prototype );
@@ -109,7 +80,7 @@ phet.scene = phet.scene || {};
     Piece.MoveTo.prototype.writeToContext = function( context ) { context.moveTo( this.point.x, this.point.y ); };
     
     Piece.LineTo = function( point ) {
-        Piece.call( this, Shape.PIECE_LINE, [ point ] );
+        Piece.call( this, [ point ] );
         this.point = point;
     };
     Piece.LineTo.prototype = Object.create( Piece.prototype );
@@ -118,7 +89,7 @@ phet.scene = phet.scene || {};
     
     // TODO: transforms for rect are different!!!
     Piece.Rect = function( upperLeft, lowerRight ) {
-        Piece.call( this, Shape.PIECE_RECT, [ upperLeft, lowerRight ] );
+        Piece.call( this, [ upperLeft, lowerRight ] );
         this.upperLeft = upperLeft;
         this.lowerRight = lowerRight;
         this.x = this.upperLeft.x;
@@ -135,7 +106,6 @@ phet.scene = phet.scene || {};
         this.subpaths = [];
         this.bounds = phet.math.Bounds2.NOTHING;
     };
-    
     var ShapeState = Shape.ShapeState;
     
     ShapeState.prototype = {
@@ -216,26 +186,6 @@ phet.scene = phet.scene || {};
                     console.log( piece );
                 }
                 piece.writeToContext( context );
-                // switch( piece.type ) {
-                //     case Shape.PIECE_MOVE: context.moveTo( piece.points[0].x, piece.points[0].y ); break;
-                //     case Shape.PIECE_LINE: context.lineTo( piece.points[0].x, piece.points[0].y ); break;
-                //     case Shape.PIECE_CLOSE: context.closePath(); break;
-                //     case Shape.PIECE_QUADRATIC: context.quadraticCurveTo( piece.points[0].x, piece.points[0].y, piece.points[1].x, piece.points[1].y ); break;
-                //     case Shape.PIECE_CUBIC: context.bezierCurveTo( piece.points[0].x, piece.points[0].y, piece.points[1].x, piece.points[1].y, piece.points[2].x, piece.points[2].y ); break;
-                //     case Shape.PIECE_ELLIPSE: context.ellipse( piece.points[0].x, piece.points[0].y, piece.args.radiusX, piece.args.radiusY, piece.args.rotation, piece.args.startAngle, piece.args.endAngle, piece.args.anticlockwise ); break;
-                //     case Shape.PIECE_ARC_TO:
-                //         // TODO: consider splitting ARC_TO into two separate piece types
-                //         if( piece.args.radius !== undefined ) {
-                //             context.ellipse( piece.points[0].x, piece.points[0].y, piece.points[1].x, piece.points[1].y, piece.args.radius );
-                //         } else {
-                //             context.ellipse( piece.points[0].x, piece.points[0].y, piece.points[1].x, piece.points[1].y, piece.args.radiusX, piece.args.radiusY, piece.args.rotation );
-                //         }
-                //         break;
-                //     case Shape.PIECE_ARC: context.arc( piece.points[0].x, piece.points[0].y, piece.args.radius, piece.args.startAngle, piece.args.endAngle, piece.args.anticlockwise ); break;
-                //     case Shape.PIECE_RECT: context.rect( piece.points[0].x, piece.points[0].y, piece.points[1].x - piece.points[0].x, piece.points[1].y - piece.points[0].y ); break;
-                //     default:
-                //         throw new Error( 'writeToContext unimplemented for piece type' + piece.type );
-                // }
             } );
         },
         
@@ -243,107 +193,19 @@ phet.scene = phet.scene || {};
         transformed: function( matrix ) {
             // TODO: reimplement transformations
             return this;
-            /*
-            return new Shape( _.map( this.pieces, function( piece ) {
-                var transformedPoints = _.map( piece.points, function( point ) { return matrix.timesVector2( point ); } );
-                var args = piece.args;
-                switch( piece.type ) {
-                    case Shape.PIECE_MOVE:
-                    case Shape.PIECE_LINE:
-                    case Shape.PIECE_CLOSE:
-                    case Shape.PIECE_QUADRATIC:
-                    case Shape.PIECE_CUBIC:
-                    case Shape.PIECE_RECT:
-                        return new Piece( piece.type, transformedPoints, args );
-                    case Shape.PIECE_ELLIPSE:
-                        return new Piece( Shape.PIECE_ELLIPSE, transformedPoints, {
-                            // TODO: more convenient way of handling one-param modifications, like args.with( { rotation: <blah> } )
-                            radiusX: args.radiusX,
-                            radiusY: args.radiusY,
-                            rotation: args.rotation + matrix.rotation(),
-                            startAngle: args.startAngle,
-                            endAngle: args.endAngle,
-                            anticlockwise: args.anticlockwise
-                        } );
-                    case Shape.PIECE_ARC_TO:
-                        if( args.radius !== undefined ) {
-                            return new Piece( Shape.PIECE_ARC_TO, transformedPoints, args );
-                        } else {
-                            return new Piece( Shape.PIECE_ARC_TO, transformedPoints, {
-                                radiusX: args.radiusX,
-                                radiusY: args.radiusY,
-                                rotation: args.rotation + matrix.rotation()
-                            } );
-                        }
-                    case Shape.PIECE_ARC:
-                        var extraRotation = matrix.rotation();
-                        return new Piece( Shape.PIECE_ARC, transformedPoints, {
-                            radius: args.radius,
-                            startAngle: args.startAngle + extraRotation,
-                            endAngle: args.endAngle + extraRotation,
-                            anticlockwise: args.anticlockwise
-                        } );
-                    default:
-                        throw new Error( 'transformed unimplemented for piece type' + piece.type );
-                }
-            } ));
-            */
         },
         
-        // returns the bounds. 
+        // returns the bounds. if lineDrawingStyles exists, include the stroke in the bounds
         computeBounds: function( lineDrawingStyles ) {
-            
-            // TODO: consider null => no stroke?
-            if( lineDrawingStyles === undefined ) {
-                lineDrawingStyles = Shape.DEFAULT_STYLES;
-            }
             
             var shapeState = new ShapeState();
             
+            // TODO: include stroke information somehow
             _.each( this.pieces, function( piece ) {
                 piece.applyPiece( shapeState );
             } );
             
             return shapeState.bounds;
-            
-            /*
-            var bounds = phet.math.Bounds2.NOTHING;
-            
-            // TODO: improve bounds constraints (not as tight as possible yet)
-            _.each( this.pieces, function( piece ) {
-                // set bounding box to contain all control points
-                _.each( piece.points, function( point ) {
-                    bounds = bounds.withPoint( point );
-                } );
-                
-                switch( piece.type ) {
-                    case Shape.PIECE_MOVE:
-                    case Shape.PIECE_LINE:
-                    case Shape.PIECE_CLOSE:
-                    case Shape.PIECE_QUADRATIC:
-                    case Shape.PIECE_CUBIC:
-                    case Shape.PIECE_RECT:
-                        break; // already handled by the points. TODO implement tighter on curves due to control points not being bounds
-                    case Shape.PIECE_ELLIPSE:
-                        var x = piece.points[0].x;
-                        var y = piece.points[0].y;
-                        var maxRadius = Math.max( piece.args.radiusX, piece.args.radiusY );
-                        bounds = bounds.union( new phet.math.Bounds2( x - maxRadius, y - maxRadius, x + maxRadius, y - maxRadius ) );
-                        break;
-                    case Shape.PIECE_ARC_TO:
-                        throw new Error( 'arcTo computeBounds not implemented yet' );
-                    case Shape.PIECE_ARC:
-                        var x = piece.points[0].x;
-                        var y = piece.points[0].y;
-                        var radius = piece.args.radius;
-                        bounds = bounds.union( new phet.math.Bounds2( x - radius, y - radius, x + radius, y - radius ) );
-                        break;
-                    default:
-                        throw new Error( 'computeBounds unimplemented for piece type: ' + piece.type );
-                }
-            } );
-            return bounds;
-            */
         },
         
         traced: function( lineDrawingStyles ) {
