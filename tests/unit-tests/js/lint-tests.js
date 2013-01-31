baa17397b388a0a5b64e1107b840d077ee84df98
Jonathan Olson <olsonsjc@gmail.com>
2013-01-30 22:53:34 -0800

    Adding JSHint / QHint for linting + unit tests

---
+++ tests/unit-tests/js/lint-tests.js
@@ -0,0 +1,31 @@
+
+
+(function(){
+    "use strict";
+    
+    module( 'JSHint' );
+    
+    // adjust with options from http://www.jshint.com/docs/
+    var options = {};
+    var globals = {};
+    
+    qHint.sendRequest( '../../build/file-list.txt', function( req ) {
+        console.log( req );
+        
+        test( 'File list OK', function() {
+            equal( req.status, 200 );
+        } );
+        
+        var filenames = req.responseText.split( /\r?\n/ );
+        
+        _.each( filenames, function( filename ) {
+            if( filename ) {
+                qHint( filename, '../../' + filename, options, globals );
+            }
+        } );
+    } );
+    
+})();
+
+
+
