3ef2b75781f9ec1159bf9a39cb8436bd3a682a0a
Jonathan Olson <olsonsjc@gmail.com>
2013-02-13 17:07:37 -0800

    TrailPointer enhancements

--- src/js/scene/TrailPointer.js
+++ src/js/scene/TrailPointer.js
@@ -5,18 +5,30 @@ var scenery = scenery || {};
 (function(){
   "use strict";
   
-  scenery.TrailPointer = function( trail, side ) {
+  /*
+   * isBefore: whether this points to before the node (and its children) have been rendered, or after
+   */
+  scenery.TrailPointer = function( trail, isBefore ) {
     this.trail = trail;
-    this.side = side;
+    
+    this.isBefore = isBefore;
+    this.isAfter = !isBefore;
   };
   var TrailPointer = scenery.TrailPointer;
   
-  TrailPointer.BEFORE = 1;
-  TrailPointer.AFTER = 2;
-  
   TrailPointer.prototype = {
-    constructor: TrailPointer
-
+    constructor: TrailPointer,
+    
+    // return the equivalent pointer that swaps before and after (may return null if it doesn't exist)
+    getSwappedPointer: function() {
+      var newTrail = this.isBefore ? trail.previous() : trail.next();
+      
+      if ( newTrail === null ) {
+        return null;
+      } else {
+        return new TrailPointer( newTrail, !this.isBefore );
+      }
+    }
   };
   
 })();
