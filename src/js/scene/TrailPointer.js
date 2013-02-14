ef4849c13e29c0086e681c8cadb5430de2f7c391
Jonathan Olson <olsonsjc@gmail.com>
2013-02-13 22:48:17 -0800

    Progress with that monstrosity of a function that is TrailPointer.eachBetween. Will take simpler route, but keeping for posterity (or if it can be recovered)

--- src/js/scene/TrailPointer.js
+++ src/js/scene/TrailPointer.js
@@ -31,6 +31,15 @@ var scenery = scenery || {};
   TrailPointer.prototype = {
     constructor: TrailPointer,
     
+    copy: function() {
+      return new TrailPointer( this.trail.copy(), this.isBefore );
+    },
+    
+    toggleBeforeAfter: function() {
+      this.isBefore = !this.isBefore;
+      this.isAfter = !this.isAfter;
+    },
+    
     // return the equivalent pointer that swaps before and after (may return null if it doesn't exist)
     getRenderSwappedPointer: function() {
       var newTrail = this.isBefore ? this.trail.previous() : this.trail.next();
@@ -111,14 +120,169 @@ var scenery = scenery || {};
       return this.compareNested( other ) === 0;
     },
     
+    // moves this pointer forwards one step in the nested order
+    nestedForwards: function() {
+      if ( this.isBefore ) {
+        if ( this.trail.lastNode().children.length > 0 ) {
+          // stay as before, just walk to the first child
+          this.trail.addDescendant( this.trail.lastNode().children[0] );
+        } else {
+          // stay on the same node, but switch to after
+          this.toggleBeforeAfter();
+        }
+      } else {
+        throw new Error( 'unimplemented! finish Trail changes first' );
+      }
+    },
+    
+    // moves this pointer backwards one step in the nested order
+    nestedBackwards: function() {
+      
+    },
+    
     // treats the pointer as render-ordered
     eachNodeBetween: function( other, callback ) {
       
     },
     
-    // treats the pointer as nesting-ordered
+    // TODO: try next/previous handling as an easier case!
+    // TODO: get rid of this monstrosity!
+    // treats the pointer as nesting-ordered. assumes that they are properly ordered
     eachBetween: function( other, callbacks, excludeEndpoints ) {
+      // make sure this pointer is before the other
+      phet.assert( this.compareNested( other ) === -1, 'TrailPointer.eachBetween pointers out of order, possibly in both meanings of the phrase!' );
+      phet.assert( this.trail[0] === other.trail[0], 'TrailPointer.eachBetween takes pointers with the same root' );
       
+      // for speed
+      this.trail.reindex();
+      other.trail.reindex();
+      
+      // our state
+      var indexStack = [];
+      var nodeStack = [ this.trail[0] ];
+      var enterAfterNext = false;
+      var exitAfterNext = false;
+      var entered = false;
+      var exited = false;
+      var match = 0;
+      
+      function enter( node ) {
+        if ( !exited ) {
+          var depth = indexStack.length - 1;
+          
+          // don't fully match if other.isAfter
+          if ( depth === match && ( other.isBefore || match + 1 !== other.trail.indices.length ) ) {
+            if ( other.trail.indices[match] === indexStack[depth] ) {
+              match++;
+              
+              // once we have matched
+              if ( match === other.trail.indices.length ) {
+                exited = true;
+                if ( !excludeEndpoints ) {
+                  callbacks.enter( node );
+                }
+                return;
+              }
+            }
+          }
+        }
+        
+        if ( entered && !exited ) {
+          callbacks.enter( node );
+        }
+        
+        // technically dead code, but left here in case we change things in the future
+        // if ( !entered && enterAfterNext ) {
+        //   entered = true;
+        // }
+        if ( !exited && exitAfterNext ) {
+          exited = true;
+        }
+      }
+      
+      function exit( node ) {
+        if ( !exited && other.isAfter ) {
+          var depth = indexStack.length - 1;
+          if ( depth === match && match + 1 === other.trail.indices.length ) {
+            if ( other.trail.indices[match] === indexStack[depth] ) {
+              match++;
+              
+              // once we have matched
+              exited = true;
+              if ( !excludeEndpoints ) {
+                callbacks.exit( node );
+              }
+              return;
+            }
+          }
+        }
+        
+        if ( entered && !exited ) {
+          callbacks.exit( node );
+        }
+        
+        if ( !entered && enterAfterNext ) {
+          entered = true;
+        }
+        if ( !exited && exitAfterNext ) {
+          exited = true;
+        }
+      }
+      
+      function push( index ) {
+        var node = topNode().children[index];
+        indexStack.push( index );
+        nodeStack.push( node );
+        enter( node );
+      }
+      
+      function pop() {
+        var index = indexStack.pop();
+        var node = nodeStack.pop();
+        exit( node );
+        return index;
+      }
+      
+      function topNode() {
+        return nodeStack[nodeStack.length - 1];
+      }
+      
+      function next( childrenOk ) {
+        if ( childrenOk && topNode().children.length > 0 ) {
+          // if there are children, walk to the first one
+          push( 0 );
+        } else {
+          while ( indexStack.length > 0 ) {
+            var index = pop();
+            if ( topNode().children.length > index + 1 ) {
+              push( index + 1 );
+              break;
+            }
+          }
+        }
+      }
+      
+      // walk the starting trail
+      for ( var i = 1; i < this.trail.length; i++ ) {
+        push( this.trail.indices( i - 1 ) );
+      }
+      
+      if ( this.isBefore ) {
+        entered = true;
+        if ( !excludeEndpoints ) {
+          callbacks.enter( topNode() );
+        }
+      } else {
+        if ( excludeEndpoints ) {
+          enterAfterNext = true;
+        } else {
+          entered = true;
+        }
+      }
+      next( this.isBefore );
+      while ( !exited ) {
+        next( true );
+      }
     }
   };
   
