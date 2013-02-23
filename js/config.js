c56584f995aab1e71f8c136d47330a06cccc0a5a
Jonathan Olson <olsonsjc@gmail.com>
2013-02-23 01:05:18 -0800

    More attemps to get around runtime error

--- js/config.js
+++ js/config.js
@@ -1,12 +1,23 @@
 
 require.config( {
   deps: [ 'main' ],
-
+  
   paths: {
+    underscore: '../contrib/lodash.min-1.0.0-rc.3',
+    jquery: '../contrib/jquery-1.8.3.min',
     SCENERY: '.',
     DOT: '../common/dot/js',
     ASSERT: '../common/assert/js'
   },
-
+  
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    jquery: {
+      exports: '$'
+    }
+  },
+  
   urlArgs: new Date().getTime() // add cache buster query string to make browser refresh actually reload everything
 } );
