f8592e611aa4731ae9fd8b5b1e6b366c8ec4277a
Jonathan Olson <olsonsjc@gmail.com>
2013-02-22 20:55:25 -0800

    Moving utils into Util

--- js/Util.js
+++ js/Util.js
@@ -19,6 +19,47 @@ define( function( require ) {
   var debugChromeBoundsScanning = false;
   
   var Util = {
+    // Object.create polyfill
+    objectCreate: Object.create || function ( o ) {
+      if ( arguments.length > 1 ) {
+        throw new Error( 'Object.create implementation only accepts the first parameter.' );
+      }
+      function F() {}
+
+      F.prototype = o;
+      return new F();
+    },
+    
+    /*---------------------------------------------------------------------------*
+     * window.requestAnimationFrame polyfill, by Erik Moller (http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating)
+     * referenced by initial Paul Irish article at http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+     *----------------------------------------------------------------------------*/
+    polyfillRequestAnimationFrame: function() {
+      var lastTime = 0;
+      var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
+      for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x ) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        phet.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+      }
+     
+      if ( !window.requestAnimationFrame ) {
+        window.requestAnimationFrame = function(callback) {
+          var currTime = new Date().getTime();
+          var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+          var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+            timeToCall);
+          lastTime = currTime + timeToCall;
+          return id;
+        };
+      }
+     
+      if ( !phet.cancelAnimationFrame ) {
+        phet.cancelAnimationFrame = function(id) {
+          clearTimeout(id);
+        };
+      }
+    },
+    
     backingStorePixelRatio: function( context ) {
       return context.webkitBackingStorePixelRatio ||
              context.mozBackingStorePixelRatio ||
