1b22df12fcb4901eb6c26dae16442bb36c76626c
Jonathan Olson <olsonsjc@gmail.com>
2013-03-02 14:23:22 -0800

    Adding LayerBoundary

---
+++ js/layers/LayerBoundary.js
@@ -0,0 +1,44 @@
+// Copyright 2002-2012, University of Colorado
+
+/**
+ * A conceptual boundary between layers, where it is optional to have information about a previous or next layer.
+ *
+ * @author Jonathan Olson <olsonsjc@gmail.com>
+ */
+
+define( function( require ) {
+  "use strict";
+  
+  var assert = require( 'ASSERT/assert' )( 'scenery' );
+  
+  var scenery = require( 'SCENERY/scenery' );
+  
+  scenery.LayerBoundary = function() {
+    // layer types before and after the boundary. null indicates the lack of information (first or last layer)
+    this.previousLayerType = null;
+    this.nextLayerType = null;
+    
+    // trails to the closest nodes with hasSelf() === true before and after the boundary
+    this.previousSelfTrail = null;
+    this.nextSelfTrail = null;
+    
+    // the TrailPointers where the previous layer was ended and the next layer begins (the trail, and enter() or exit())
+    this.previousEndPointer = null;
+    this.nextStartPointer = null;
+  };
+  var LayerBoundary = scenery.LayerBoundary;
+  
+  LayerBoundary.prototype = {
+    constructor: LayerBoundary,
+    
+    hasPrevious: function() {
+      return !!this.previousLayerType;
+    },
+    
+    hasNext: function() {
+      return !!this.nextLayerType;
+    }
+  };
+  
+  return LayerBoundary;
+} );
