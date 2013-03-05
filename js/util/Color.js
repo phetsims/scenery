864aa05bbac0040f5a565e4a898a8cfa9e3143b7
Jonathan Olson <olsonsjc@gmail.com>
2013-03-05 14:24:42 -0800

    Partial progress on Color support for gradients

--- js/util/Color.js
+++ js/util/Color.js
@@ -7,6 +7,8 @@
  *
  * TODO: support JS/CSS colors, color parsing, etc. Investigate Canvg's dependency for use?
  *
+ * See http://www.w3.org/TR/css3-color/
+ *
  * @author Jonathan Olson <olsonsjc@gmail.com>
  */
 
@@ -16,27 +18,314 @@ define( function( require ) {
   var assert = require( 'ASSERT/assert' )( 'scenery' );
   
   var scenery = require( 'SCENERY/scenery' );
+  
+  var clamp = require( 'DOT/Util' ).clamp;
+  
+  var rgbNumber = '(-?\\d{1,3}%?)';
+  var aNumber = '(\\d|\\d\\.\\d+)';
+  var shortHexFormat = /^#(\w{1})(\w{1})(\w{1})$/;
+  var fullHexFormat = /^#(\w{2})(\w{2})(\w{2})$/;
+  var rgbFormat = new RegExp( '^rgb\\(' + rgbNumber + ',' + rgbNumber + ',' + rgbNumber + '\\)$' );
+  var rgbaFormat = new RegExp( '^rgba\\(' + rgbNumber + ',' + rgbNumber + ',' + rgbNumber + ',' + aNumber + '\\)$' );
+  var hslFormat = new RegExp( '^hsl\\(' + rgbNumber + ',' + rgbNumber + ',' + rgbNumber + '\\)$' );
+  var hslaFormat = new RegExp( '^hsla\\(' + rgbNumber + ',' + rgbNumber + ',' + rgbNumber + ',' + aNumber + '\\)$' );
 
   // integer-based constructor
   scenery.Color = function( r, g, b, a ) {
+    
+    if ( typeof r === 'string' ) {
+      var str = r.replace( / /g, '' ).toLowerCase();
+      var match;
+      
+      // short hex format, e.g. #f0e
+      match = shortHexFormat.exec( str );
+      if ( match ) {
+        this.setRedInt( parseInt( match[1] + match[1], 16 ) );
+        this.setGreenInt( parseInt( match[2] + match[2], 16 ) );
+        this.setBlueInt( parseInt( match[3] + match[3], 16 ) );
+        this.setAlphaInt( 255 );
+      } else {
+        // full hex format, e.g. #f0ade5
+        match = fullHexFormat.exec( str );
+        if ( match ) {
+          this.setRedInt( parseInt( match[1], 16 ) );
+          this.setGreenInt( parseInt( match[2], 16 ) );
+          this.setBlueInt( parseInt( match[3], 16 ) );
+          this.setAlphaInt( 255 );
+        }
+      }
+    } else {
+      // alpha
+      this.a = a === undefined ? 255 : a;
 
-    // alpha
-    this.a = a === undefined ? 255 : a;
-
-    // bitwise handling if 3 elements aren't defined
-    if ( g === undefined || b === undefined ) {
-      this.r = ( r >> 16 ) && 0xFF;
-      this.g = ( r >> 8 ) && 0xFF;
-      this.b = ( r >> 0 ) && 0xFF;
-    }
-    else {
-      // otherwise, copy them over
-      this.r = r;
-      this.g = g;
-      this.b = b;
+      // bitwise handling if 3 elements aren't defined
+      if ( g === undefined || b === undefined ) {
+        this.r = ( r >> 16 ) && 0xFF;
+        this.g = ( r >> 8 ) && 0xFF;
+        this.b = ( r >> 0 ) && 0xFF;
+      }
+      else {
+        // otherwise, copy them over
+        this.r = r;
+        this.g = g;
+        this.b = b;
+      }
     }
   };
   var Color = scenery.Color;
+  
+  Color.prototype = {
+    constructor: Color,
+    
+    /*---------------------------------------------------------------------------*
+    * Integers clamped to the range 0-255
+    *----------------------------------------------------------------------------*/
+    
+    setRedInt: function( value ) {
+      value = Math.round( clamp( value, 0, 255 ) );
+      this.r = value;
+      this.rRatio = value / 255;
+    },
+    
+    setGreenInt: function( value ) {
+      value = Math.round( clamp( value, 0, 255 ) );
+      this.g = value;
+      this.gRatio = value / 255;
+    },
+    
+    setBlueInt: function( value ) {
+      value = Math.round( clamp( value, 0, 255 ) );
+      this.b = value;
+      this.bRatio = value / 255;
+    },
+    
+    setAlphaInt: function( value ) {
+      value = Math.round( clamp( value, 0, 255 ) );
+      this.a = value;
+      this.aRatio = value / 255;
+    },
+    
+    /*---------------------------------------------------------------------------*
+    * Numbers clamped to the range 0-1
+    *----------------------------------------------------------------------------*/
+    
+    setRedRatio: function( value ) {
+      value = clamp( value, 0, 1 );
+      this.r = Math.round( value * 255 );
+      this.rRatio = value;
+    },
+    
+    setGreenRatio: function( value ) {
+      value = clamp( value, 0, 1 );
+      this.g = Math.round( value * 255 );
+      this.gRatio = value;
+    },
+    
+    setBlueRatio: function( value ) {
+      value = clamp( value, 0, 1 );
+      this.b = Math.round( value * 255 );
+      this.bRatio = value;
+    },
+    
+    setAlphaRatio: function( value ) {
+      value = clamp( value, 0, 1 );
+      this.a = Math.round( value * 255 );
+      this.aRatio = value;
+    }
+    
+  };
+  
+  // support: #aaaaaa, #aaa, rgb(...), rgba(...), hsl(...). numbers can be 0%-100%. clip 0-255. 0%-100%. Integer or percentage. Trim whitespace
+  // it is defined as sRGB
+  // hsl( ... ), hsla( ... )
+  
+  /*
+  HSL parse algorithm:
+      HOW TO RETURN hsl.to.rgb(h, s, l):
+       SELECT:
+    l<=0.5: PUT l*(s+1) IN m2
+    ELSE: PUT l+s-l*s IN m2
+       PUT l*2-m2 IN m1
+       PUT hue.to.rgb(m1, m2, h+1/3) IN r
+       PUT hue.to.rgb(m1, m2, h    ) IN g
+       PUT hue.to.rgb(m1, m2, h-1/3) IN b
+       RETURN (r, g, b)
+
+    HOW TO RETURN hue.to.rgb(m1, m2, h):
+       IF h<0: PUT h+1 IN h
+       IF h>1: PUT h-1 IN h
+       IF h*6<1: RETURN m1+(m2-m1)*h*6
+       IF h*2<1: RETURN m2
+       IF h*3<2: RETURN m1+(m2-m1)*(2/3-h)*6
+       RETURN m1
+  */
+  
+  var basicColorKeywords = {
+    aqua: '00ffff',
+    black: '000000',
+    blue: '0000ff',
+    fuchsia: 'ff00ff',
+    gray: '808080',
+    green: '008000',
+    lime: '00ff00',
+    maroon: '800000',
+    navy: '000080',
+    olive: '808000',
+    purple: '800080',
+    red: 'ff0000',
+    silver: 'c0c0c0',
+    teal: '008080',
+    white: 'ffffff',
+    yellow: 'ffff00'
+  };
+  
+  var colorKeywords = {
+    aliceblue: 'f0f8ff',
+    antiquewhite: 'faebd7',
+    aqua: '00ffff',
+    aquamarine: '7fffd4',
+    azure: 'f0ffff',
+    beige: 'f5f5dc',
+    bisque: 'ffe4c4',
+    black: '000000',
+    blanchedalmond: 'ffebcd',
+    blue: '0000ff',
+    blueviolet: '8a2be2',
+    brown: 'a52a2a',
+    burlywood: 'deb887',
+    cadetblue: '5f9ea0',
+    chartreuse: '7fff00',
+    chocolate: 'd2691e',
+    coral: 'ff7f50',
+    cornflowerblue: '6495ed',
+    cornsilk: 'fff8dc',
+    crimson: 'dc143c',
+    cyan: '00ffff',
+    darkblue: '00008b',
+    darkcyan: '008b8b',
+    darkgoldenrod: 'b8860b',
+    darkgray: 'a9a9a9',
+    darkgreen: '006400',
+    darkkhaki: 'bdb76b',
+    darkmagenta: '8b008b',
+    darkolivegreen: '556b2f',
+    darkorange: 'ff8c00',
+    darkorchid: '9932cc',
+    darkred: '8b0000',
+    darksalmon: 'e9967a',
+    darkseagreen: '8fbc8f',
+    darkslateblue: '483d8b',
+    darkslategray: '2f4f4f',
+    darkturquoise: '00ced1',
+    darkviolet: '9400d3',
+    deeppink: 'ff1493',
+    deepskyblue: '00bfff',
+    dimgray: '696969',
+    dodgerblue: '1e90ff',
+    feldspar: 'd19275',
+    firebrick: 'b22222',
+    floralwhite: 'fffaf0',
+    forestgreen: '228b22',
+    fuchsia: 'ff00ff',
+    gainsboro: 'dcdcdc',
+    ghostwhite: 'f8f8ff',
+    gold: 'ffd700',
+    goldenrod: 'daa520',
+    gray: '808080',
+    green: '008000',
+    greenyellow: 'adff2f',
+    honeydew: 'f0fff0',
+    hotpink: 'ff69b4',
+    indianred : 'cd5c5c',
+    indigo : '4b0082',
+    ivory: 'fffff0',
+    khaki: 'f0e68c',
+    lavender: 'e6e6fa',
+    lavenderblush: 'fff0f5',
+    lawngreen: '7cfc00',
+    lemonchiffon: 'fffacd',
+    lightblue: 'add8e6',
+    lightcoral: 'f08080',
+    lightcyan: 'e0ffff',
+    lightgoldenrodyellow: 'fafad2',
+    lightgrey: 'd3d3d3',
+    lightgreen: '90ee90',
+    lightpink: 'ffb6c1',
+    lightsalmon: 'ffa07a',
+    lightseagreen: '20b2aa',
+    lightskyblue: '87cefa',
+    lightslateblue: '8470ff',
+    lightslategray: '778899',
+    lightsteelblue: 'b0c4de',
+    lightyellow: 'ffffe0',
+    lime: '00ff00',
+    limegreen: '32cd32',
+    linen: 'faf0e6',
+    magenta: 'ff00ff',
+    maroon: '800000',
+    mediumaquamarine: '66cdaa',
+    mediumblue: '0000cd',
+    mediumorchid: 'ba55d3',
+    mediumpurple: '9370d8',
+    mediumseagreen: '3cb371',
+    mediumslateblue: '7b68ee',
+    mediumspringgreen: '00fa9a',
+    mediumturquoise: '48d1cc',
+    mediumvioletred: 'c71585',
+    midnightblue: '191970',
+    mintcream: 'f5fffa',
+    mistyrose: 'ffe4e1',
+    moccasin: 'ffe4b5',
+    navajowhite: 'ffdead',
+    navy: '000080',
+    oldlace: 'fdf5e6',
+    olive: '808000',
+    olivedrab: '6b8e23',
+    orange: 'ffa500',
+    orangered: 'ff4500',
+    orchid: 'da70d6',
+    palegoldenrod: 'eee8aa',
+    palegreen: '98fb98',
+    paleturquoise: 'afeeee',
+    palevioletred: 'd87093',
+    papayawhip: 'ffefd5',
+    peachpuff: 'ffdab9',
+    peru: 'cd853f',
+    pink: 'ffc0cb',
+    plum: 'dda0dd',
+    powderblue: 'b0e0e6',
+    purple: '800080',
+    red: 'ff0000',
+    rosybrown: 'bc8f8f',
+    royalblue: '4169e1',
+    saddlebrown: '8b4513',
+    salmon: 'fa8072',
+    sandybrown: 'f4a460',
+    seagreen: '2e8b57',
+    seashell: 'fff5ee',
+    sienna: 'a0522d',
+    silver: 'c0c0c0',
+    skyblue: '87ceeb',
+    slateblue: '6a5acd',
+    slategray: '708090',
+    snow: 'fffafa',
+    springgreen: '00ff7f',
+    steelblue: '4682b4',
+    tan: 'd2b48c',
+    teal: '008080',
+    thistle: 'd8bfd8',
+    tomato: 'ff6347',
+    turquoise: '40e0d0',
+    violet: 'ee82ee',
+    violetred: 'd02090',
+    wheat: 'f5deb3',
+    white: 'ffffff',
+    whitesmoke: 'f5f5f5',
+    yellow: 'ffff00',
+    yellowgreen: '9acd32'
+  };
+  
+  // TODO: add support for 'transparent' support => rgba(0,0,0,0). see http://www.w3.org/TR/css3-color/#transparent-def
 
   Color.BLACK = new Color( 0, 0, 0 );
   Color.BLUE = new Color( 0, 0, 255 );
