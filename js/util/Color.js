1a58ca4cea1bf73c9f22bab7053688404a57183f
Jonathan Olson <olsonsjc@gmail.com>
2013-03-05 19:24:19 -0800

    Adding Color.withAlpha()

--- js/util/Color.js
+++ js/util/Color.js
@@ -211,6 +211,10 @@ define( function( require ) {
     
     equals: function( color ) {
       return this.r === color.r && this.g === color.g && this.b === color.b && this.a === color.a;
+    },
+    
+    withAlpha: function( alpha ) {
+      return new Color( this.r, this.g, this.b, alpha );
     }
   };
   
