e5a53a2e4ace1ce3c6862562d37562693a0308a6
Jonathan Olson <olsonsjc@gmail.com>
2013-03-19 20:49:19 -0700

    Refactoring out pixel test logic

--- tests/qunit/js/pixel-perfect.js
+++ tests/qunit/js/pixel-perfect.js
@@ -4,41 +4,43 @@
   
   module( 'Scenery: Pixel Perfect' );
   
-  asyncTest( 'Rectangle with stroke', function() {
-    function setup( scene ) {
+  function pixelTest( name, setup, dataURL, threshold ) {
+    asyncTest( name, function() {
+      function process() {
+        if ( window.snapshotFromDataURL ) {
+          snapshotFromDataURL( dataURL, function( dataURLSnapshot ) {
+            var $div = $( '<div>' );
+            $div.width( dataURLSnapshot.width );
+            $div.height( dataURLSnapshot.height );
+            
+            var scene = new scenery.Scene( $div );
+            setup( scene );
+            scene.updateScene();
+            
+            asyncSnapshot( scene, function( sceneSnapshot ) {
+              snapshotEquals( sceneSnapshot, dataURLSnapshot, threshold, name );
+              start();
+            }, dataURLSnapshot.width, dataURLSnapshot.height );
+          } );
+        } else {
+          setTimeout( process, 40 );
+        }
+      }
+      
+      setTimeout( process, 40 );
+    } );
+  }
+  
+  pixelTest( 'Rectangle with stroke',
+    function( scene ) {
       scene.addChild( new scenery.Path( {
         shape: kite.Shape.rectangle( 8, 8, 48, 48 ),
         fill: '#000000',
         stroke: '#ff0000',
         lineWidth: 2
       } ) );
-    }
-    
-    var dataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAQUlEQVRYR+3RwQkAMAgDQN1/6LYTlH5EKRfyN5wZzcnm+2EAgdkCK+K0NleBMQMq//QkYAABAgQIECBAgACBrwU2CDoyBu/dqEkAAAAASUVORK5CYII=";
-    
-    var name = QUnit.config.current.testName;
-    
-    function process() {
-      if ( window.snapshotFromDataURL ) {
-        snapshotFromDataURL( dataURL, function( dataURLSnapshot ) {
-          var $div = $( '<div>' );
-          $div.width( dataURLSnapshot.width );
-          $div.height( dataURLSnapshot.height );
-          
-          var scene = new scenery.Scene( $div );
-          setup( scene );
-          scene.updateScene();
-          
-          asyncSnapshot( scene, function( sceneSnapshot ) {
-            snapshotEquals( sceneSnapshot, dataURLSnapshot, 0, name );
-            start();
-          }, dataURLSnapshot.width, dataURLSnapshot.height );
-        } );
-      } else {
-        setTimeout( process, 40 );
-      }
-    }
-    
-    setTimeout( process, 40 );
-  } );
+    }, "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAQUlEQVRYR+3RwQkAMAgDQN1/6LYTlH5EKRfyN5wZzcnm+2EAgdkCK+K0NleBMQMq//QkYAABAgQIECBAgACBrwU2CDoyBu/dqEkAAAAASUVORK5CYII=",
+    0
+  );
+  
 })();
