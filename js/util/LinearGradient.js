a293cfc05e17d456016427c52a51f8d28ffab228
Jonathan Olson <olsonsjc@gmail.com>
2013-03-27 19:31:38 -0700

    Allowing chaining on setTransformMatrix calls on gradients/patterns

--- js/util/LinearGradient.js
+++ js/util/LinearGradient.js
@@ -58,6 +58,7 @@ define( function( require ) {
     
     setTransformMatrix: function( transformMatrix ) {
       this.transformMatrix = transformMatrix;
+      return this;
     },
     
     getCanvasStyle: function() {
