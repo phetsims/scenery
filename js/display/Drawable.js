4d37f666003f1897a3bddbdfd3859266b983454b
Jonathan Olson <olsonsjc@gmail.com>
2013-12-09 12:11:56 -0700

    Doc notes

--- js/display/Drawable.js
+++ js/display/Drawable.js
@@ -26,6 +26,9 @@ define( function( require ) {
   var Drawable = scenery.Drawable;
   
   inherit( Object, Drawable, {
+    
+    // NOTE: must have a 'markDirty' method
+    
     dispose: function() {
       
     },
