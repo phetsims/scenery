e42677fa16be34e850c604b69a7801606259c22e
Sam Reid <reids@colorado.edu>
2014-01-17 14:59:19 -0700

    Added options to VBox to make it update dynamically, added a test case

--- js/nodes/VBox.js
+++ js/nodes/VBox.js
@@ -3,7 +3,6 @@
 /**
  * VBox arranges the child nodes vertically, and they can be centered, left or right justified.
  * Vertical spacing can be set as a constant or a function which depends on the adjacent nodes.
- * TODO: add an option (not enabled by default) to update layout when children or children bounds change
  *
  * @author Sam Reid
  */
@@ -11,9 +10,9 @@
 define( function( require ) {
   'use strict';
 
+  var inherit = require( 'PHET_CORE/inherit' );
   var scenery = require( 'SCENERY/scenery' );
   var Node = require( 'SCENERY/nodes/Node' );
-  var objectCreate = require( 'SCENERY/util/Util' ).objectCreate; // i.e. Object.create
 
   /**
    *
@@ -26,58 +25,104 @@ define( function( require ) {
    * @constructor
    */
   scenery.VBox = function VBox( options ) {
+    this.boundsListener = this.updateLayout.bind( this );
+
     // ensure we have a parameter object
-    this.options = options = _.extend( {
+    this.options = _.extend( {
       // defaults
       spacing: function() { return 0; },
-      align: 'center'
+      align: 'center',
+
+      //By default, don't update the layout when changes are made
+      updateLayoutOnChildBoundsChange: false,
+      updateLayoutOnChildAddedOrRemoved: false
     }, options );
 
-    if ( typeof options.spacing === 'number' ) {
-      var spacingConstant = options.spacing;
-      options.spacing = function() { return spacingConstant; };
+    if ( typeof this.options.spacing === 'number' ) {
+      var spacingConstant = this.options.spacing;
+      this.options.spacing = function() { return spacingConstant; };
     }
 
-    Node.call( this, options );
-    this.updateLayout();
+    Node.call( this );
 
-    //TODO: perhaps not all options must be passed to Node.call AND this.mutate.  On 7/17/2013, layout problems occurred when using only mutate() or only Node.call(this,options)
     //See HBox.js
-    if ( options ) {
-      this.mutate( options );
-    }
+    this.inited = false;
+    this.mutate( this.options );
+    this.inited = true;
   };
   var VBox = scenery.VBox;
 
-  VBox.prototype = objectCreate( Node.prototype );
+  inherit( Node, VBox, {
+    updateLayout: function() {
+      if ( !this.updatingLayout ) {
+        //Bounds of children are changed in updateLayout, we don't want to stackoverflow so bail if already updating layout
+        this.updatingLayout = true;
+        var minX = _.min( _.map( this.children, function( child ) {return child.left;} ) );
+        var maxX = _.max( _.map( this.children, function( child ) {return child.left + child.width;} ) );
+        var centerX = (maxX + minX) / 2;
 
-  VBox.prototype.updateLayout = function() {
-    var minX = _.min( _.map( this.children, function( child ) {return child.left;} ) );
-    var maxX = _.max( _.map( this.children, function( child ) {return child.left + child.width;} ) );
-    var centerX = (maxX + minX) / 2;
+        //Start at y=0 in the coordinate frame of this node.  Not possible to set this through the spacing option, instead just set it with the {y:number} option.
+        var y = 0;
+        for ( var i = 0; i < this.children.length; i++ ) {
+          var child = this.children[i];
+          child.top = y;
 
-    //Start at y=0 in the coordinate frame of this node.  Not possible to set this through the spacing option, instead just set it with the {y:number} option.
-    var y = 0;
-    for ( var i = 0; i < this.children.length; i++ ) {
-      var child = this.children[i];
-      child.top = y;
+          //Set the position horizontally
+          if ( this.options.align === 'left' ) {
+            child.left = minX;
+          }
+          else if ( this.options.align === 'right' ) {
+            child.right = maxX;
+          }
+          else {//default to center
+            child.centerX = centerX;
+          }
 
-      //Set the position horizontally
-      if ( this.options.align === 'left' ) {
-        child.left = minX;
+          //Move to the next vertical position.
+          y += child.height + this.options.spacing( child, this.children[i + 1] );
+        }
+        this.updatingLayout = false;
       }
-      else if ( this.options.align === 'right' ) {
-        child.right = maxX;
-      }
-      else {//default to center
-        child.centerX = centerX;
-      }
-
-      //Move to the next vertical position.
-      y += child.height + this.options.spacing( child, this.children[i + 1] );
     }
-  };
-  VBox.prototype.constructor = VBox;
+  } );
+
+  //Override the child mutators to updateLayout
+  //Have to listen to the child bounds individually because there are a number of possible ways to change the child
+  //bounds without changing the overall bounds.
+  var overrides = ['insertChild', 'removeChildWithIndex'];
+  overrides.forEach( function( override ) {
+
+    //Support up to two args for overrides
+    VBox.prototype[override] = function( arg1, arg2 ) {
+
+      //Remove event listeners from any nodes (will be added back later if the node was not removed)
+      var vbox = this;
+      if ( this.options.updateLayoutOnChildBoundsChange ) {
+        this.children.forEach( function( child ) {
+          if ( child.containsEventListener( 'bounds', vbox.boundsListener ) ) {
+            child.removeEventListener( 'bounds', vbox.boundsListener );
+          }
+        } );
+      }
+
+      //Super call
+      Node.prototype[override].call( this, arg1, arg2 );
+
+      //Update the layout if it should be dynamic
+      if ( this.options.updateLayoutOnChildAddedOrRemoved || !this.inited ) {
+        this.updateLayout();
+      }
+
+      //Add event listeners for any current children (if it should be dynamic)
+      if ( this.options.updateLayoutOnChildBoundsChange ) {
+        this.children.forEach( function( child ) {
+          if ( !child.containsEventListener( 'bounds', vbox.boundsListener ) ) {
+            child.addEventListener( 'bounds', vbox.boundsListener );
+          }
+        } );
+      }
+    }
+  } );
 
   return VBox;
 } );
