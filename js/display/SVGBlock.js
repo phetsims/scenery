295d8fd0d02711f4ccf0bc19e20b857816037b68
Jonathan Olson <olsonsjc@gmail.com>
2014-04-26 16:09:57 -0600

    Initial SVGBlock setup

--- js/display/SVGBlock.js
+++ js/display/SVGBlock.js
@@ -10,21 +10,65 @@ define( function( require ) {
   'use strict';
   
   var inherit = require( 'PHET_CORE/inherit' );
+  var Poolable = require( 'PHET_CORE/Poolable' );
   var scenery = require( 'SCENERY/scenery' );
   var Drawable = require( 'SCENERY/display/Drawable' );
   
-  scenery.SVGBlock = function SVGBlock( renderer ) {
-    Drawable.call( this, renderer );
-    // TODO: add count of boundsless objects
+  scenery.SVGBlock = function SVGBlock( renderer, transformRootInstance, filterRootInstance ) {
+    this.initialize( renderer, transformRootInstance, filterRootInstance );
   };
   var SVGBlock = scenery.SVGBlock;
   
   inherit( Drawable, SVGBlock, {
-    getDomElement: function() {
+    initialize: function( renderer, transformRootInstance, filterRootInstance ) {
+      this.initializeDrawable( renderer );
       
-    }
+      this.transformRootInstance = transformRootInstance;
+      this.filterRootInstance = filterRootInstance;
+      
+      if ( !this.domElement ) {
+        // main SVG element
+        this.svg = document.createElementNS( scenery.svgns, 'svg' );
+        // this.svg.setAttribute( 'width', width );
+        // this.svg.setAttribute( 'height', height );
+        this.svg.setAttribute( 'stroke-miterlimit', 10 ); // to match our Canvas brethren so we have the same default behavior
+        // this.svg.style.position = 'absolute';
+        // this.svg.style.left = '0';
+        // this.svg.style.top = '0';
+        // this.svg.style.clip = 'rect(0px,' + width + 'px,' + height + 'px,0px)';
+        this.svg.style['pointer-events'] = 'none';
+        
+        // the <defs> block that we will be stuffing gradients and patterns into
+        this.defs = document.createElementNS( scenery.svgns, 'defs' );
+        this.svg.appendChild( this.defs );
+        
+        this.domElement = this.svg;
+      }
+      
+      // TODO: add count of boundsless objects?
+      // TODO: dirty list of nodes (each should go dirty only once, easier than scanning all?)
+    },
     
-    // TODO: add canvas draw methods
+    dispose: function() {
+      // clear references
+      this.transformRootInstance = null;
+      this.filterRootInstance = null;
+      
+      Drawable.prototype.dispose.call( this );
+    }
+  } );
+  
+  /* jshint -W064 */
+  Poolable( SVGBlock, {
+    constructorDuplicateFactory: function( pool ) {
+      return function( renderer, transformRootInstance, filterRootInstance ) {
+        if ( pool.length ) {
+          return pool.pop().initialize( renderer, transformRootInstance, filterRootInstance );
+        } else {
+          return new SVGBlock( renderer, transformRootInstance, filterRootInstance );
+        }
+      };
+    }
   } );
   
   return SVGBlock;
