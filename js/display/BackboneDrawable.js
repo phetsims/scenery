f39fec3f1a9531ac3ea6ce9457ddfff32face275
Jonathan Olson <olsonsjc@gmail.com>
2014-06-26 01:15:56 -0600

    Re-enabling greedy on branch

--- js/display/BackboneDrawable.js
+++ js/display/BackboneDrawable.js
@@ -20,7 +20,7 @@ define( function( require ) {
   var RebuildStitcher = require( 'SCENERY/display/RebuildStitcher' );
   var Util = require( 'SCENERY/util/Util' );
   
-  var useGreedyStitcher = false;
+  var useGreedyStitcher = true;
   
   scenery.BackboneDrawable = function BackboneDrawable( display, backboneInstance, transformRootInstance, renderer, isDisplayRoot ) {
     this.initialize( display, backboneInstance, transformRootInstance, renderer, isDisplayRoot );
