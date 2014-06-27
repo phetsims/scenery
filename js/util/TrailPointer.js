7106fe64df997dfe582a9d400280c62f493eef47
Jonathan Olson <olsonsjc@gmail.com>
2014-06-26 21:25:20 -0600

    Adding deprecated note

--- js/util/TrailPointer.js
+++ js/util/TrailPointer.js
@@ -20,6 +20,7 @@ define( function( require ) {
   /*
    * isBefore: whether this points to before the node (and its children) have been rendered, or after
    */
+  //@deprecated
   scenery.TrailPointer = function TrailPointer( trail, isBefore ) {
     assert && assert( trail instanceof scenery.Trail, 'trail is not a trail' );
     this.trail = trail;
