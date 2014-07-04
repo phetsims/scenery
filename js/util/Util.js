6624593ff8a260fd7a3f004428a6d76c0287171f
Jonathan Olson <olsonsjc@gmail.com>
2014-07-03 20:48:03 -0500

    Removing deprecated polyfill

--- js/util/Util.js
+++ js/util/Util.js
@@ -42,17 +42,6 @@ define( function( require ) {
       return obj;
     },
 
-    // Object.create polyfill
-    objectCreate: Object.create || function( o ) {
-      if ( arguments.length > 1 ) {
-        throw new Error( 'Object.create implementation only accepts the first parameter.' );
-      }
-      function F() {}
-
-      F.prototype = o;
-      return new F();
-    },
-
     // @deprecated (bad performance since it is setting multiple properties). see applyPreparedTransform
     applyCSSTransform: function( matrix, element, forceAcceleration ) {
       var transformCSS = matrix.getCSSTransform();
