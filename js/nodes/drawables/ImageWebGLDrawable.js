c27d81309e8fb35e11e4d0d86d2c9d3b5b9b1ba2
Sam Reid <reids@colorado.edu>
2014-09-22 08:29:00 -0600

    Use LINEAR_MIPMAP_NEAREST for texture filtering

--- js/nodes/drawables/ImageWebGLDrawable.js
+++ js/nodes/drawables/ImageWebGLDrawable.js
@@ -59,10 +59,15 @@ define( function( require ) {
       gl.bindTexture( gl.TEXTURE_2D, texture );
       gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
       gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
-      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR );
-      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR ); // TODO: better filtering
+
       gl.pixelStorei( gl.UNPACK_FLIP_Y_WEBGL, true );
       gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas );
+
+      // Texture filtering, see http://learningwebgl.com/blog/?p=571
+      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
+      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST );
+      gl.generateMipmap( gl.TEXTURE_2D );
+
       gl.bindTexture( gl.TEXTURE_2D, null );
     },
 
