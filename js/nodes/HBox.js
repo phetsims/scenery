66695595b8305d807e88d6e71cf7c46c2fc94029
Jonathan Olson <olsonsjc@gmail.com>
2014-11-06 02:48:06 -0700

    Syncing HBox/VBox with master, removing AbstractBox

--- js/nodes/HBox.js
+++ js/nodes/HBox.js
@@ -1,59 +1,26 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder Boulder
 
 /**
- * VBox arranges the child nodes vertically, and they can be centered, left or right justified.
- * Vertical spacing can be set as a constant or a function which depends on the adjacent nodes.
- *
- * See a dynamic test in scenery\tests\test-vbox.html
+ * HBox is a convenience specialization of LayoutBox with horizontal orientation.
  *
  * @author Sam Reid
  */
-
 define( function( require ) {
   'use strict';
 
   var inherit = require( 'PHET_CORE/inherit' );
+  var LayoutBox = require( 'SCENERY/nodes/LayoutBox' );
   var scenery = require( 'SCENERY/scenery' );
-  var AbstractBox = require( 'SCENERY/nodes/AbstractBox' );
 
   /**
-   *
-   * @param options Same as Node.constructor.options with the following additions:
-   *
-   * spacing: can be a number or a function.  If a number, then it will be the vertical spacing between each object.
-   *              If a function, then the function will have the signature function(top,bottom){} which returns the spacing between adjacent pairs of items.
-   * align:   How to line up the items horizontally.  One of 'center', 'top' or 'bottom'.  Defaults to 'center'.
-   *
+   * @param {Object} [options] see LayoutBox
    * @constructor
    */
-  scenery.HBox = function VBox( options ) {
-    AbstractBox.call( this, 'horizontal', function() {
-      var minY = _.min( _.map( this.children, function( child ) {return child.top;} ) );
-      var maxY = _.max( _.map( this.children, function( child ) {return child.top + child.height;} ) );
-      var centerY = (maxY + minY) / 2;
+  function HBox( options ) {
+    LayoutBox.call( this, _.extend( {}, options, { orientation: 'horizontal' } ) );
+  }
 
-      //Start at x=0 in the coordinate frame of this node.  Not possible to set this through the spacing option, instead just set it with the {x:number} option.
-      var x = 0;
-      for ( var i = 0; i < this.children.length; i++ ) {
-        var child = this.children[i];
-        child.left = x;
+  scenery.HBox = HBox;
 
-        //Set the position horizontally
-        if ( this.options.align === 'top' ) {
-          child.top = minY;
-        }
-        else if ( this.options.align === 'bottom' ) {
-          child.bottom = maxY;
-        }
-        else {//default to center
-          child.centerY = centerY;
-        }
-
-        //Move to the next vertical position.
-        x += child.width + this.options.spacing( child, this.children[i + 1] );
-      }
-    }, options );
-  };
-
-  return inherit( AbstractBox, scenery.HBox );
+  return inherit( LayoutBox, HBox );
 } );
--- js/nodes/VBox.js
+++ js/nodes/VBox.js
@@ -1,59 +1,26 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder Boulder
 
 /**
- * VBox arranges the child nodes vertically, and they can be centered, left or right justified.
- * Vertical spacing can be set as a constant or a function which depends on the adjacent nodes.
- *
- * See a dynamic test in scenery\tests\test-vbox.html
+ * VBox is a convenience specialization of LayoutBox with vertical orientation.
  *
  * @author Sam Reid
  */
-
 define( function( require ) {
   'use strict';
 
   var inherit = require( 'PHET_CORE/inherit' );
+  var LayoutBox = require( 'SCENERY/nodes/LayoutBox' );
   var scenery = require( 'SCENERY/scenery' );
-  var AbstractBox = require( 'SCENERY/nodes/AbstractBox' );
 
   /**
-   *
-   * @param options Same as Node.constructor.options with the following additions:
-   *
-   * spacing: can be a number or a function.  If a number, then it will be the vertical spacing between each object.
-   *              If a function, then the function will have the signature function(top,bottom){} which returns the spacing between adjacent pairs of items.
-   * align:   How to line up the items horizontally.  One of 'center', 'left' or 'right'.  Defaults to 'center'.
-   *
+   * @param {Object} [options] see LayoutBox
    * @constructor
    */
-  scenery.VBox = function VBox( options ) {
-    AbstractBox.call( this, 'vertical', function() {
-      var minX = _.min( _.map( this.children, function( child ) {return child.left;} ) );
-      var maxX = _.max( _.map( this.children, function( child ) {return child.left + child.width;} ) );
-      var centerX = (maxX + minX) / 2;
+  function VBox( options ) {
+    LayoutBox.call( this, _.extend( {}, options, { orientation: 'vertical' } ) );
+  }
 
-      //Start at y=0 in the coordinate frame of this node.  Not possible to set this through the spacing option, instead just set it with the {y:number} option.
-      var y = 0;
-      for ( var i = 0; i < this.children.length; i++ ) {
-        var child = this.children[i];
-        child.top = y;
+  scenery.VBox = VBox;
 
-        //Set the position horizontally
-        if ( this.options.align === 'left' ) {
-          child.left = minX;
-        }
-        else if ( this.options.align === 'right' ) {
-          child.right = maxX;
-        }
-        else {//default to center
-          child.centerX = centerX;
-        }
-
-        //Move to the next vertical position.
-        y += child.height + this.options.spacing( child, this.children[i + 1] );
-      }
-    }, options );
-  };
-
-  return inherit( AbstractBox, scenery.VBox );
+  return inherit( LayoutBox, VBox );
 } );
