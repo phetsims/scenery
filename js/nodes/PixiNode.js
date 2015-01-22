9149e2c3f37a3eb5be283a4fefb05e56485c0b3f
Sam Reid <reids@colorado.edu>
2015-01-22 15:13:56 -0700

    Initial support for Text and children in PixiNode

--- js/nodes/PixiNode.js
+++ js/nodes/PixiNode.js
@@ -17,62 +17,98 @@ define( function( require ) {
   var DOM = require( 'SCENERY/nodes/DOM' );
   var Path = require( 'SCENERY/nodes/Path' );
   var scenery = require( 'SCENERY/scenery' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Text = require( 'SCENERY/nodes/Text' );
 
   /**
-   *
-   * @param sceneryRootNode
-   * @constructor
+   * Convert a single scenery node to a pixi node (without the children, which are handled in toPixi)
    */
-  scenery.PixiNode = function PixiNode( sceneryRootNode, options ) {
-    // iterate over the full dag and turn into pixi displayObjects
-    // additionally observe all elements in the dag and update this node when they change.
-
-
-    var stage = new PIXI.Stage( 0xFFFFFF );
-
-    var renderer = PIXI.autoDetectRenderer( 400, 300, { transparent: true } );
-
-    DOM.call( this, renderer.view, options );
-    var toPixi = function( sceneryNode ) {
+  var toPixiWithoutChildren = function( sceneryNode ) {
+    var i;
+    if ( sceneryNode instanceof Path ) {
 
       // TODO: Use Pixi.Shape where possible
-      if ( sceneryNode instanceof Path ) {
-        var path = sceneryNode;
-        var graphics = new PIXI.Graphics();
+      var path = sceneryNode;
+      var graphics = new PIXI.Graphics();
 
-        graphics.beginFill( path.getFillColor().toNumber() );
-        var shape = path.shape;
+      graphics.beginFill( path.getFillColor().toNumber() );
+      var shape = path.shape;
 
-        for ( var i = 0; i < shape.subpaths.length; i++ ) {
-          var subpath = shape.subpaths[ i ];
-          for ( var k = 0; k < subpath.segments.length; k++ ) {
-            var segment = subpath.segments[ k ];
-            if ( i === 0 && k === 0 ) {
-              graphics.moveTo( segment.start.x, segment.start.y );
-            }
-            else {
-              graphics.lineTo( segment.start.x, segment.start.y );
-            }
+      for ( i = 0; i < shape.subpaths.length; i++ ) {
+        var subpath = shape.subpaths[ i ];
+        for ( var k = 0; k < subpath.segments.length; k++ ) {
+          var segment = subpath.segments[ k ];
+          if ( i === 0 && k === 0 ) {
+            graphics.moveTo( segment.start.x, segment.start.y );
+          }
+          else {
+            graphics.lineTo( segment.start.x, segment.start.y );
+          }
 
-            if ( i === shape.subpaths.length - 1 && k === subpath.segments.length - 1 ) {
-              graphics.lineTo( segment.end.x, segment.end.y );
-            }
+          if ( k === subpath.segments.length - 1 ) {
+            graphics.lineTo( segment.end.x, segment.end.y );
           }
         }
 
-        graphics.endFill();
-        return graphics;
+        if ( subpath.isClosed() ) {
+          segment = subpath.segments[ 0 ];
+          graphics.lineTo( segment.start.x, segment.start.y );
+        }
       }
-      else {
-        console.log( 'unknown node type', sceneryNode );
-      }
-    };
 
-    var root = toPixi( sceneryRootNode );
-    stage.addChild( root );
+      graphics.endFill();
 
+      return graphics;
+    }
+    else if ( sceneryNode instanceof Text ) {
+      return new PIXI.Text( sceneryNode.text );
+    }
+    else if ( sceneryNode instanceof Node ) {
+
+      // Handle node case last since Path, Text, Image also instanceof Node
+      return new PIXI.DisplayObjectContainer();
+    }
+    else {
+      throw new Error( 'unknown node type', sceneryNode );
+    }
+  };
+
+  /**
+   * Recursively convert a scenery node to a pixi DisplayObject
+   * @param {Node} sceneryNode
+   * @returns {PIXI.DisplayObject}
+   */
+  var toPixi = function toPixi( sceneryNode ) {
+    var pixiNode = toPixiWithoutChildren( sceneryNode );
+    for ( var i = 0; i < sceneryNode.children.length; i++ ) {
+      pixiNode.addChild( toPixi( sceneryNode.children[ i ] ) );
+    }
+    return pixiNode;
+  };
+
+  /**
+   * Iterate over the full dag and turn into pixi displayObjects
+   * additionally observe all elements in the dag and update this node when they change.
+   * @param sceneryRootNode
+   * @param options
+   * @constructor
+   */
+  scenery.PixiNode = function PixiNode( sceneryRootNode, options ) {
+
+    // Create the Pixi Stage
+    var stage = new PIXI.Stage( 0xFFFFFF );
+
+    // Convert the scenery node to Pixi and add it to the stage
+    stage.addChild( toPixi( sceneryRootNode ) );
+
+    // Create the renderer and view
+    var renderer = PIXI.autoDetectRenderer( 400, 300, { transparent: true } );
+
+    // Initial draw
     renderer.render( stage );
 
+    // Show the canvas in the DOM
+    DOM.call( this, renderer.view, options );
   };
 
   return inherit( DOM, scenery.PixiNode );
