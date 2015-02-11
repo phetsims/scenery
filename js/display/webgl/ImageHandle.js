f26cd6125096a2d7cee6f66de9236e9353e1ea75
Sam Reid <reids@colorado.edu>
2015-02-11 11:32:44 -0700

    Moved methods to prototype

--- js/display/webgl/ImageHandle.js
+++ js/display/webgl/ImageHandle.js
@@ -61,51 +61,54 @@ define( function( require ) {
       x2, y2, z, u1, v1, matrix4.m00(), matrix4.m01(), matrix4.m03(), matrix4.m10(), matrix4.m11(), matrix4.m13()
     );
 
+    this.startIndex = index;
+    this.endIndex = textureBufferData.vertexArray.length;
+    this.image = image;
+    this.frameRange = frameRange;
+    this.textureBufferData = textureBufferData;
     //Track the index so it can delete itself, update itself, etc.
     //TODO: Move to a separate class.
-    return {
-      startIndex: index,
-      endIndex: textureBufferData.vertexArray.length,
-      image: image,
-      frameRange: frameRange,
-      setTransform: function( matrix4 ) {
-        for ( var i = 0; i < 6; i++ ) {
-          textureBufferData.vertexArray[ index + 5 + i * 11 ] = matrix4.m00();
-          textureBufferData.vertexArray[ index + 6 + i * 11 ] = matrix4.m01();
-          textureBufferData.vertexArray[ index + 7 + i * 11 ] = matrix4.m03();
-          textureBufferData.vertexArray[ index + 8 + i * 11 ] = matrix4.m10();
-          textureBufferData.vertexArray[ index + 9 + i * 11 ] = matrix4.m11();
-          textureBufferData.vertexArray[ index + 10 + i * 11 ] = matrix4.m13();
-        }
-      },
-      setRect: function( x, y, width, height ) {
-
-        var x1 = x;
-        var y1 = y;
-        var x2 = x1 + width;
-        var y2 = y1 + height;
-        textureBufferData.vertexArray[ index + 0 ] = x1;
-        textureBufferData.vertexArray[ index + 1 ] = y1;
-
-        textureBufferData.vertexArray[ index + 11 ] = x2;
-        textureBufferData.vertexArray[ index + 12 ] = y1;
-
-        textureBufferData.vertexArray[ index + 22 ] = x1;
-        textureBufferData.vertexArray[ index + 23 ] = y2;
-
-        textureBufferData.vertexArray[ index + 33 ] = x1;
-        textureBufferData.vertexArray[ index + 34 ] = y2;
-
-        textureBufferData.vertexArray[ index + 44 ] = x2;
-        textureBufferData.vertexArray[ index + 45 ] = y1;
-
-        textureBufferData.vertexArray[ index + 55 ] = x2;
-        textureBufferData.vertexArray[ index + 56 ] = y2;
-
-      }
-    };
-
   }
 
-  return inherit( Object, ImageHandle, {} );
+  return inherit( Object, ImageHandle, {
+    setTransform: function( matrix4 ) {
+      var textureBufferData = this.textureBufferData;
+      var index = this.startIndex;
+      for ( var i = 0; i < 6; i++ ) {
+        textureBufferData.vertexArray[ index + 5 + i * 11 ] = matrix4.m00();
+        textureBufferData.vertexArray[ index + 6 + i * 11 ] = matrix4.m01();
+        textureBufferData.vertexArray[ index + 7 + i * 11 ] = matrix4.m03();
+        textureBufferData.vertexArray[ index + 8 + i * 11 ] = matrix4.m10();
+        textureBufferData.vertexArray[ index + 9 + i * 11 ] = matrix4.m11();
+        textureBufferData.vertexArray[ index + 10 + i * 11 ] = matrix4.m13();
+      }
+    },
+    setRect: function( x, y, width, height ) {
+      var index = this.startIndex;
+      var textureBufferData = this.textureBufferData;
+      var x1 = x;
+      var y1 = y;
+      var x2 = x1 + width;
+      var y2 = y1 + height;
+      textureBufferData.vertexArray[ index + 0 ] = x1;
+      textureBufferData.vertexArray[ index + 1 ] = y1;
+
+      textureBufferData.vertexArray[ index + 11 ] = x2;
+      textureBufferData.vertexArray[ index + 12 ] = y1;
+
+      textureBufferData.vertexArray[ index + 22 ] = x1;
+      textureBufferData.vertexArray[ index + 23 ] = y2;
+
+      textureBufferData.vertexArray[ index + 33 ] = x1;
+      textureBufferData.vertexArray[ index + 34 ] = y2;
+
+      textureBufferData.vertexArray[ index + 44 ] = x2;
+      textureBufferData.vertexArray[ index + 45 ] = y1;
+
+      textureBufferData.vertexArray[ index + 55 ] = x2;
+      textureBufferData.vertexArray[ index + 56 ] = y2;
+
+    }
+
+  } );
 } );
