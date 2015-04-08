79fb64c130a9b772d7871a927d4b1258279fe2ff
Jonathan Olson <olsonsjc@gmail.com>
2015-04-08 12:22:40 -0600

    Partial incomplete push, switching development machines

--- js/display/webgl/SpriteSheet.js
+++ js/display/webgl/SpriteSheet.js
@@ -1,59 +1,168 @@
 //  Copyright 2002-2014, University of Colorado Boulder
 
 /**
- * A single image with different regions within the image representing different distinct textures to be drawn in WebGL.
- * The TextureRenderer will normally use more than one SpriteSheet for rendering.
+ * A single Canvas/texture with multiple different images (sprites) drawn internally. During rendering, this texture
+ * can be used in one draw call to render multiple different images by providing UV coordinates to each quad for each
+ * image to be drawn.
  *
+ * TODO: How to use custom mipmap levels?
+ * TODO: Add padding around sprites, otherwise interpolation could cause issues!
  *
  * @author Sam Reid (PhET Interactive Simulations)
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 define( function( require ) {
   'use strict';
 
   // modules
+  var scenery = require( 'SCENERY/scenery' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Packer = require( 'SCENERY/display/webgl/Packer' );
+  var BinPacker = require( 'DOT/BinPacker' );
   var Bounds2 = require( 'DOT/Bounds2' );
 
   /**
-   *
    * @constructor
    */
-  function SpriteSheet() {
-    this.image = document.createElement( 'canvas' );
+  scenery.SpriteSheet = function SpriteSheet( useMipmaps ) {
+    this.useMipmaps = useMipmaps;
+
+    this.gl = null; // assume later creation of context for now
 
     // Use the max supported texture size (according to http://codeflow.org/entries/2013/feb/22/how-to-write-portable-webgl/ )
-    this.image.width = 2048;
-    this.image.height = 2048;
-    this.context = this.image.getContext( '2d' );
+    // TODO: potentially support larger texture sizes based on reported capabilities (could cause fewer draw calls?)
+    this.bounds = new Bounds2( 0, 0, 2048, 2048 );
+    assert && assert( this.bounds.minX === 0 && this.bounds.minY === 0, 'Assumed constraint later on for transforms' );
+    this.width = this.bounds.width;
+    this.height = this.bounds.height;
 
-    this.packer = new Packer( this.image.width, this.image.height );
+    this.canvas = document.createElement( 'canvas' );
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.context = this.canvas.getContext( '2d' );
 
-    // Flag as dirty initially because it has not yet been registered with the GPU as a texture unit.
-    // @public, settable
-    this.dirty = true;
-  }
+    this.binPacker = new BinPacker( this.bounds );
 
-  return inherit( Object, SpriteSheet, {
-    /**
-     * Draws the given image at a position calculated by the packer and returns the normalized bounded region
-     * reserved for the image within this SpriteSheet. returns null if the image cannot be drawn, upon which a
-     * new SpriteSheet will be created by SpriteBatch. see 'reserveImage' on SpriteBatch
-     *
-     * @param image
-     * @returns {Bounds2} // in normalized coordinates
-     */
-    reserveImageSpace: function( image ) {
-      var startPosition = this.packer.reserveSpace( image.width, image.height );
-      var normalizedBounds = null;
-      this.dirty = true;
-      if ( startPosition ) {
-        //Draw Image at specific position
-        this.context.drawImage( image, startPosition.x, startPosition.y );
-        normalizedBounds = Bounds2.rect( startPosition.x / this.image.width, startPosition.y / this.image.height,
-          image.width / this.image.width, image.height / this.image.height );
+    this.dirty = true; // @public [read-only] - Used to check if we need to updateTexture()
+
+    this.usedSprites = [];
+    this.unusedSprites = []; // works as a LRU cache for removing items when we need to allocate new space
+  };
+
+  inherit( Object, scenery.SpriteSheet, {
+    initializeContext: function( gl ) {
+      this.gl = gl;
+    },
+
+    createTexture: function() {
+      var gl = this.gl;
+
+      this.texture = gl.createTexture();
+      gl.bindTexture( gl.TEXTURE_2D, this.texture );
+      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
+      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
+      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, this.useMipmaps ? gl.LINEAR_MIPMAP_LINEAR : gl.LINEAR );
+      gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
+      gl.pixelStorei( gl.UNPACK_FLIP_Y_WEBGL, false );
+      gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.canvas );
+      if ( this.useMipmaps ) {
+        gl.hint( gl.GENERATE_MIPMAP_HINT, gl.NICEST );
+        gl.generateMipmap( gl.TEXTURE_2D );
       }
-      return normalizedBounds;
+      gl.bindTexture( gl.TEXTURE_2D, null );
+
+      this.dirty = false;
+    },
+
+    updateTexture: function() {
+      if ( this.dirty ) {
+        this.dirty = false;
+
+        var gl = this.gl;
+
+        gl.bindTexture( gl.TEXTURE_2D, this.texture );
+        gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.canvas );
+        if ( this.useMipmaps ) {
+          gl.generateMipmap( gl.TEXTURE_2D );
+        }
+        gl.bindTexture( gl.TEXTURE_2D, null );
+      }
+    },
+
+    /**
+     * Adds an image (if possible) to our sprite sheet. If successful, will return a {Sprite}, otherwise null.
+     *
+     * @param {HTMLCanvasElement | HTMLImageElement} image
+     * @param {number} width
+     * @param {number} height
+     *
+     * @returns {Sprite | null}
+     */
+    addImage: function( image, width, height ) {
+      var i;
+
+      // check used cache
+      for ( i = 0; i < this.usedSprites.length; i++ ) {
+        var usedSprite = this.usedSprites[i];
+        if ( usedSprite.image === image ) {
+          usedSprite.count++;
+          return usedSprite;
+        }
+      }
+
+      // check unused cache
+      for ( i = 0; i < this.unusedSprites.length; i++ ) {
+        var unusedSprite = this.unusedSprites[i];
+        if ( unusedSprite.image === image ) {
+          unusedSprite.count++;
+          this.unusedSprites.splice( i, 1 ); // remove it from the unused array
+          this.usedSprites.push( unusedSprite ); // add it to the used array
+          return unusedSprite;
+        }
+      }
+
+      // Not in any caches, let's try to find space. If we can't find space at first, we start removing unused sprites
+      // one-by-one.
+      var bin;
+      // Enters 'while' loop only if allocate() returns null and we have unused sprites (i.e. conditions where we will
+      // want to deallocate the least recently used (LRU) unused sprite and then check for allocation again).
+      while ( !( bin = this.binPacker.allocate( width, height ) ) && this.unusedSprites.length ) {
+        var ejectedSprite = this.unusedSprites.shift(); // LRU policy by taking first item
+
+        // clear its space in the Canvas
+        this.dirty = true;
+        var ejectedBounds = ejectedSprite.bin.bounds;
+        this.context.clearRect( ejectedBounds.x, ejectedBounds.y, ejectedBounds.width, ejectedBounds.height );
+
+        // deallocate its area in the bin packer
+        this.binPacker.deallocate( ejectedSprite.bin );
+      }
+
+      if ( bin ) {
+        // WebGL will want UV coordinates in the [0,1] range
+        var uvBounds = new Bounds2( bin.bounds.minX / this.width, bin.bounds.minY / this.height,
+                                    bin.bounds.maxX / this.width, bin.bounds.maxY / this.height );
+        var sprite = new scenery.SpriteSheet.Sprite( bin, uvBounds, image, 1 );
+        this.context.drawImage( image, bin.bounds.x, bin.bounds.y );
+        this.dirty = true;
+        this.usedSprites.push( sprite );
+      }
+      // no space, even after clearing out our unused sprites
+      else {
+        return null;
+      }
+    },
+
+    removeImage: function( image ) {
+
     }
   } );
+
+  scenery.SpriteSheet.Sprite = function( bin, uvBounds, image, initialCount ) {
+    this.bin = bin;
+    this.uvBounds = uvBounds;
+    this.image = image;
+    this.count = initialCount;
+  };
+
+  return scenery.SpriteSheet;
 } );
