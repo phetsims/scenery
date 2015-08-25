847b0c39cc06b517a2023322deff70dd2cf7ca25
Jonathan Olson <olsonsjc@gmail.com>
2015-08-24 18:38:17 -0600

    Fixing blending with separate functions and premultiplication. See https://github.com/phetsims/scenery/issues/397

--- js/display/WebGLBlock.js
+++ js/display/WebGLBlock.js
@@ -79,10 +79,10 @@ define( function( require ) {
         // What color gets set when we call gl.clear()
         gl.clearColor( 0, 0, 0, 0 );
 
-        // NOTE: not using gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA ) since that is for non-premultiplied alpha
-        // see http://webglfundamentals.org/webgl/lessons/webgl-and-alpha.html
-        gl.blendFunc( gl.ONE, gl.ONE_MINUS_SRC_ALPHA );
+        // Blending similar to http://localhost/phet/git/webgl-blendfunctions/blendfuncseparate.html
         gl.enable( gl.BLEND );
+        gl.blendEquationSeparate( gl.FUNC_ADD, gl.FUNC_ADD );
+        gl.blendFuncSeparate( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA );
 
         this.domElement = this.canvas;
 
