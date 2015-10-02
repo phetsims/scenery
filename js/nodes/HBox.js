e1edb6ee71c5a0885b5805f5a00987d863b917f8
Sam Reid <reids@colorado.edu>
2015-10-02 15:30:57 -0600

    Fixed copyright statements, see https://github.com/phetsims/chipper/issues/235

--- js/nodes/HBox.js
+++ js/nodes/HBox.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * HBox is a convenience specialization of LayoutBox with horizontal orientation.
--- js/nodes/VBox.js
+++ js/nodes/VBox.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * VBox is a convenience specialization of LayoutBox with vertical orientation.
