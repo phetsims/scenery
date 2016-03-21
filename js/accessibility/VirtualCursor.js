a1e896958762a62871f50c1b6e94b2ef44dfcee0
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-03-21 12:40:03 -0600

    initial work on getAccessibleText and goToNextItem to find text in the DOM, see #227.

--- js/accessibility/VirtualCursor.js
+++ js/accessibility/VirtualCursor.js
@@ -1,8 +1,9 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- *
+ * 
  * @author Sam Reid (PhET Interactive Simulations)
+ * @author Jesse Greenberg
  */
 define( function( require ) {
   'use strict';
@@ -13,9 +14,54 @@ define( function( require ) {
   function VirtualCursor() {
     var selectedElement = null;
 
-    //var goToNextItem = function( element ) {
-    //  element.children
-    //};
+    /**
+     * Get the accessible text for this element.  An element will have accessible text if it contains
+     * accessible markup, of is one of many defined elements that implicitly have accessible text.
+     * 
+     * @param  {DOMElement} element searching through 
+     * @return {string}
+     */
+    var getAccessibleText = function( element ) {
+
+      // filter out structural elements that do not have accessible text
+      if( element.getAttribute( 'class' ) === 'ScreenView' ) {
+        return null;
+      }
+
+      // search for accessibility mark up in the pararllel DOM, these elements have accessible text
+      if( element.getAttribute( 'aria-labelledby' ) ) {
+        var labelledElement = document.getElementById( element.getAttribute( 'aria-labelledby') );
+        return labelledElement.textContent;
+      }
+
+      // search for elememnts in the parallel DOM that will have implicit accessible text without markup
+
+      return null;    
+    };
+
+    /**
+     * Go to next element in the parallel DOM that has accessible text content.  If an element has
+     * text content, it is returned.  Otherwise, 
+     * @param  {[type]} element [description]
+     * @return {[type]}         [description]
+     */
+    var goToNextItem = function( element ) {
+      for( var i = 0; i < element.children.length; i ++ ) {
+        if( getAccessibleText( element.children[i] ) ) {
+          return element.children[i];
+        }
+        else{
+          var nextElement = goToNextItem( element.children[i] );
+
+          if( nextElement === null ) {
+            continue;
+          }
+          else{
+            return nextElement;
+          }
+        }
+      }
+    };
 
     // if the user pressed tab, right or down, move the cursor to the next dom element with accessibility markup
     document.addEventListener( 'keydown', function( k ) {
@@ -23,18 +69,15 @@ define( function( require ) {
         console.log( 'moving forward' );
 
         // TODO: access this once?
-        //var accessibilityDOMElement = document.body.getElementsByClassName( 'accessibility' )[ 0 ];
+        var accessibilityDOMElement = document.body.getElementsByClassName( 'accessibility' )[ 0 ];
         if ( selectedElement === null ) {
-          //selectedElement = goToNextItem( accessibilityDOMElement );
+          selectedElement = goToNextItem( accessibilityDOMElement );
+
+          console.log( getAccessibleText( selectedElement ) );
         }
       }
     } );
 
-
-    //left = 37
-    //up = 38
-    //right = 39
-    //down = 40
   }
 
   return inherit( Object, VirtualCursor, {} );
