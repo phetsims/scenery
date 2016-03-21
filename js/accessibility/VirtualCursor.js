7737123707c42ef4eb8e9dc5555b512f3e65a1d9
samreid <reids@colorado.edu>
2016-03-21 13:40:32 -0600

    Logic fixes for VirtualCursor, see #227

--- js/accessibility/VirtualCursor.js
+++ js/accessibility/VirtualCursor.js
@@ -1,7 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * 
+ *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Jesse Greenberg
  */
@@ -17,46 +17,63 @@ define( function( require ) {
     /**
      * Get the accessible text for this element.  An element will have accessible text if it contains
      * accessible markup, of is one of many defined elements that implicitly have accessible text.
-     * 
-     * @param  {DOMElement} element searching through 
+     *
+     * @param  {DOMElement} element searching through
      * @return {string}
      */
     var getAccessibleText = function( element ) {
 
+      console.log( 'checking', element );
+
       // filter out structural elements that do not have accessible text
-      if( element.getAttribute( 'class' ) === 'ScreenView' ) {
+      if ( element.getAttribute( 'class' ) === 'ScreenView' ) {
         return null;
       }
 
       // search for accessibility mark up in the pararllel DOM, these elements have accessible text
-      if( element.getAttribute( 'aria-labelledby' ) ) {
-        var labelledElement = document.getElementById( element.getAttribute( 'aria-labelledby') );
+      if ( element.getAttribute( 'aria-labelledby' ) ) {
+        var labelledElement = document.getElementById( element.getAttribute( 'aria-labelledby' ) );
         return labelledElement.textContent;
       }
+      if ( element.tagName === 'P' ) {
+        return element.textContent;
+      }
+      if ( element.tagName === 'H2' ) {
+        return element.textContent;
+      }
 
-      // search for elememnts in the parallel DOM that will have implicit accessible text without markup
+      // search for elements in the parallel DOM that will have implicit accessible text without markup
 
-      return null;    
+      return null;
     };
 
     /**
      * Go to next element in the parallel DOM that has accessible text content.  If an element has
-     * text content, it is returned.  Otherwise, 
+     * text content, it is returned.  Otherwise,
      * @param  {[type]} element [description]
      * @return {[type]}         [description]
      */
-    var goToNextItem = function( element ) {
-      for( var i = 0; i < element.children.length; i ++ ) {
-        if( getAccessibleText( element.children[i] ) ) {
-          return element.children[i];
-        }
-        else{
-          var nextElement = goToNextItem( element.children[i] );
+    var goToNextItem = function( element, visited ) {
+      for ( var i = 0; i < element.children.length; i++ ) {
+        if ( getAccessibleText( element.children[ i ] ) ) {
 
-          if( nextElement === null ) {
+          if ( visited ) {
+            return element.children[ i ];
+          }
+          else if ( element.children[ i ] === selectedElement ) {
+
+            // Running the first pass depth-first search from the root has found the previously selected item
+            // so now we can continue the search and return the next focusable item.
+            visited = true;
+          }
+        }
+        else {
+          var nextElement = goToNextItem( element.children[ i ], visited );
+
+          if ( nextElement === null ) {
             continue;
           }
-          else{
+          else {
             return nextElement;
           }
         }
@@ -69,12 +86,19 @@ define( function( require ) {
         console.log( 'moving forward' );
 
         // TODO: access this once?
+        //debugger;
         var accessibilityDOMElement = document.body.getElementsByClassName( 'accessibility' )[ 0 ];
-        if ( selectedElement === null ) {
-          selectedElement = goToNextItem( accessibilityDOMElement );
+        var visited = selectedElement === null;
+        selectedElement = goToNextItem( accessibilityDOMElement, visited );
 
-          console.log( getAccessibleText( selectedElement ) );
+        if ( !selectedElement ) {
+          console.log( '--wrapped' );
+
+          selectedElement = null;
+          visited = selectedElement === null;
+          selectedElement = goToNextItem( accessibilityDOMElement, visited );
         }
+        console.log( getAccessibleText( selectedElement ) );
       }
     } );
 
