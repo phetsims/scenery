fb6a6f837e7e5681f1360b0cbd4c5bfd7b46de18
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-05-02 17:09:27 -0600

    Move TimedQueue and functions out of constructor, see #544

--- js/accessibility/VirtualCursor.js
+++ js/accessibility/VirtualCursor.js
@@ -1,7 +1,19 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
+ * Prototype virtual cursor.  The virtual cursor is typically used by asistive technologies to enable the user
+ * to traverse content on the page. This is meant to prototype a tool that lets the team
+ * and trusted translators 'view' the accessible content for a simulation.  The accessible content exsists in the 
+ * accessible DOM, and the virtual cursor traverses this document.
+ * 
+ * The cursor moves with the 'tab' and 'arrow' keys.  'Tab', 'down arrow', and 'right arrow' move cursor to 
+ * the next item, while 'shift + tab', 'left arrow', and 'up arrow' moves the cursor to the revious item.
  *
+ * This prototype cursor assumes that the simulation is embedded in an iFrame, and the output is to be printed under
+ * the iFrame, see balloons-and-static-electricity/screen-reader.html
+ *
+ * See https://github.com/phetsims/scenery/issues/538
+ * 
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Jesse Greenberg
  */
@@ -12,106 +24,109 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var scenery = require( 'SCENERY/scenery' );
 
+  // constants
+  // flags which are set on visited html elements during traversal
   var DATA_VISITED = 'data-visited';
   var DATA_VISITED_LINEAR = 'data-visited-linear';
 
+  /**
+   * Constructor.
+   */
   function VirtualCursor() {
 
-    /**
-     * A timed queue that can be used to queue descriptions to the text output.  Usually used for
-     * 'aria-live' updates.  If multiple updates are fired at once, they are added to the queue
-     * in the order that they occur.
-     * 
-     * @param {number} defaultDelay - default delay for items in the queue
-     */
-    var TimedQueue = function( defaultDelay ) {
-      Object.call( this );
+    var thisCursor = this;
 
-      // @private - track when we are running so we do not try to run the queue while it is already running
-      this.running = false;
+    // handle traversal of the DOM with keyboard navigation
+    document.addEventListener( 'keydown', function( k ) {
+      var selectedElement = null;
+      var accessibilityDOMElement = document.body.getElementsByClassName( 'accessibility' )[ 0 ];
 
-      // @private - the queue of items to be read, populated with objects like
-      // { callback: {function}, delay: {number} };
-      this.queue = [];
+      // forward traversal
+      if ( k.keyCode === 39 || k.keyCode === 40 || ( k.keyCode === 9 && !k.shiftKey ) ) {
 
-      // @private - current index in the queue
-      this.index = 0;
+        // we do not want to go to the next forms element if tab (yet)
+        k.preventDefault();
 
-      // @private - default delay of five seconds
-      this.defaultDelay = defaultDelay || 5000;
-    };
+        selectedElement = thisCursor.goToNextItem( accessibilityDOMElement, DATA_VISITED );
 
-    inherit( Object, TimedQueue, {
-
-      /**
-       * Add a callback to this queue, with a delay
-       * 
-       * @param {function} callBack - callback fired by this addition
-       * @param {number} [delay]- optional delay for this item
-       */
-      add: function( callBack, delay ) {
-        var thisQueue = this;
-        this.queue.push( {
-          callBack: callBack,
-          delay: delay || thisQueue.defaultDelay
-        } );
-      },
-
-      /**
-       * Run through items in the queue, starting at index
-       * @param  {number} index
-       */
-      run: function( index ) {
-        if ( !this.running ) {
-          this.index = index || 0;
-          this.next();
-        }
-      },
-
-      /**
-       * Remove all items from the queue
-       */
-      clear: function() {
-        this.queue = [];
-      },
-
-      /**
-       * Get the next item in the queue, then delaying after firing callback
-       */
-      next: function() {
-
-        this.running = true;
-        var thisQueue = this;
-        var i = this.index++;
-
-        var active = this.queue[i];
-        var next = this.queue[ this.index ];
-
-        // return and set running flag to false if there are no items in the queue
-        var endRun = function() {
-          thisQueue.running = false;
-          thisQueue.clear();
-        };
-
-        if( !active ) {
-          endRun();
-          return;
-        }
-
-        // fire the callback function
-        active.callBack();
-
-        if( next ) {
-          setTimeout( function() {
-            thisQueue.next();
-          }, active.delay || thisQueue.defaultDelay );
-        }
-        else {
-          endRun();
-          return;
+        if ( !selectedElement ) {
+          thisCursor.clearVisited( accessibilityDOMElement, DATA_VISITED );
+          selectedElement = thisCursor.goToNextItem( accessibilityDOMElement, DATA_VISITED );
         }
       }
+
+      // backwards traversal
+      else if ( k.keyCode === 37 || k.keyCode === 38 || ( k.shiftKey && k.keyCode === 9 ) ) {
+
+        // we do not want to go to the next forms element if tab (yet)
+        k.preventDefault();
+
+        var listOfAccessibleElements = thisCursor.getLinearDOM( accessibilityDOMElement );
+
+        var foundAccessibleText = false;
+        for ( var i = listOfAccessibleElements.length - 1; i >= 0; i-- ) {
+          if ( listOfAccessibleElements[ i ].getAttribute( DATA_VISITED ) ) {
+            listOfAccessibleElements[ i ].removeAttribute( DATA_VISITED );
+
+            if ( i !== 0 ) {
+              selectedElement = listOfAccessibleElements[ i - 1 ];
+              foundAccessibleText = true;
+            }
+            else {
+              selectedElement = null;
+              foundAccessibleText = false;
+            }
+
+            break;
+          }
+        }
+
+        // Wrap backwards, going from the end of the linearized DOM
+        if ( !foundAccessibleText ) {
+          selectedElement = listOfAccessibleElements[ listOfAccessibleElements.length - 1 ];
+
+          for ( i = 0; i < listOfAccessibleElements.length - 1; i++ ) {
+            listOfAccessibleElements[ i ].setAttribute( DATA_VISITED, true );
+          }
+        }
+      }
+
+      // if the element has a tabindex or is a button, navigation with the virtual cursor will set
+      // it as the activeElement
+      if ( selectedElement ) {
+        if ( selectedElement.getAttribute( 'tabindex' ) || selectedElement.tagName === 'BUTTON' ) {
+          selectedElement.focus();
+        }
+
+        // print the output to the iFrame
+        var accessibleText = thisCursor.getAccessibleText( selectedElement );
+        parent && parent.updateAccessibilityReadoutText && parent.updateAccessibilityReadoutText( accessibleText );
+      }
     } );
+
+    // create a queue for aria live textcontent changes
+    // changes to elements with aria-live text content will be added to queue by mutation observers
+    this.ariaLiveQueue = new TimedQueue( 5000 );
+
+    // look for new live elements and add listeners to them every five seconds
+    // TODO: Is there a better way to search through the DOM for new aria-live elements?
+    var searchForLiveElements = function() {
+      thisCursor.updateLiveElementList();
+      setTimeout( searchForLiveElements, 5000 );
+    };
+    searchForLiveElements();
+
+    // run through the active queue of aria live elements
+    var step = function() {
+      thisCursor.ariaLiveQueue.run();
+      setTimeout( step, 100 );
+    };
+    step();
+  }
+
+  scenery.register( 'VirtualCursor', VirtualCursor );
+
+  inherit( Object, VirtualCursor, {
 
     /**
      * Get the accessible text for this element.  An element will have accessible text if it contains
@@ -119,8 +134,9 @@ define( function( require ) {
      *
      * @param  {DOMElement} element searching through
      * @return {string}
+     * @private
      */
-    var getAccessibleText = function( element ) {
+    getAccessibleText: function( element ) {
 
       // filter out structural elements that do not have accessible text
       if ( element.getAttribute( 'class' ) === 'ScreenView' ) {
@@ -160,36 +176,7 @@ define( function( require ) {
       }
 
       return null;
-    };
-
-    var clearVisited = function( element, visitedFlag ) {
-      element.removeAttribute( visitedFlag );
-      for ( var i = 0; i < element.children.length; i++ ) {
-        clearVisited( element.children[ i ], visitedFlag );
-      }
-    };
-
-    /**
-     * Go to next element in the parallel DOM that has accessible text content.  If an element has
-     * text content, it is returned.  Otherwise,
-     * @param {DOMElement} element
-     * @return {string} visitedFlag - a flag for which 'data-*' attribute to set as we search through the tree
-     */
-    var goToNextItem = function( element, visitedFlag ) {
-      if ( getAccessibleText( element ) ) {
-        if ( !element.getAttribute( visitedFlag ) ) {
-          element.setAttribute( visitedFlag, true );
-          return element;
-        }
-      }
-      for ( var i = 0; i < element.children.length; i++ ) {
-        var nextElement = goToNextItem( element.children[ i ], visitedFlag );
-
-        if ( nextElement ) {
-          return nextElement;
-        }
-      }
-    };
+    },
 
     /**
      * Get all 'element' nodes of the parent element, placing them in an array for easy traversal
@@ -199,9 +186,10 @@ define( function( require ) {
      * 
      * @param  {DOMElement} element - parent element for which you want an array of all children
      * @return {array<DOMElement>}
+     * @private
      */
-    var getLinearDOMElements = function( element ) {
-      // gets all descendent children for the element
+    getLinearDOMElements: function( element ) {
+      // gets all descendent children for the element 
       var children = element.getElementsByTagName( '*' );
       var linearDOM = [];
       for( var i = 0; i < children.length; i++ ) {
@@ -210,7 +198,7 @@ define( function( require ) {
         }
       }
       return linearDOM;
-    };
+    },
 
     /**
      * Get a 'linear' representation of the DOM, collapsing the accessibility tree into an array that
@@ -218,95 +206,56 @@ define( function( require ) {
      *
      * @param {DOMElement} element
      * @return {array<DOMElement>} linearDOM
+     * @private
      */
-    var getLinearDOM = function( element ) {
+    getLinearDOM: function( element ) {
 
-      clearVisited( element, DATA_VISITED_LINEAR );
+      this.clearVisited( element, DATA_VISITED_LINEAR );
 
       var linearDOM = [];
 
-      var nextElement = goToNextItem( element, DATA_VISITED_LINEAR );
+      var nextElement = this.goToNextItem( element, DATA_VISITED_LINEAR );
       while ( nextElement ) {
         linearDOM.push( nextElement );
-        nextElement = goToNextItem( element, DATA_VISITED_LINEAR );
+        nextElement = this.goToNextItem( element, DATA_VISITED_LINEAR );
       }
 
       return linearDOM;
-    };
+    },
 
-    // if the user presses right or down, move the cursor to the next dom element with accessibility markup
-    // It will be difficult to synchronize the virtual cursor with tab navigation.  For now, tab/shift+tab
-    // will take us to the next/previos description.
-    document.addEventListener( 'keydown', function( k ) {
-      var selectedElement = null;
-      var accessibilityDOMElement = document.body.getElementsByClassName( 'accessibility' )[ 0 ];
-
-      // forward traversal
-      if ( k.keyCode === 39 || k.keyCode === 40 || ( k.keyCode === 9 && !k.shiftKey ) ) {
-
-        // we do not want to go to the next forms element if tab (yet)
-        k.preventDefault();
-
-        selectedElement = goToNextItem( accessibilityDOMElement, DATA_VISITED );
-
-        if ( !selectedElement ) {
-          clearVisited( accessibilityDOMElement, DATA_VISITED );
-          selectedElement = goToNextItem( accessibilityDOMElement, DATA_VISITED );
+    /**
+     * Go to next element in the parallel DOM that has accessible text content.  If an element has
+     * text content, it is returned.
+     * 
+     * @param {DOMElement} element
+     * @return {string} visitedFlag - a flag for which 'data-*' attribute to set as we search through the tree
+     * @private
+     */
+    goToNextItem: function( element, visitedFlag ) {
+      if ( this.getAccessibleText( element ) ) {
+        if ( !element.getAttribute( visitedFlag ) ) {
+          element.setAttribute( visitedFlag, true );
+          return element;
         }
       }
+      for ( var i = 0; i < element.children.length; i++ ) {
+        var nextElement = this.goToNextItem( element.children[ i ], visitedFlag );
 
-      // backwards traversal on up/left/shift+tab
-      else if ( k.keyCode === 37 || k.keyCode === 38 || ( k.shiftKey && k.keyCode === 9 ) ) {
-
-        // we do not want to go to the next forms element if tab (yet)
-        k.preventDefault();
-
-        var listOfAccessibleElements = getLinearDOM( accessibilityDOMElement );
-
-        var foundAccessibleText = false;
-        for ( var i = listOfAccessibleElements.length - 1; i >= 0; i-- ) {
-          if ( listOfAccessibleElements[ i ].getAttribute( DATA_VISITED ) ) {
-            listOfAccessibleElements[ i ].removeAttribute( DATA_VISITED );
-
-            if ( i !== 0 ) {
-              selectedElement = listOfAccessibleElements[ i - 1 ];
-              foundAccessibleText = true;
-            }
-            else {
-              selectedElement = null;
-              foundAccessibleText = false;
-            }
-
-            break;
-          }
-        }
-
-        // Wrap backwards, going from the end of the linearized DOM
-        if ( !foundAccessibleText ) {
-          selectedElement = listOfAccessibleElements[ listOfAccessibleElements.length - 1 ];
-
-          for ( i = 0; i < listOfAccessibleElements.length - 1; i++ ) {
-            listOfAccessibleElements[ i ].setAttribute( DATA_VISITED, true );
-          }
+        if ( nextElement ) {
+          return nextElement;
         }
       }
+    },
 
-      if ( selectedElement ) {
-        if ( selectedElement.getAttribute( 'tabindex' ) || selectedElement.tagName === 'BUTTON' ) {
-          selectedElement.focus();
-        }
+    /**
+     * Update the list of live elements, and add a MutationObserver to any element that has an attribute
+     * for aria-live.
+     *
+     * @private
+     */
+    updateLiveElementList: function() {
 
-        // print the output to the iFrame
-        var accessibleText = getAccessibleText( selectedElement );
-        parent && parent.updateAccessibilityReadoutText && parent.updateAccessibilityReadoutText( accessibleText );
-      }
-    } );
-
-    // create a queue for aria live textcontent changes
-    // changes to elements with aria-live text content will be added to queue by element mutation observers
-    var ariaLiveQueue = new TimedQueue( 5000 );
-
-    var updateLiveElementList = function() {
+      var thisCursor = this;
 
       // remove all observers from live elements to prevent a memory leak
       if( window.liveElementList ) {
@@ -322,7 +271,7 @@ define( function( require ) {
 
       // get a linear representation of the DOM
       var accessibilityDOMElement = document.body.getElementsByClassName( 'accessibility' )[ 0 ];
-      var linearDOM = getLinearDOMElements( accessibilityDOMElement );
+      var linearDOM = this.getLinearDOMElements( accessibilityDOMElement );
 
       // search the DOM for elements with 'aria-live' attributes
       for( var i = 0; i < linearDOM.length; i++ ) {
@@ -333,8 +282,7 @@ define( function( require ) {
           var observer = new MutationObserver( function( mutations ) {
             mutations.forEach( function( mutation ) {
               var updatedText = mutation.addedNodes[0].data;
-              ariaLiveQueue.add( function() {
-                console.log( updatedText );
+              thisCursor.ariaLiveQueue.add( function() {
                 parent &&
                   parent.updateAccessibilityReadoutText &&
                     parent.updateAccessibilityReadoutText( updatedText );
@@ -354,26 +302,125 @@ define( function( require ) {
           observer.observe( domElement, observerConfig );
         }        
       }
-    };
+    },
 
-    // look for new live elements and add listeners to them every five seconds
-    // TODO: Is there a better way to search through the DOM for new aria-live elements?
-    var searchForLiveElements = function() {
-      updateLiveElementList();
-      setTimeout( searchForLiveElements, 5000 );
-    };
-    searchForLiveElements();
+    /**
+     * Clear the visited flag from all child elements under the parent element
+     * 
+     * @param  {DOMElememt} element
+     * @param  {string} visitedFlag
+     * @private
+     */
+    clearVisited: function( element, visitedFlag ) {
+      element.removeAttribute( visitedFlag );
+      for ( var i = 0; i < element.children.length; i++ ) {
+        this.clearVisited( element.children[ i ], visitedFlag );
+      }
+    }
 
-    // run through the active queue of aria live elements
-    var step = function() {
-      ariaLiveQueue.run();
-      setTimeout( step, 100 );
-    };
-    step();
+  } );
+
+  /**
+   * A timed queue that can be used to queue descriptions to the text output.  Usually used for
+   * 'aria-live' updates.  If multiple updates are fired at once, they are added to the queue
+   * in the order that they occur.
+   * 
+   * @param {number} defaultDelay - default delay for items in the queue
+   */
+  function TimedQueue( defaultDelay ) {
+
+    // @private - track when we are running so we do not try to run the queue while it is already running
+    this.running = false;
+
+    // @private - the queue of items to be read, populated with objects like
+    // { callback: {function}, delay: {number} };
+    this.queue = [];
+
+    // @private - current index in the queue
+    this.index = 0;
+
+    // @private - default delay of five seconds
+    this.defaultDelay = defaultDelay || 5000;
   }
 
-  scenery.register( 'VirtualCursor', VirtualCursor );
+  scenery.register( 'TimedQueue', TimedQueue );
 
-  return inherit( Object, VirtualCursor, {} );
+  inherit( Object, TimedQueue, {
+
+    /**
+     * Add a callback to this queue, with a delay
+     * 
+     * @param {function} callBack - callback fired by this addition
+     * @param {number} [delay]- optional delay for this item
+     * @public
+     */
+    add: function( callBack, delay ) {
+      var thisQueue = this;
+      this.queue.push( {
+        callBack: callBack,
+        delay: delay || thisQueue.defaultDelay
+      } );
+    },
+
+    /**
+     * Run through items in the queue, starting at index
+     * @param  {number} index
+     * @public
+     */
+    run: function( index ) {
+      if ( !this.running ) {
+        this.index = index || 0;
+        this.next();
+      }
+    },
+
+    /**
+     * Remove all items from the queue
+     * @public
+     */
+    clear: function() {
+      this.queue = [];
+    },
+
+    /**
+     * Get the next item in the queue, then delaying after firing callback
+     * @public
+     */
+    next: function() {
+
+      this.running = true;
+      var thisQueue = this;
+      var i = this.index++;
+
+      var active = this.queue[i];
+      var next = this.queue[ this.index ];
+
+      // return and set running flag to false if there are no items in the queue
+      var endRun = function() {
+        thisQueue.running = false;
+        thisQueue.clear();
+      };
+
+      if( !active ) {
+        endRun();
+        return;
+      }
+
+      // fire the callback function
+      active.callBack();
+
+      if( next ) {
+        setTimeout( function() {
+          thisQueue.next();
+        }, active.delay || thisQueue.defaultDelay );
+      }
+      else {
+        endRun();
+        return;
+      }
+    }
+  } );
+
+  return VirtualCursor;
 
 } );
