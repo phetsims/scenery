d45245018708110623248789a7b8406d389bbd2f
Jonathan Olson <olsonsjc@gmail.com>
2016-12-02 22:16:27 -0700

    Adding in options for horizontal-only or vertical-only matching for AlignGroup.

--- js/nodes/AlignGroup.js
+++ js/nodes/AlignGroup.js
@@ -35,10 +35,26 @@ define( function( require ) {
    * group to dispose all of them.
    *
    * It is also possible to create AlignBox instances independently and assign their 'group' to this AlignGroup.
+   *
+   * @param {Object} [options]
    */
-  function AlignGroup() {
+  function AlignGroup( options ) {
+    options = _.extend( {
+      matchHorizontal: true, // {boolean} - Whether the boxes should have all matching widths (otherwise it fits to size)
+      matchVertical: true  // {boolean} - Whether the boxes should have all matching heights (otherwise it fits to size)
+    }, options );
+
+    assert && assert( typeof options.matchHorizontal === 'boolean' );
+    assert && assert( typeof options.matchVertical === 'boolean' );
+
     // @private {Array.<AlignBox>}
     this._alignBoxes = [];
+
+    // @private {boolean}
+    this._matchHorizontal = options.matchHorizontal;
+
+    // @private {boolean}
+    this._matchVertical = options.matchVertical;
 
     // @private {boolean} - Gets locked when certain layout is performed.
     this._resizeLock = false;
@@ -63,6 +79,72 @@ define( function( require ) {
         group: this
       }, options ) );
     },
+
+    /**
+     * Sets whether the widths of the align boxes should all match. If false, each box will use its preferred width
+     * (usually equal to the content width + horizontal margins).
+     * @public
+     *
+     * @param {boolean} matchHorizontal
+     * @returns {AlignGroup} - For chaining
+     */
+    setMatchHorizontal: function( matchHorizontal ) {
+      assert && assert( typeof matchHorizontal === 'boolean' );
+
+      if ( this._matchHorizontal !== matchHorizontal ) {
+        this._matchHorizontal = matchHorizontal;
+
+        // Update layout, since it will probably change
+        this.updateLayout();
+      }
+
+      return this;
+    },
+    set matchHorizontal( value ) { this.setMatchHorizontal( value ); },
+
+    /**
+     * Returns whether boxes currently are horizontally matched. See setMatchHorizontal() for details.
+     * @public
+     *
+     * @returns {boolean}
+     */
+    getMatchHorizontal: function() {
+      return this._matchHorizontal;
+    },
+    get matchHorizontal() { return this.getMatchHorizontal(); },
+
+    /**
+     * Sets whether the heights of the align boxes should all match. If false, each box will use its preferred height
+     * (usually equal to the content height + vertical margins).
+     * @public
+     *
+     * @param {boolean} matchVertical
+     * @returns {AlignGroup} - For chaining
+     */
+    setMatchVertical: function( matchVertical ) {
+      assert && assert( typeof matchVertical === 'boolean' );
+
+      if ( this._matchVertical !== matchVertical ) {
+        this._matchVertical = matchVertical;
+
+        // Update layout, since it will probably change
+        this.updateLayout();
+      }
+
+      return this;
+    },
+    set matchVertical( value ) { this.setMatchVertical( value ); },
+
+    /**
+     * Returns whether boxes currently are vertically matched. See setMatchVertical() for details.
+     * @public
+     *
+     * @returns {boolean}
+     */
+    getMatchVertical: function() {
+      return this._matchVertical;
+    },
+    get matchVertical() { return this.getMatchVertical(); },
 
     /**
      * Dispose all of the boxes.
@@ -104,14 +186,48 @@ define( function( require ) {
 
 
       if ( maxWidth > 0 && maxHeight > 0 ) {
-        var alignBounds = new Bounds2( 0, 0, maxWidth, maxHeight );
         // Apply that maximum dimension for each alignBox
         for ( i = 0; i < this._alignBoxes.length; i++ ) {
-          this._alignBoxes[ i ].alignBounds = alignBounds;
+          this.setBoxBounds( this._alignBoxes[ i ], maxWidth, maxHeight );
         }
       }
 
       this._resizeLock = false;
+    },
+
+    /**
+     * Sets a box's bounds based on our maximum dimensions.
+     * @private
+     *
+     * @param {AlignBox} alignBox
+     * @param {number} maxWidth
+     * @param {number} maxHeight
+     */
+    setBoxBounds: function( alignBox, maxWidth, maxHeight ) {
+      var alignBounds;
+
+      // If we match both dimensions, we don't have to inspect the box's preferred size
+      if ( this._matchVertical && this._matchHorizontal ) {
+        alignBounds = new Bounds2( 0, 0, maxWidth, maxHeight );
+      }
+      else {
+        // Grab the preferred size
+        var contentBounds = alignBox.getContentBounds();
+
+        // Match one orientation
+        if ( this._matchVertical ) {
+          alignBounds = new Bounds2( 0, 0, contentBounds.width, maxHeight );
+        }
+        else if ( this._matchHorizontal ) {
+          alignBounds = new Bounds2( 0, 0, maxWidth, contentBounds.height );
+        }
+        // If not matching anything, just use its preferred size
+        else {
+          alignBounds = contentBounds;
+        }
+      }
+
+      alignBox.alignBounds = alignBounds;
     },
 
     /**
--- tests/qunit/js/alignment.js
+++ tests/qunit/js/alignment.js
@@ -5,108 +5,108 @@
 
   module( 'Scenery: AlignBox/AlignGroup' );
 
-  test( 'Single Container', function() {
+  test( 'Single Box Group', function() {
     var circle = new scenery.Circle( 20 );
 
     var group = new scenery.AlignGroup();
-    var container = group.createBox( circle, {
+    var box = group.createBox( circle, {
       xMargin: 10,
       yMargin: 20
     } );
 
-    ok( container.xMargin === 10, 'xMargin' );
-    ok( container.yMargin === 20, 'yMargin' );
-    ok( container.xAlign === 'center', 'xAlign' );
-    ok( container.yAlign === 'center', 'yAlign' );
+    ok( box.xMargin === 10, 'xMargin' );
+    ok( box.yMargin === 20, 'yMargin' );
+    ok( box.xAlign === 'center', 'xAlign' );
+    ok( box.yAlign === 'center', 'yAlign' );
 
-    ok( container.bounds.equals( new dot.Bounds2( 0, 0, 60, 80 ) ), 'Margins' );
+    ok( box.bounds.equals( new dot.Bounds2( 0, 0, 60, 80 ) ), 'Margins' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 50, 60 ) ), 'Circle: Margins' );
 
     circle.radius = 10; circle.getBounds(); // trigger bounds check
-    ok( container.bounds.equals( new dot.Bounds2( 0, 0, 40, 60 ) ), 'Change to the content size' );
+    ok( box.bounds.equals( new dot.Bounds2( 0, 0, 40, 60 ) ), 'Change to the content size' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Change to the content size' );
 
     circle.x = 100; circle.getBounds(); // trigger bounds check
-    ok( container.bounds.equals( new dot.Bounds2( 0, 0, 40, 60 ) ), 'Reposition on content location change' );
+    ok( box.bounds.equals( new dot.Bounds2( 0, 0, 40, 60 ) ), 'Reposition on content location change' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Reposition on content location change' );
 
     circle.scale( 2 ); circle.getBounds(); // trigger bounds check
-    ok( container.bounds.equals( new dot.Bounds2( 0, 0, 60, 80 ) ), 'Handle scaling' );
+    ok( box.bounds.equals( new dot.Bounds2( 0, 0, 60, 80 ) ), 'Handle scaling' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 50, 60 ) ), 'Circle: Handle scaling' );
 
-    container.xMargin = 0; circle.getBounds(); // trigger bounds check
-    ok( container.bounds.equals( new dot.Bounds2( 0, 0, 40, 80 ) ), 'xMargin change' );
+    box.xMargin = 0; circle.getBounds(); // trigger bounds check
+    ok( box.bounds.equals( new dot.Bounds2( 0, 0, 40, 80 ) ), 'xMargin change' );
     ok( circle.bounds.equals( new dot.Bounds2( 0, 20, 40, 60 ) ), 'Circle: xMargin change' );
 
     group.dispose();
   } );
 
-  test( 'Multiple Containers With Alignment', function() {
+  test( 'Multiple Boxes in a Group', function() {
     var circle = new scenery.Circle( 10 );
     var rectangle = new scenery.Rectangle( 0, 0, 60, 60 );
 
     var group = new scenery.AlignGroup();
 
-    var circleContainer = group.createBox( circle, {
+    var circleBox = group.createBox( circle, {
       xMargin: 10,
       yMargin: 20,
       xAlign: 'left',
       yAlign: 'bottom'
     } );
 
-    var rectangleContainer = group.createBox( rectangle );
+    var rectangleBox = group.createBox( rectangle );
 
-    ok( circleContainer.xMargin === 10, 'circle: xMargin' );
-    ok( circleContainer.yMargin === 20, 'circle: yMargin' );
-    ok( circleContainer.xAlign === 'left', 'circle: xAlign' );
-    ok( circleContainer.yAlign === 'bottom', 'circle: yAlign' );
+    ok( circleBox.xMargin === 10, 'circle: xMargin' );
+    ok( circleBox.yMargin === 20, 'circle: yMargin' );
+    ok( circleBox.xAlign === 'left', 'circle: xAlign' );
+    ok( circleBox.yAlign === 'bottom', 'circle: yAlign' );
 
-    ok( rectangleContainer.xMargin === 0, 'rectangle: xMargin' );
-    ok( rectangleContainer.yMargin === 0, 'rectangle: yMargin' );
-    ok( rectangleContainer.xAlign === 'center', 'rectangle: xAlign' );
-    ok( rectangleContainer.yAlign === 'center', 'rectangle: yAlign' );
+    ok( rectangleBox.xMargin === 0, 'rectangle: xMargin' );
+    ok( rectangleBox.yMargin === 0, 'rectangle: yMargin' );
+    ok( rectangleBox.xAlign === 'center', 'rectangle: xAlign' );
+    ok( rectangleBox.yAlign === 'center', 'rectangle: yAlign' );
 
     // circle: 20x20, with margin: 40x60
     // rectangle: 60x60 (max in both)
-    ok( circleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 60 ) ), 'Circle Container: Initial multiple' );
-    ok( rectangleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 60 ) ), 'Rectangle Container: Initial multiple' );
+    ok( circleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 60 ) ), 'Circle Container: Initial multiple' );
+    ok( rectangleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 60 ) ), 'Rectangle Container: Initial multiple' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Initial multiple' );
     ok( rectangle.bounds.equals( new dot.Bounds2( 0, 0, 60, 60 ) ), 'Rectangle: Initial multiple' );
 
-    rectangleContainer.yMargin = 20; circleContainer.getBounds(); rectangleContainer.getBounds(); // trigger check
+    rectangleBox.yMargin = 20; circleBox.getBounds(); rectangleBox.getBounds(); // trigger check
     // circle: 20x20, with margin: 40x60
     // rectangle: 60x60, with margin: 60x100
-    ok( circleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: Align Change Rect' );
-    ok( rectangleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: Align Change Rect' );
+    ok( circleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: Align Change Rect' );
+    ok( rectangleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: Align Change Rect' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 60, 30, 80 ) ), 'Circle: Align Change Rect' );
     ok( rectangle.bounds.equals( new dot.Bounds2( 0, 20, 60, 80 ) ), 'Rectangle: Align Change Rect' );
 
-    circleContainer.yAlign = 'top'; circleContainer.getBounds(); rectangleContainer.getBounds(); // trigger check
+    circleBox.yAlign = 'top'; circleBox.getBounds(); rectangleBox.getBounds(); // trigger check
     // circle: 20x20, with margin: 40x60
     // rectangle: 60x60, with margin: 60x100
-    ok( circleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: Align Change Circle' );
-    ok( rectangleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: Align Change Circle' );
+    ok( circleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: Align Change Circle' );
+    ok( rectangleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: Align Change Circle' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Align Change Circle' );
     ok( rectangle.bounds.equals( new dot.Bounds2( 0, 20, 60, 80 ) ), 'Rectangle: Align Change Circle' );
 
-    rectangleContainer.dispose(); circleContainer.getBounds(); // trigger check
+    rectangleBox.dispose(); circleBox.getBounds(); // trigger check
     // circle: 20x20, with margin: 40x60
-    ok( circleContainer.bounds.equals( new dot.Bounds2( 0, 0, 40, 60 ) ), 'Circle Container: Removed Rect Container' );
+    ok( circleBox.bounds.equals( new dot.Bounds2( 0, 0, 40, 60 ) ), 'Circle Container: Removed Rect Container' );
     ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Removed Rect Container' );
 
-    rectangleContainer = group.createBox( rectangle, { yMargin: 20 } ); circleContainer.getBounds(); rectangleContainer.getBounds(); // trigger check
+    rectangleBox = group.createBox( rectangle, { yMargin: 20 } ); circleBox.getBounds(); rectangleBox.getBounds(); // trigger check
     // circle: 20x20, with margin: 40x60
     // rectangle: 60x60, with margin: 60x100
-    ok( circleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: Added back container' );
-    ok( rectangleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: Added back container' );
-    ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Added back container' );
-    ok( rectangle.bounds.equals( new dot.Bounds2( 0, 20, 60, 80 ) ), 'Rectangle: Added back container' );
+    ok( circleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: Added back box' );
+    ok( rectangleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: Added back box' );
+    ok( circle.bounds.equals( new dot.Bounds2( 10, 20, 30, 40 ) ), 'Circle: Added back box' );
+    ok( rectangle.bounds.equals( new dot.Bounds2( 0, 20, 60, 80 ) ), 'Rectangle: Added back box' );
 
-    circleContainer.xAlign = 'right'; circleContainer.getBounds(); rectangleContainer.getBounds(); // trigger check
+    circleBox.xAlign = 'right'; circleBox.getBounds(); rectangleBox.getBounds(); // trigger check
     // circle: 20x20, with margin: 40x60
     // rectangle: 60x60, with margin: 60x100
-    ok( circleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: More circle xAlign:right' );
-    ok( rectangleContainer.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: More circle xAlign:right' );
+    ok( circleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'Circle Container: More circle xAlign:right' );
+    ok( rectangleBox.bounds.equals( new dot.Bounds2( 0, 0, 60, 100 ) ), 'RectangleContainer: More circle xAlign:right' );
     ok( circle.bounds.equals( new dot.Bounds2( 30, 20, 50, 40 ) ), 'Circle: More circle xAlign:right' );
     ok( rectangle.bounds.equals( new dot.Bounds2( 0, 20, 60, 80 ) ), 'Rectangle: More circle xAlign:right' );
 
