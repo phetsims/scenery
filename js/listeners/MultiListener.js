a2ca26c8dd184dd7e20884c520bacf64a8062f9b
Jonathan Olson <olsonsjc@gmail.com>
2017-02-15 03:44:27 -0600

    Improving multitouch listener prototype

--- js/listeners/MultiListener.js
+++ js/listeners/MultiListener.js
@@ -13,6 +13,10 @@ define( function( require ) {
 
   var inherit = require( 'PHET_CORE/inherit' );
   var arrayRemove = require( 'PHET_CORE/arrayRemove' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var Matrix = require( 'DOT/Matrix' );
+  var SingularValueDecomposition = require( 'DOT/SingularValueDecomposition' );
+  var Matrix3 = require( 'DOT/Matrix3' );
   var scenery = require( 'SCENERY/scenery' );
 
   /**
@@ -25,12 +29,17 @@ define( function( require ) {
 
     options = _.extend( {
       mouseButton: 0, // TODO: see PressListener
-
       pressCursor: 'pointer', // TODO: see PressListener
+      targetNode: null, // TODO: required? pass in at front
+      allowScale: true,
+      allowRotation: true
     }, options );
 
     this._mouseButton = options.mouseButton;
     this._pressCursor = options.pressCursor;
+    this._targetNode = options.targetNode;
+    this._allowScale = options.allowScale;
+    this._allowRotation = options.allowRotation;
 
     // @private {Array.<Press>}
     this._presses = [];
@@ -80,39 +89,191 @@ define( function( require ) {
 
       if ( event.pointer.isMouse && event.domEvent.button !== this._mouseButton ) { return; }
 
-      this.addPress( new Press( event.pointer ) );
+      this.addPress( new Press( event.pointer, event.trail.subtrailTo( event.currentTarget, false ) ) );
     },
 
     addPress: function( press ) {
       this._presses.push( press );
 
       press.pointer.cursor = this._pressCursor;
-      press.pointer.addListener( this._pressListener );
+      press.pointer.addInputListener( this._pressListener );
+
+      this.reposition();
 
       // TODO: handle interrupted
     },
 
     movePress: function( press ) {
-
+      this.reposition();
     },
 
     removePress: function( press ) {
-      press.pointer.removeListener( this._pressListener );
+      press.pointer.removeInputListener( this._pressListener );
       press.pointer.cursor = null;
 
       arrayRemove( this._presses, press );
+
+      this.reposition();
+    },
+
+    reposition: function() {
+      this._targetNode.matrix = this.computeMatrix();
+    },
+
+    interrupt: function() {
+      while ( this._presses.length ) {
+        this.removePress( this._presses[ this._presses.length - 1 ] );
+      }
+    },
+
+    computeMatrix: function() {
+      if ( this._presses.length === 0 ) {
+        return this._targetNode.getMatrix();
+      }
+      else if ( this._presses.length === 1 ) {
+        return this.computeSinglePressMatrix();
+      }
+      else if ( this._allowScale && this._allowRotation ) {
+        return this.computeTranslationRotationScaleMatrix();
+      } else if ( this._allowScale ) {
+        return this.computeTranslationScaleMatrix();
+      } else if ( this._allowRotation ) {
+        return this.computeTranslationRotationMatrix();
+      } else {
+        return this.computeTranslationMatrix();
+      }
+    },
+
+    computeSinglePressMatrix: function() {
+      // TODO: scratch things
+      var singleTargetPoint = this._presses[ 0 ].targetPoint;
+      var singleMappedPoint = this._targetNode.localToParentPoint( this._presses[ 0 ].localPoint );
+      var delta = singleTargetPoint.minus( singleMappedPoint );
+      return Matrix3.translationFromVector( delta ).timesMatrix( this._targetNode.getMatrix() );
+    },
+
+    computeTranslationMatrix: function() {
+      // translation only. linear least-squares simplifies to sum of differences
+      var sum = new Vector2();
+      for ( var i = 0; i < this._presses.length; i++ ) {
+        sum.add( this._presses[ i ].targetPoint );
+        sum.subtract( this._presses[ i ].localPoint );
+      }
+      return Matrix3.translationFromVector( sum.dividedScalar( this._presses.length ) );
+    },
+
+    computeTranslationScaleMatrix: function() {
+      // TODO: minimize closures
+      var localPoints = this._presses.map( function( press ) { return press.localPoint; } );
+      var targetPoints = this._presses.map( function( press ) { return press.targetPoint; } );
+
+      var localCentroid = new Vector2();
+      var targetCentroid = new Vector2();
+
+      localPoints.forEach( function( localPoint ) { localCentroid.add( localPoint ); } );
+      targetPoints.forEach( function( targetPoint ) { targetCentroid.add( targetPoint ); } );
+
+      localCentroid.divideScalar( this._presses.length );
+      targetCentroid.divideScalar( this._presses.length );
+
+      var localSquaredDistance = 0;
+      var targetSquaredDistance = 0;
+
+      localPoints.forEach( function( localPoint ) { localSquaredDistance += localPoint.distanceSquared( localCentroid ); } );
+      targetPoints.forEach( function( targetPoint ) { targetSquaredDistance += targetPoint.distanceSquared( targetCentroid ); } );
+
+      var scale = Math.sqrt( targetSquaredDistance / localSquaredDistance );
+
+      var translateToTarget = Matrix3.translation( targetCentroid.x, targetCentroid.y );
+      var translateFromLocal = Matrix3.translation( -localCentroid.x, -localCentroid.y );
+
+      return translateToTarget.timesMatrix( Matrix3.scaling( scale ) ).timesMatrix( translateFromLocal );
+    },
+
+    computeTranslationRotationMatrix: function() {
+      var i;
+      var localMatrix = new Matrix( 2, this._presses.length );
+      var targetMatrix = new Matrix( 2, this._presses.length );
+      var localCentroid = new Vector2();
+      var targetCentroid = new Vector2();
+      for ( i = 0; i < this._presses.length; i++ ) {
+        var localPoint = this._presses[ i ].localPoint;
+        var targetPoint = this._presses[ i ].targetPoint;
+        localCentroid.add( localPoint );
+        targetCentroid.add( targetPoint );
+        localMatrix.set( 0, i, localPoint.x );
+        localMatrix.set( 1, i, localPoint.y );
+        targetMatrix.set( 0, i, targetPoint.x );
+        targetMatrix.set( 1, i, targetPoint.y );
+      }
+      localCentroid.divideScalar( this._presses.length );
+      targetCentroid.divideScalar( this._presses.length );
+
+      // determine offsets from the centroids
+      for ( i = 0; i < this._presses.length; i++ ) {
+        localMatrix.set( 0, i, localMatrix.get( 0, i ) - localCentroid.x );
+        localMatrix.set( 1, i, localMatrix.get( 1, i ) - localCentroid.y );
+        targetMatrix.set( 0, i, targetMatrix.get( 0, i ) - targetCentroid.x );
+        targetMatrix.set( 1, i, targetMatrix.get( 1, i ) - targetCentroid.y );
+      }
+      var covarianceMatrix = localMatrix.times( targetMatrix.transpose() );
+      var svd = new SingularValueDecomposition( covarianceMatrix );
+      var rotation = svd.getV().times( svd.getU().transpose() );
+      if ( rotation.det() < 0 ) {
+        rotation = svd.getV().times( Matrix.diagonalMatrix( [ 1, -1 ] ) ).times( svd.getU().transpose() );
+      }
+      var rotation3 = new Matrix3().rowMajor( rotation.get( 0, 0 ), rotation.get( 0, 1 ), 0,
+                                              rotation.get( 1, 0 ), rotation.get( 1, 1 ), 0,
+                                              0, 0, 1 );
+      var translation = targetCentroid.minus( rotation3.timesVector2( localCentroid ) );
+      rotation3.set02( translation.x );
+      rotation3.set12( translation.y );
+      return rotation3;
+    },
+
+    computeTranslationRotationScaleMatrix: function() {
+      var i;
+      var localMatrix = new Matrix( this._presses.length * 2, 4 );
+      for ( i = 0; i < this._presses.length; i++ ) {
+        // [ x  y 1 0 ]
+        // [ y -x 0 1 ]
+        var localPoint = this._presses[ i ].localPoint;
+        localMatrix.set( 2 * i + 0, 0, localPoint.x );
+        localMatrix.set( 2 * i + 0, 1, localPoint.y );
+        localMatrix.set( 2 * i + 0, 2, 1 );
+        localMatrix.set( 2 * i + 1, 0, localPoint.y );
+        localMatrix.set( 2 * i + 1, 1, -localPoint.x );
+        localMatrix.set( 2 * i + 1, 3, 1 );
+      }
+      var targetMatrix = new Matrix( this._presses.length * 2, 1 );
+      for ( i = 0; i < this._presses.length; i++ ) {
+        var targetPoint = this._presses[ i ].targetPoint;
+        targetMatrix.set( 2 * i + 0, 0, targetPoint.x );
+        targetMatrix.set( 2 * i + 1, 0, targetPoint.y );
+      }
+      var coefficientMatrix = SingularValueDecomposition.pseudoinverse( localMatrix ).times( targetMatrix );
+      var m11 = coefficientMatrix.get( 0, 0 );
+      var m12 = coefficientMatrix.get( 1, 0 );
+      var m13 = coefficientMatrix.get( 2, 0 );
+      var m23 = coefficientMatrix.get( 3, 0 );
+      return new Matrix3().rowMajor( m11, m12, m13,
+                                     -m12, m11, m23,
+                                     0, 0, 1 );
     }
-
-
   } );
 
-  function Press( pointer ) {
+  function Press( pointer, trail ) {
     this.pointer = pointer;
+    this.trail = trail;
     this.interrupted = false;
+
+    this.localPoint = trail.globalToLocalPoint( pointer.point );
   }
 
   inherit( Object, Press, {
-    // ..?
+    get targetPoint() {
+      return this.trail.globalToParentPoint( this.pointer.point );
+    }
   } );
 
   return MultiListener;
