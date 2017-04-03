baccf21353bb924c3910af710150a4c7ffb3e67b
Chris Malley <cmalley@pixelzoom.com>
2017-04-03 12:47:03 -0600

    doc typo #131

--- js/listeners/FireListener.js
+++ js/listeners/FireListener.js
@@ -68,7 +68,7 @@ define( function( require ) {
     PressListener.call( this, options );
 
     // @public {ObservableArray.<Pointer>} - Contains all pointers that are over our button. Tracked by adding with
-    // 'enter' events and removin with 'exit' events.
+    // 'enter' events and removing with 'exit' events.
     this.overPointers = new ObservableArray();
 
     // @public {Property.<boolean>} - See options for documentation.
