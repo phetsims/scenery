fe91c795b2c8f36b63d52b655167f89c85b8bb37
samreid <reids@colorado.edu>
2017-10-31 12:41:53 -0600

    Added preliminary support for changing images with instance proxies, see https://github.com/phetsims/joist/issues/456

---
+++ js/nodes/TImage.js
@@ -0,0 +1,52 @@
+// Copyright 2016-2017, University of Colorado Boulder
+
+/**
+ * Wrapper type for scenery's Image node.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var scenery = require( 'SCENERY/scenery' );
+  var TNode = require( 'SCENERY/nodes/TNode' );
+
+  // phet-io modules
+  var assertInstanceOf = require( 'ifphetio!PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
+  var TString = require( 'ifphetio!PHET_IO/types/TString' );
+  var TVoid = require( 'ifphetio!PHET_IO/types/TVoid' );
+
+  /**
+   * Wrapper type for scenery's Text node.
+   * @param {Text} text
+   * @param {string} phetioID
+   * @constructor
+   */
+  function TImage( text, phetioID ) {
+    TNode.call( this, text, phetioID );
+    assertInstanceOf( text, phet.scenery.Image );
+  }
+
+  phetioInherit( TNode, 'TImage', TImage, {
+
+    setImage: {
+      returnType: TVoid,
+      parameterTypes: [ TString ],
+      implementation: function( base64Text ) {
+        var im = new window.Image();
+        im.src = base64Text;
+        this.instance.image = im;
+      },
+      documentation: 'Set the image from a base64 string'
+    }
+  }, {
+    documentation: 'The tandem wrapper type for the scenery Text node',
+    events: [ 'changed' ]
+  } );
+
+  scenery.register( 'TImage', TImage );
+
+  return TImage;
+} );
