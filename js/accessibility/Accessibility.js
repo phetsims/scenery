0b866a481a81dd200938a664c646abdcd6cb3f39
samreid <reids@colorado.edu>
2018-05-11 10:54:29 -0600

    Reorganized option keys

--- js/accessibility/Accessibility.js
+++ js/accessibility/Accessibility.js
@@ -147,18 +147,23 @@ define( function( require ) {
 
   // The options for the Accessibility API. In general, most default to null; to clear, set back to null.
   var ACCESSIBILITY_OPTION_KEYS = [
+
+    'containerTagName', // Sets the tag name for an [optional] element that contains this Node's siblings, see setContainerTagName()
+
     'tagName', // Sets the tag name for the primary sibling DOM element in the parallel DOM
+    'innerContent', // Sets the inner text or HTML for a node's primary sibling element, see setInnerContent()
     'inputType', // Sets the input type for the primary sibling DOM element, only relevant if tagName is 'input'
     'inputValue', // Sets the input value for the primary sibling DOM element, only relevant if tagName is 'input'
     'accessibleChecked', // Sets the 'checked' state for inputs of type 'radio' and 'checkbox', see setAccessibleChecked()
-    'containerTagName', // Sets the tag name for an element that contains this Node's siblings, see setContainerTagName()
+
     'labelTagName', // Sets the tag name for the DOM element sibling labelling this node, see setLabelTagName()
-    'descriptionTagName', // Sets the tag name for the DOM element sibling describing this node, see setDescriptionTagName()
-    'innerContent', // Sets the inner text or HTML for a node's primary sibling element, see setInnerContent()
     'labelContent', // Sets the label content for the node, see setLabelContent()
-    'descriptionContent', // Sets the description content for the node, see setDescriptionContent()
     'appendLabel', // Sets the label sibling to come after the primary sibling in the pDOM, see setAppendLabel()
+
+    'descriptionTagName', // Sets the tag name for the DOM element sibling describing this node, see setDescriptionTagName()
+    'descriptionContent', // Sets the description content for the node, see setDescriptionContent()
     'appendDescription', // Sets the description sibling to come after the primary sibling in the pDOM, see setAppendDescription()
+
     'focusHighlight', // Sets the focus highlight for the node, see setFocusHighlight()
     'focusHighlightLayerable', // Flag to determine if the focus highlight node can be layered in the scene graph, see setFocusHighlightLayerable()
     'groupFocusHighlight', // Sets the outer focus highlight for this node when a descendant has focus, see setGroupFocusHighlight()
