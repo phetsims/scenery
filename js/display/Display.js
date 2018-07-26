5a17a930c357bf9ce40248a5739259335d33abcc
Jonathan Olson <jonathan.olson@colorado.edu>
2018-07-26 14:12:19 -0600

    Adding userGestureEmitter for https://github.com/phetsims/vibe/issues/32 compatibility

--- js/display/Display.js
+++ js/display/Display.js
@@ -43,6 +43,7 @@ define( function( require ) {
   'use strict';
 
   var inherit = require( 'PHET_CORE/inherit' );
+  var Emitter = require( 'AXON/Emitter' );
   var extend = require( 'PHET_CORE/extend' );
   var Events = require( 'AXON/Events' );
   var Property = require( 'AXON/Property' );
@@ -1609,6 +1610,12 @@ define( function( require ) {
   };
 
   Property.addProperty( Display, 'focus', null ); // { display: {Display}, trail: {Trail} }
+
+  // @public {Emitter} - Fires when we detect an input event that would be considered a "user gesture" by Chrome, so
+  // that we can trigger browser actions that are only allowed as a result.
+  // See https://github.com/phetsims/scenery/issues/802 and https://github.com/phetsims/vibe/issues/32 for more
+  // information.
+  Display.userGestureEmitter = new Emitter();
 
   return Display;
 } );
