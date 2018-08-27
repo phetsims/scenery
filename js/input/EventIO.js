3fc13a6fc247aef932fb971ee81e61a6ea1dc769
zepumph <michael.kauzmann@colorado.edu>
2018-08-27 14:28:04 -0800

    added draggedEmitter to DragListener, delete DragListenerIO, https://github.com/phetsims/phet-io-wrappers/issues/74

---
+++ js/input/EventIO.js
@@ -0,0 +1,54 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * IO type for scenery Event
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  const Vector2IO = require( 'DOT/Vector2IO' );
+  const scenery = require( 'SCENERY/scenery' );
+
+  // ifphetio
+  const assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
+  const ObjectIO = require( 'ifphetio!PHET_IO/types/ObjectIO' );
+  const phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
+
+  /**
+   * IO type for phet/scenery's Event class.
+   * @param {Event} event
+   * @param phetioID
+   * @constructor
+   */
+  function EventIO( event, phetioID ) {
+    assert && assertInstanceOf( event, phet.scenery.Event );
+    ObjectIO.call( this, event, phetioID );
+  }
+
+  phetioInherit( ObjectIO, 'EventIO', EventIO, {}, {
+    get documentation() { return 'An event, with a point'; },
+
+    /**
+     * Encodes a Color into a state object.
+     * @param {Event} event
+     * @returns {Object}
+     */
+    toStateObject( event ) {
+      assert && assertInstanceOf( event, phet.scenery.Event );
+
+      var eventObject = {};
+      if ( event.pointer && event.pointer.point ) {
+        eventObject.point = Vector2IO.toStateObject( event.pointer.point );
+      }
+
+      // Note: If changing the contents of this object, please document it in the public documentation string.
+      return eventObject;
+    }
+  } );
+
+  return scenery.register( 'EventIO', EventIO );
+} );
+
--- js/listeners/DragListener.js
+++ js/listeners/DragListener.js
@@ -41,7 +41,11 @@ define( function( require ) {
 
   // modules
   var Bounds2 = require( 'DOT/Bounds2' );
+  var Emitter = require( 'AXON/Emitter' );
+  var EmitterIO = require( 'AXON/EmitterIO' );
+  var EventIO = require( 'SCENERY/input/EventIO' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var PhetioObject = require( 'TANDEM/PhetioObject' );
   var PressListener = require( 'SCENERY/listeners/PressListener' );
   var Property = require( 'AXON/Property' );
   var scenery = require( 'SCENERY/scenery' );
@@ -50,9 +54,6 @@ define( function( require ) {
   var Transform3 = require( 'DOT/Transform3' );
   var TransformTracker = require( 'SCENERY/util/TransformTracker' );
   var Vector2 = require( 'DOT/Vector2' );
-
-  // ifphetio
-  var DragListenerIO = require( 'SCENERY/listeners/DragListenerIO' );
 
   // Scratch vectors used to prevent allocations
   var scratchVector2A = new Vector2();
@@ -64,6 +65,8 @@ define( function( require ) {
    * @param {Object} [options] - See the constructor body (below) and in PressListener for documented options.
    */
   function DragListener( options ) {
+    var self = this;
+
     options = _.extend( {
       // {boolean} - If true, unattached touches that move across our node will trigger a press(). This helps sometimes
       // for small draggable objects.
@@ -116,9 +119,7 @@ define( function( require ) {
       // {Tandem} - For instrumenting
       tandem: Tandem.required,
 
-      // {DragListenerIO} - PhET-iO type
-      phetioType: DragListenerIO,
-      phetioEventType: 'user'
+      phetioReadOnly: PhetioObject.DEFAULT_OPTIONS.phetioReadOnly // to support properly passing this to children, see https://github.com/phetsims/tandem/issues/60
     }, options );
 
     // Initialize with the alias isUserControlledProperty => isPressedProperty
@@ -188,6 +189,25 @@ define( function( require ) {
     // interruptions here so that we can prevent future enter/down events from the same touch pointer from triggering
     // another startDrag.
     this._lastInterruptedTouchPointer = null;
+
+    // @private {Emitter} - emitted on drag. Used for triggering phet-io events, see https://github.com/phetsims/scenery/issues/842
+    this._draggedEmitter = new Emitter( {
+      tandem: options.tandem.createTandem( 'draggedEmitter' ),
+      phetioInstanceDocumentation: 'Emits whenever a drag occurs with a single EventIO argument.',
+      phetioReadOnly: options.phetioReadOnly,
+      phetioEventType: 'user',
+      phetioType: EmitterIO( [ EventIO ] )
+    } );
+
+    this._draggedEmitter.addListener( function( event ) {
+
+      // This is done first, before the drag listener is called (from the prototype drag call)
+      if ( !self._globalPoint.equals( self.pointer.point ) ) {
+        self.reposition( self.pointer.point );
+      }
+
+      PressListener.prototype.drag.call( self, event );
+    } );
   }
 
   scenery.register( 'DragListener', DragListener );
@@ -273,19 +293,7 @@ define( function( require ) {
       sceneryLog && sceneryLog.InputListener && sceneryLog.InputListener( 'DragListener drag' );
       sceneryLog && sceneryLog.InputListener && sceneryLog.push();
 
-      this.phetioStartEvent( 'drag', {
-        x: event.pointer.point.x,
-        y: event.pointer.point.y
-      } );
-
-      // This is done first, before the drag listener is called (from the prototype drag call)
-      if ( !this._globalPoint.equals( this.pointer.point ) ) {
-        this.reposition( this.pointer.point );
-      }
-
-      PressListener.prototype.drag.call( this, event );
-
-      this.phetioEndEvent();
+      this._draggedEmitter.emit1( event );
 
       sceneryLog && sceneryLog.InputListener && sceneryLog.pop();
     },
@@ -649,6 +657,8 @@ define( function( require ) {
     dispose: function() {
       sceneryLog && sceneryLog.InputListener && sceneryLog.InputListener( 'DragListener dispose' );
       sceneryLog && sceneryLog.InputListener && sceneryLog.push();
+
+      this._draggedEmitter.dispose();
 
       this.detachTransformTracker();
 
