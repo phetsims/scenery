e2be6c840f8cce48d9004572414269c5aa9e095e
chrisklus <christopher.klusendorf@colorado.edu>
2018-10-25 15:30:54 -0600

    Sort NodeIO require statements, see https://github.com/phetsims/dot/issues/80 and https://github.com/phetsims/tasks/issues/963

--- js/nodes/NodeIO.js
+++ js/nodes/NodeIO.js
@@ -10,18 +10,18 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var BooleanIO = require( 'TANDEM/types/BooleanIO' );
   var NodeProperty = require( 'SCENERY/util/NodeProperty' );
+  var NullableIO = require( 'TANDEM/types/NullableIO' );
   var NumberProperty = require( 'AXON/NumberProperty' );
+  var ObjectIO = require( 'TANDEM/types/ObjectIO' );
   var phetioInherit = require( 'TANDEM/phetioInherit' );
   var PropertyIO = require( 'AXON/PropertyIO' );
+  var Range = require( 'DOT/Range' );
   var scenery = require( 'SCENERY/scenery' );
-  var BooleanIO = require( 'TANDEM/types/BooleanIO' );
-  var NullableIO = require( 'TANDEM/types/NullableIO' );
-  var ObjectIO = require( 'TANDEM/types/ObjectIO' );
 
   // ifphetio
   var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
-  var Range = require( 'DOT/Range' );
 
   /**
    * IO type for phet/scenery's Node
