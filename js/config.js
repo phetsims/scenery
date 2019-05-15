d9368c35ad8fb4249399318bf496055713a5f480
chrisklus <christopher.klusendorf@colorado.edu>
2019-05-14 20:26:29 -0600

    Fix urlArgs arguments, see https://github.com/phetsims/sherpa/issues/76

--- js/config.js
+++ js/config.js
@@ -31,5 +31,5 @@ require.config( {
   },
 
   // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
-  urlArgs: Date.now()
+  urlArgs: 'bust=' + ( new Date() ).getTime()
 } );
