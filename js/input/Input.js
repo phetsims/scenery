4ace157efd412f4f193fc86cd3503b2a5c71ac91
zepumph <michael.kauzmann@colorado.edu>
2020-02-07 14:45:27 -0900

    add type as a readonly property, https://github.com/phetsims/gravity-force-lab/issues/245

--- js/input/Input.js
+++ js/input/Input.js
@@ -186,6 +186,7 @@ define( require => {
 
   // Object literal makes it easy to check for the existence of an attribute (compared to [].indexOf()>=0)
   const domEventPropertiesToSerialize = {
+    type: true,
     button: true, keyCode: true,
     deltaX: true, deltaY: true, deltaZ: true, deltaMode: true, pointerId: true,
     pointerType: true, charCode: true, which: true, clientX: true, clientY: true, changedTouches: true,
@@ -1961,7 +1962,9 @@ define( require => {
       // IE doesn't support `new Event()`, but document.createEvent is largely deprecated, so only use it when we must.
       const domEvent = platform.ie ? document.createEvent( 'Event' ) : new window.Event( 'Event' );
       for ( const key in eventObject ) {
-        if ( eventObject.hasOwnProperty( key ) ) {
+
+        // `type` is readonly, so don't try to set it.
+        if ( eventObject.hasOwnProperty( key ) && key !== 'type' ) {
 
           // Special case for target since we can't set that read-only property. Instead use a substitute key.
           if ( key === 'target' ) {
