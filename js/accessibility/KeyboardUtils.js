cdcfcd420c7d379c21871a9e25e368564646e58f
Jesse <jesse.greenberg@colorado.edu>
2020-04-22 11:45:33 -0400

    global hotkey to toggle isPlayingProperty, see phetsims/scenery-phet#571 and phetsims/scenery#1048

--- js/accessibility/KeyboardUtils.js
+++ js/accessibility/KeyboardUtils.js
@@ -51,6 +51,7 @@ var KeyboardUtils = {
   KEY_N: 78,
   KEY_S: 83,
   KEY_W: 87,
+  KEY_K: 75,
 
   // beware that "="" and "+" keys share the same keycode, distinguish with shfitKey Event property
   // also, these keycodes are different in Firefox, see http://www.javascripter.net/faq/keycodes.htm
