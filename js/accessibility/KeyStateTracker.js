f814a09d526f0049f4db39dc29f9c05febed29a1
Jesse <jesse.greenberg@colorado.edu>
2020-04-22 21:24:03 -0400

    check for body before attaching to body in response to error in build found on CT, see #1048

--- js/accessibility/KeyStateTracker.js
+++ js/accessibility/KeyStateTracker.js
@@ -310,6 +310,9 @@ class KeyStateTracker {
    */
   attachToBody() {
     assert && assert( !this.attachedToBody, 'KeyStateTracker is already attached to body.' );
+    if ( !document.body ) {
+      throw new Error( 'Cant access body element until it is declared, run script later or in a timeout.' );
+    }
 
     this.bodyKeydownListener = event => {
       if ( this.blockTrustedEvents && event.isTrusted ) {
