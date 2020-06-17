86637bc36ac2803a937e4de76c1b0568e4357d13
Jonathan Olson <jonathan.olson@colorado.edu>
2020-06-17 16:03:45 -0600

    Updating require.js documentation and associated linting/build changes, see https://github.com/phetsims/chipper/issues/905

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 // Copyright 2016, University of Colorado Boulder
 
+/* eslint-env node */
+
 // use chipper's gruntfile
 module.exports = require( '../chipper/js/grunt/Gruntfile.js' );
--- tests/event-logs/server/server.js
+++ tests/event-logs/server/server.js
@@ -5,6 +5,8 @@
 
 // This is intended to allow reads/writes to the recorded sim data area (../data), depending on whether it is a GET or POST request
 
+/* eslint-env node */
+
 const http = require( 'http' );
 const fs = require( 'fs' );
 
