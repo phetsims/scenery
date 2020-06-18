328ff8ebb8046af28f50b8f565be6e2cf3c00a46
pixelzoom <cmalley@pixelzoom.com>
2020-06-18 10:11:08 -0600

    migrate color utilities to scenery.Color, https://github.com/phetsims/natural-selection/issues/107

--- js/util/Color.js
+++ js/util/Color.js
@@ -803,4 +803,54 @@ Color.checkPaint = function( paint ) {
   }
 };
 
+/**
+ * Gets the luminance of a color, per ITU-R recommendation BT.709, https://en.wikipedia.org/wiki/Rec._709.
+ * Green contributes the most to the intensity perceived by humans, and blue the least.
+ * This algorithm works correctly with a grayscale color because the RGB coefficients sum to 1.
+ * @param {Color|string} color
+ * @returns {number} - a value in the range [0,255]
+ * @public
+ */
+Color.getLuminance = function( color ) {
+  const sceneryColor = Color.toColor( color );
+  const luminance = ( sceneryColor.red * 0.2126 + sceneryColor.green * 0.7152 + sceneryColor.blue * 0.0722 );
+  assert && assert( luminance >= 0 && luminance <= 255, `unexpected luminance: ${luminance}` );
+  return luminance;
+};
+
+/**
+ * Converts a color to grayscale.
+ * @param {Color|string} color
+ * @returns {Color}
+ * @public
+ */
+Color.toGrayscale = function( color ) {
+  const luminance = Color.getLuminance( color );
+  return new Color( luminance, luminance, luminance );
+};
+
+/**
+ * Determines whether a color is 'dark'.
+ * @param {Color|string} color
+ * @param {number} [luminanceThreshold] - colors with luminance < this value are dark, range [0,255], default 186
+ * @returns {boolean}
+ * @public
+ */
+Color.isDarkColor = function( color, luminanceThreshold = 186 ) {
+  assert && assert( typeof luminanceThreshold === 'number' && luminanceThreshold >= 0 && luminanceThreshold <= 255,
+    'invalid luminanceThreshold' );
+  return ( Color.getLuminance( color ) < luminanceThreshold );
+};
+
+/**
+ * Determines whether a color is 'light'.
+ * @param {Color|string} color
+ * @param {number} [luminanceThreshold] - colors with luminance >= this value are light, range [0,255], default 186
+ * @returns {boolean}
+ * @public
+ */
+Color.isLightColor = function( color, luminanceThreshold ) {
+  return !Color.isDarkColor( color, luminanceThreshold );
+};
+
 export default Color;
