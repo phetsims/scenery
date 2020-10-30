c0b02f87490844bb79f19ed2d2e0b4b2ff19e9aa
zepumph <michael.kauzmann@colorado.edu>
2020-10-30 13:26:49 -0800

    delete EnabledNode, and move tests to NodeTests.js, https://github.com/phetsims/scenery/issues/1100

--- js/nodes/NodeTests.js
+++ js/nodes/NodeTests.js
@@ -7,10 +7,15 @@
  */
 
 import BooleanProperty from '../../../axon/js/BooleanProperty.js';
+import Property from '../../../axon/js/Property.js';
+import TinyProperty from '../../../axon/js/TinyProperty.js';
 import Bounds2 from '../../../dot/js/Bounds2.js';
+import Range from '../../../dot/js/Range.js';
 import Vector2 from '../../../dot/js/Vector2.js';
 import Shape from '../../../kite/js/Shape.js';
+import HSlider from '../../../sun/js/HSlider.js';
 import Tandem from '../../../tandem/js/Tandem.js';
+import Display from '../display/Display.js';
 import Touch from '../input/Touch.js';
 import Node from './Node.js';
 import Rectangle from './Rectangle.js';
@@ -882,4 +887,89 @@ if ( Tandem.PHET_IO_ENABLED ) {
       Tandem.unlaunch();
     }
   } );
+}
+
+QUnit.test( 'Node.enabledProperty', assert => {
+
+  let node = new Node();
+
+  testEnabledNode( assert, node, 'For Node' );
+
+  const disabledOpacity = .2;
+  node = new Node( {
+    disabledOpacity: disabledOpacity
+  } );
+
+  assert.ok( node.opacity === new Node().opacity, 'opacity should default to Node default' );
+  node.enabled = false;
+  assert.ok( node.opacity === disabledOpacity, 'test disabled opacity' );
+
+  node.dispose();
+  assert.ok( node.enabledProperty.isDisposed, 'should be disposed' );
+
+  const myEnabledProperty = new BooleanProperty( true );
+  const defaultListenerCount = myEnabledProperty.changedEmitter.getListenerCount();
+  const node2 = new Node( {
+    enabledProperty: myEnabledProperty
+  } );
+  assert.ok( myEnabledProperty.changedEmitter.getListenerCount() > defaultListenerCount, 'listener count should be more since passing in enabledProperty' );
+
+  node2.dispose();
+  assert.ok( myEnabledProperty.changedEmitter.getListenerCount() === defaultListenerCount, 'listener count should match original' );
+} );
+
+QUnit.test( 'Node.enabledProperty with PDOM', assert => {
+
+  const rootNode = new Node( { tagName: 'div' } );
+  var display = new Display( rootNode ); // eslint-disable-line
+  document.body.appendChild( display.domElement );
+
+  const a11yNode = new Node( {
+    tagName: 'p'
+  } );
+
+  rootNode.addChild( a11yNode );
+  assert.ok( a11yNode.accessibleInstances.length === 1, 'should have an instance when attached to display' );
+  assert.ok( !!a11yNode.accessibleInstances[ 0 ].peer, 'should have a peer' );
+
+  // TODO: is it important that aria-disabled is false on all enabled Nodes? See https://github.com/phetsims/scenery/issues/1100
+  // assert.ok( a11yNode.accessibleInstances[ 0 ].peer.primarySibling.getAttribute( 'aria-disabled' ) === 'false', 'should be enabled' );
+
+  a11yNode.enabled = false;
+  assert.ok( a11yNode.accessibleInstances[ 0 ].peer.primarySibling.getAttribute( 'aria-disabled' ) === 'true', 'should be enabled' );
+  testEnabledNode( assert, a11yNode, 'For accessible Node' );
+} );
+
+QUnit.test( 'Node.enabledProperty in Slider', assert => {
+  let slider = new HSlider( new Property( 0 ), new Range( 0, 10 ), {
+    tandem: Tandem.GENERAL.createTandem( 'mySlider' )
+  } );
+  testEnabledNode( assert, slider, 'For Slider' );
+  slider.dispose();
+
+  const myEnabledProperty = new BooleanProperty( true, { tandem: Tandem.GENERAL.createTandem( 'myEnabledProperty' ) } );
+  slider = new HSlider( new Property( 0 ), new Range( 0, 10 ), {
+    tandem: Tandem.GENERAL.createTandem( 'mySlider' ),
+    enabledProperty: myEnabledProperty
+  } );
+  testEnabledNode( assert, slider, 'For Slider' );
+  slider.dispose();
+  myEnabledProperty.dispose();
+} );
+
+/**
+ * Test basic functionality for an object that mixes in EnabledComponent
+ * @param {Object} assert - from QUnit
+ * @param {Object} enabledNode - mixed in with EnabledComponent
+ * @param {string} message - to tack onto assert messages
+ */
+function testEnabledNode( assert, enabledNode, message ) {
+  assert.ok( enabledNode.enabledProperty instanceof Property || enabledNode.enabledProperty instanceof TinyProperty, `${message}: enabledProperty should exist` );
+
+  assert.ok( enabledNode.enabledProperty.value === enabledNode.enabled, `${message}: test getter` );
+
+  enabledNode.enabled = false;
+  assert.ok( enabledNode.enabled === false, `${message}: test setter` );
+  assert.ok( enabledNode.enabledProperty.value === enabledNode.enabled, `${message}: test getter after setting` );
+  assert.ok( enabledNode.enabledProperty.value === false, `${message}: test getter after setting` );
 }
