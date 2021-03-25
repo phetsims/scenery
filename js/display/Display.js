d19131b6d29a2e8ef0a965e7c9e8fae8ad332163
Jonathan Olson <jonathan.olson@colorado.edu>
2021-03-24 20:11:26 -0600

    Track pointer capture events for working around when pointer capture doesn't work (e.g. Chrome outside of the iframe), see https://github.com/phetsims/scenery/issues/1186
    Adding interrupt to SimpleDragHandler pointer listener, see https://github.com/phetsims/scenery/issues/1186
    
    Adding pointer ID to mouseDown and mouseDownAction, see https://github.com/phetsims/natural-selection/issues/264
    
    Oops fix

--- js/display/Display.js
+++ js/display/Display.js
@@ -1218,7 +1218,7 @@ define( function( require ) {
             this._fuzzMouseIsDown = false;
           }
           else {
-            this._input.mouseDown( this._fuzzMousePosition, domEvent );
+            this._input.mouseDown( null, this._fuzzMousePosition, domEvent );
             this._fuzzMouseIsDown = true;
           }
 
