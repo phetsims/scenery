c2b392f21d69086654f8254db94383ed43a72cd5
Jonathan Olson <jonathan.olson@colorado.edu>
2021-03-24 20:11:26 -0600

    Track pointer capture events for working around when pointer capture doesn't work (e.g. Chrome outside of the iframe), see https://github.com/phetsims/scenery/issues/1186
    Adding interrupt to SimpleDragHandler pointer listener, see https://github.com/phetsims/scenery/issues/1186
    
    Adding pointer ID to mouseDown and mouseDownAction, see https://github.com/phetsims/natural-selection/issues/264
    
    Oops fix

--- js/input/SimpleDragHandler.js
+++ js/input/SimpleDragHandler.js
@@ -292,6 +292,11 @@ define( function( require ) {
       // mouse/touch move
       move: function( event ) {
         self.dragAction.execute( event.pointer.point, event );
+      },
+
+      // pointer interruption
+      interrupt: () => {
+        self.interrupt();
       }
     };
     PhetioObject.call( this, options );
