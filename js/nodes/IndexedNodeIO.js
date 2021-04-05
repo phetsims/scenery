894a16a0dffefe291a7645ef441225002dd69a59
samreid <reids@colorado.edu>
2021-04-05 16:32:33 -0600

    Show current index for IndexedNodeIO instances, see https://github.com/phetsims/studio/issues/95

--- js/nodes/IndexedNodeIO.js
+++ js/nodes/IndexedNodeIO.js
@@ -7,10 +7,18 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import FunctionIO from '../../../tandem/js/types/FunctionIO.js';
 import IOType from '../../../tandem/js/types/IOType.js';
+import NumberIO from '../../../tandem/js/types/NumberIO.js';
 import VoidIO from '../../../tandem/js/types/VoidIO.js';
 import scenery from '../scenery.js';
 import Node from './Node.js';
+
+// In order to support unlinking from listening to the index property, keep an indexed map to callback functions
+const map = {};
+
+// The next index at which a callback will appear in the map. This always increments and we do reuse old indices
+let index = 0;
 
 const IndexedNodeIO = new IOType( 'IndexedNodeIO', {
   valueType: Node,
@@ -29,6 +37,39 @@ const IndexedNodeIO = new IOType( 'IndexedNodeIO', {
     }
   },
   methods: {
+    linkIndex: {
+      returnType: NumberIO,
+      parameterTypes: [ FunctionIO( VoidIO, [ NumberIO ] ) ],
+      documentation: 'Following the PropertyIO.link pattern, subscribe for notifications when the index in the parent ' +
+                     'changes, and receive a callback with the current value.  The return value is a numeric ID for use ' +
+                     'with clearLinkIndex.',
+      implementation: function( listener ) {
+
+        // The callback which signifies the current index
+        const callback = () => {
+          const index = this.parents[ 0 ].indexOfChild( this );
+          listener( index );
+        };
+
+        this.parents[ 0 ].childrenChangedEmitter.addListener( callback );
+        callback();
+
+        const myIndex = index;
+        map[ myIndex ] = callback;
+        index++;
+        return myIndex;
+      }
+    },
+    clearLinkIndex: {
+      returnType: VoidIO,
+      parameterTypes: [ NumberIO ],
+      documentation: 'Unlink a listener that has been added using linkIndex, by its numerical ID (like setTimeout/clearTimeout)',
+      implementation: function( index ) {
+        const method = map[ index ];
+        this.parents[ 0 ].childrenChangedEmitter.removeListener( method );
+        delete map[ index ];
+      }
+    },
     moveForward: {
       returnType: VoidIO,
       parameterTypes: [],
