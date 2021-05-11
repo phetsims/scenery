1b8a48071328c72ae2532032ece09e0b11e82e5f
samreid <reids@colorado.edu>
2021-05-11 10:30:07 -0600

    Fix whitespace in phetioDocumentation

--- js/nodes/Node.js
+++ js/nodes/Node.js
@@ -6782,7 +6782,7 @@ class Node extends PhetioObject {
 
           // by default, use the value from the Node
           phetioReadOnly: this.phetioReadOnly,
-          phetioDocumentation: 'Sets whether the node is enabled. This will set whether input is enabled for this Node and' +
+          phetioDocumentation: 'Sets whether the node is enabled. This will set whether input is enabled for this Node and ' +
                                'most often children as well. It will also control and toggle the "disabled look" of the node.',
           tandem: this.tandem.createTandem( ENABLED_PROPERTY_TANDEM_NAME )
         }, config.enabledPropertyOptions ) )
