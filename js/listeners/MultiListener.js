51363beb7eb0aca1c8b7ed124664ed94542f48c2
Jonathan Olson <jonathan.olson@colorado.edu>
2021-05-11 18:37:52 -0600

    MultiListener patch for https://github.com/phetsims/scenery/issues/1218

--- js/listeners/MultiListener.js
+++ js/listeners/MultiListener.js
@@ -196,7 +196,7 @@ class MultiListener {
         sceneryLog && sceneryLog.InputListener && sceneryLog.InputListener( 'MultiListener background interrupt' );
         sceneryLog && sceneryLog.InputListener && sceneryLog.push();
 
-        this.removeBackgroundPress( this.findBackgroundPress( event.pointer ) );
+        this.interrupt();
 
         sceneryLog && sceneryLog.InputListener && sceneryLog.pop();
       }
@@ -499,6 +499,10 @@ class MultiListener {
 
     while ( this._presses.length ) {
       this.removePress( this._presses[ this._presses.length - 1 ] );
+    }
+
+    while ( this._backgroundPresses.length ) {
+      this.removeBackgroundPress( this._backgroundPresses[ this._backgroundPresses.length - 1 ] );
     }
 
     sceneryLog && sceneryLog.InputListener && sceneryLog.pop();
