5da11203e674da652f74cecd0def2a021e9e0991
Jesse <jesse.greenberg@colorado.edu>
2021-05-17 22:32:46 -0400

    More documentation improvements

--- js/accessibility/voicing/Voicing.js
+++ js/accessibility/voicing/Voicing.js
@@ -3,19 +3,20 @@
 /**
  * A trait for Node that supports the Voicing feature, under accessibility. Allows you to define responses for the Node
  * and make requests to speak that content using HTML5 SpeechSynthesis and the UtteranceQueue. Voicing content is
- * organized into four categories which are responsible for describing different things and whose output of content
+ * organized into four categories which are responsible for describing different things. Output of this content
  * can be controlled by the voicingManager. These include the
  *
- * - "Name" response: The name of the object that uses Voicing. Similar to the "Accessible Name" of PDOM.
+ * - "Name" response: The name of the object that uses Voicing. Similar to the "Accessible Name" in web accessibility.
  * - "Object" response: The state information about the object that uses Voicing.
  * - "Context" response: The contextual changes that result from interaction with the Node that uses Voicing.
- * - "Hint" response: A supporting hint that guides the user toward a desired interaction.
+ * - "Hint" response: A supporting hint that guides the user toward a desired interaction with this Node.
  *
  * See the property and setter documentation for each of these responses for more information.
  *
  * Once this content is set, you can make a request to speak it using an UtteranceQueue with one of the provided
- * functions of Voicing. It is up to you to call one of these functions when you wish for speech to be made. The only
- * exception is on the 'focus', which consistently speaks content for all Nodes that are composed with Voicing.
+ * functions in this Trait. It is up to you to call one of these functions when you wish for speech to be made. The only
+ * exception is on the 'focus' event. Every Node that composes Voicing will speak its responses by when it
+ * receives focus.
  *
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
