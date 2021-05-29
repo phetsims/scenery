6b0d04a6b1da706544500608a22629e4655af37f
pixelzoom <cmalley@pixelzoom.com>
2021-05-28 20:54:01 -0600

    fix doc typo

--- js/accessibility/pdom/ParallelDOM.js
+++ js/accessibility/pdom/ParallelDOM.js
@@ -567,7 +567,7 @@ const ParallelDOM = {
 
       /**
        * Called when assertions are enabled and once the Node has been completely constructed. This is the time to
-       * make sure that options are saet up the way they are expected to be. For example. you don't want accessibleName
+       * make sure that options are set up the way they are expected to be. For example. you don't want accessibleName
        * and labelContent declared
        * @public (only called by Screen.js)
        */
