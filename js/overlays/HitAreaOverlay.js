559083962666438909a8ffc814d04c15504024f3
phet-dev <phetadmin@gmail.com>
2021-06-30 03:47:27 -0600

    update copyright dates from daily grunt work

--- js/overlays/HitAreaOverlay.js
+++ js/overlays/HitAreaOverlay.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Displays the "hittable" mouse/touch regions for items with input listeners.
