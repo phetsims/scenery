93810c0b0ffdacde978a6a8b0b2329aa2ab8229e
samreid <reids@colorado.edu>
2021-07-19 10:15:39 -0600

    Remove ColorProfile.js, see https://github.com/phetsims/scenery-phet/issues/515

--- js/SceneryConstants.js
+++ js/SceneryConstants.js
@@ -13,7 +13,10 @@ const SceneryConstants = {
   // Opacity that is typically applied to a UI component in its disabled state, to make it look grayed out.
   // This was moved here from SunConstants because it's needed by LayoutBox.
   // See https://github.com/phetsims/scenery/issues/1153
-  DISABLED_OPACITY: 0.45
+  DISABLED_OPACITY: 0.45,
+
+  // The name of the color profile used for projector mode
+  PROJECTOR_COLOR_PROFILE_NAME: 'projector'
 };
 
 scenery.register( 'SceneryConstants', SceneryConstants );
