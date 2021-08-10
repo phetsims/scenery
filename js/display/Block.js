f36b6961a3b3a9d6133ccb7e81887fd56bb82561
phet-dev <phetadmin@gmail.com>
2021-08-10 03:41:12 -0600

    update copyright dates from daily grunt work

--- js/display/Block.js
+++ js/display/Block.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * A specialized drawable for a layer of drawables with the same renderer (basically, it's a Canvas element, SVG
