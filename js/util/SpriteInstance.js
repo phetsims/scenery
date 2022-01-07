2771c22e6d440402eea916b1f4ebdc017b3a49eb
phet-dev <phetadmin@gmail.com>
2022-01-07 03:27:22 -0700

    update copyright dates from daily grunt work

--- js/util/SpriteInstance.js
+++ js/util/SpriteInstance.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * Represents a single instance on the screen of a given Sprite object. It can have its own transformation matrix, and
