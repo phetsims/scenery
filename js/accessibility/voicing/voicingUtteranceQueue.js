ad30d2610014df029889c79eb8d22c1e924d917e
phet-dev <phetadmin@gmail.com>
2022-01-08 03:23:25 -0700

    update copyright dates from daily grunt work

--- js/accessibility/voicing/voicingUtteranceQueue.js
+++ js/accessibility/voicing/voicingUtteranceQueue.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * A singleton UtteranceQueue that is used for Voicing. It uses the voicingManager to announce Utterances,
