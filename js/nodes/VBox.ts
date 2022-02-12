a0f5f1d95b221d6f7ddf7b219a83b3ad049d11c2
phet-dev <phetadmin@gmail.com>
2022-02-12 03:41:57 -0700

    update copyright dates from daily grunt work

--- js/nodes/VBox.ts
+++ js/nodes/VBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * VBox is a convenience specialization of LayoutBox with vertical orientation.
