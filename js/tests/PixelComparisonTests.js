9b0594646936e03d9dc045602f6e0082240c9075
phet-dev <phetadmin@gmail.com>
2022-03-05 03:28:36 -0700

    update copyright dates from daily grunt work

--- js/tests/PixelComparisonTests.js
+++ js/tests/PixelComparisonTests.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * PixelComparison tests
--- js/util/TrailTests.js
+++ js/util/TrailTests.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Trail tests
