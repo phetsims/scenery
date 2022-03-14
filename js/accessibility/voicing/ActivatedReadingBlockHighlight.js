d76950d7273551d0ddbf929396a1ecca4cc404e3
phet-dev <phetadmin@gmail.com>
2022-03-14 03:29:16 -0600

    update copyright dates from daily grunt work

--- js/accessibility/voicing/ActivatedReadingBlockHighlight.js
+++ js/accessibility/voicing/ActivatedReadingBlockHighlight.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * The highlight with styling used for ReadingBlocks when the are "activated" and
