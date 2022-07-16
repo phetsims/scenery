bcc360af81870a4b9535464972f0ab0165cce014
phet-dev <phetadmin@gmail.com>
2022-07-16 05:43:48 -0600

    update copyright dates from daily grunt work

--- js/display/drawables/LineSVGDrawable.js
+++ js/display/drawables/LineSVGDrawable.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * SVG drawable for Line nodes.
--- js/listeners/animatedPanZoomSingleton.js
+++ js/listeners/animatedPanZoomSingleton.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * A singleton with an instance of an AnimatedPanZoomListener that can be easily accessed for an application.
