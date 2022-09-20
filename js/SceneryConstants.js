a9c39bdfec6dcbdfffb6c9bd22d7943131b85075
phet-dev <phetadmin@gmail.com>
2022-09-20 05:42:46 -0600

    update copyright dates from daily grunt work

--- js/SceneryConstants.js
+++ js/SceneryConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * SceneryConstants is a collection of constants used throughout scenery.
