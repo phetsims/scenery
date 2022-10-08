fd08ab604d4d62b3a21418f8f0826b7c4b1e8159
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-07 19:09:12 -0600

    Documentation for LayoutProxyProperty, see https://github.com/phetsims/scenery/issues/1419

--- js/layout/LayoutProxyProperty.ts
+++ js/layout/LayoutProxyProperty.ts
@@ -5,25 +5,37 @@
  * - a LayoutProxy with the single connected Trail (if it exists)
  * - null if there are zero or 2+ connected Trails between the two Nodes
  *
+ * When defined, this will provide a LayoutProxy for the leafNode within the rootNode's local coordinate frame. This
+ * will allow positioning the leafNode within the rootNode's coordinate frame (which is ONLY well-defined when there
+ * is exactly one trail between the two).
+ *
+ * Thus, it will only be defined as a proxy if there is a unique trail between the two Nodes. This is needed for layout
+ * work, where often we'll need to provide a proxy IF this condition is true, and NO proxy if it's not (since layout
+ * would be ambiguous). E.g. for ManualConstraint, if a Node isn't connected to the root, there's nothing the constraint
+ * can do.
+ *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
 import { DerivedProperty1 } from '../../../axon/js/DerivedProperty.js';
-import Tandem from '../../../tandem/js/Tandem.js';
 import { LayoutProxy, Node, scenery, Trail, TrailsBetweenProperty } from '../imports.js';
 
 export default class LayoutProxyProperty extends DerivedProperty1<LayoutProxy | null, Trail[]> {
 
+  // This will contain the number of trails connecting our rootNode and leafNode. Our value will be solely based off of
+  // this Property's value, and is thus created as a DerivedProperty.
   private readonly trailsBetweenProperty: TrailsBetweenProperty;
 
+  /**
+   * @param rootNode - The root whose local coordinate frame we'll want the proxy to be in
+   * @param leafNode - The leaf that we'll create the proxy for
+   */
   public constructor( rootNode: Node, leafNode: Node ) {
 
     const trailsBetweenProperty = new TrailsBetweenProperty( rootNode, leafNode );
 
     super( [ trailsBetweenProperty ], trails => {
       return trails.length === 1 ? LayoutProxy.pool.create( trails[ 0 ].copy().removeAncestor() ) : null;
-    }, {
-      tandem: Tandem.OPT_OUT
     } );
 
     this.trailsBetweenProperty = trailsBetweenProperty;
