10144522edbccf2fdd17fdc7643941a7cff04e8c
phet-dev <phetadmin@gmail.com>
2022-10-28 04:36:15 -0600

    update copyright dates from daily grunt work

--- js/util/colorProfileProperty.js
+++ js/util/colorProfileProperty.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * Singleton Property<string> which chooses between the available color profiles of a simulation, such as 'default',
