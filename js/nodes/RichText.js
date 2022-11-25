cd1d0b170a4651307085e0aaaa3d4994b028ea17
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-25 10:49:50 -0700

    Respecting ?allowLinks in RichText, see https://github.com/phetsims/joist/issues/830

--- js/nodes/RichText.js
+++ js/nodes/RichText.js
@@ -1736,8 +1736,10 @@ define( function( require ) {
         this.buttonListener = new ButtonListener( {
           fire: function( event ) {
             self._linkEventsHandled && event.handle();
-            var newWindow = window.open( href, '_blank' ); // open in a new window/tab
-            newWindow.focus();
+            if ( !window.phet || !phet.chipper || !phet.chipper.queryParameters || phet.chipper.queryParameters.allowLinks ) {
+              var newWindow = window.open( href, '_blank' ); // open in a new window/tab
+              newWindow.focus();
+            }
           }
         } );
         this.addInputListener( this.buttonListener );
