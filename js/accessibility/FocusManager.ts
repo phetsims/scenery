78dfdcb4d2d50d3ee80cbc647578a8bfaed3c577
Jesse <jesse.greenberg@colorado.edu>
2023-01-17 17:18:46 -0500

    remove TODO involving phet-io, see https://github.com/phetsims/scenery/issues/1516

--- js/accessibility/FocusManager.ts
+++ js/accessibility/FocusManager.ts
@@ -231,9 +231,7 @@ export default class FocusManager {
    * A Property that lets you know when the window has focus. When the window has focus, it is in the user's foreground.
    * When in the background, the window does not receive keyboard input (important for global keyboard events).
    */
-  public static windowHasFocusProperty = new BooleanProperty( true, {
-    // TODO: PhET-iO?
-  } );
+  public static windowHasFocusProperty = new BooleanProperty( true);
 
   /**
    * Updates the windowHasFocusProperty when the window receives/loses focus. When the window has focus
