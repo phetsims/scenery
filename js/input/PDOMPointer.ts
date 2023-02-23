cfc2c2d9d563f09c36b8f3885b1f8f23bf26d1a8
phet-dev <phetadmin@gmail.com>
2023-02-23 05:20:47 -0700

    update copyright dates from daily grunt work

--- js/input/PDOMPointer.ts
+++ js/input/PDOMPointer.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * Pointer type for managing accessibility, in particular the focus in the display.
