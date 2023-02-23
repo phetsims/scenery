ee45a87f29c3f47b4d2f755c4e3a82bdca3147c3
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-23 16:31:58 -0700

    Moving exclusively to phet.X namespaces, see https://github.com/phetsims/scenery/issues/966

--- js/scenery-main.js
+++ js/scenery-main.js
@@ -1,11 +1,11 @@
 // Copyright 2016-2022, University of Colorado Boulder
 
-import axon from '../../axon/js/main.js'; // eslint-disable-line default-import-match-filename
-import dot from '../../dot/js/main.js'; // eslint-disable-line default-import-match-filename
-import kite from '../../kite/js/main.js'; // eslint-disable-line default-import-match-filename,bad-sim-text
-import phetCore from '../../phet-core/js/main.js'; // eslint-disable-line default-import-match-filename
-import utteranceQueue from '../../utterance-queue/js/main.js'; // eslint-disable-line default-import-match-filename
-import scenery from './main.js'; // eslint-disable-line default-import-match-filename
+import '../../axon/js/main.js';
+import '../../dot/js/main.js';
+import '../../kite/js/main.js';
+import '../../phet-core/js/main.js';
+import '../../utterance-queue/js/main.js';
+import './main.js';
 
 if ( !window.hasOwnProperty( '_' ) ) {
   throw new Error( 'Underscore/Lodash not found: _' );
@@ -14,10 +14,4 @@ if ( !window.hasOwnProperty( '$' ) ) {
   throw new Error( 'jQuery not found: $' );
 }
 
-window.axon = axon;
-window.dot = dot;
-window.kite = kite;
-window.phetCore = phetCore;
-window.utteranceQueue = utteranceQueue;
-window.scenery = scenery;
-window.scenery.Utils.polyfillRequestAnimationFrame();
+phet.scenery.Utils.polyfillRequestAnimationFrame();
