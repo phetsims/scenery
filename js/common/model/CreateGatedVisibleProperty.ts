a2f5b582735a60c3704e05d99c539071f8c80629
Marla Schulz <marlaa.schulz@gmail.com>
2023-07-19 18:50:25 -0600

    Factor out CreateGatedVisibleProperty.ts, see: https://github.com/phetsims/center-and-variability/issues/289

---
+++ js/common/model/CreateGatedVisibleProperty.ts
@@ -0,0 +1,27 @@
+// Copyright 2023, University of Colorado Boulder
+
+/**
+ * Function that factors out the creation of a gated visible property for PhET-iO
+ *
+ * @author Marla Schulz (PhET Interactive Simulations)
+ *
+ */
+
+import Property from '../../../../axon/js/Property.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import BooleanIO from '../../../../tandem/js/types/BooleanIO.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import centerAndVariability from '../../centerAndVariability.js';
+
+export const CreateGatedVisibleProperty = ( visibleProperty: Property<boolean>, tandem: Tandem ): TReadOnlyProperty<boolean> => {
+  return DerivedProperty.and( [ visibleProperty, new BooleanProperty( true, {
+    tandem: tandem.createTandem( 'selfVisibleProperty' )
+  } ) ], {
+    tandem: tandem.createTandem( 'visibleProperty' ),
+    phetioValueType: BooleanIO
+  } );
+};
+
+centerAndVariability.register( 'CreateGatedVisibleProperty', CreateGatedVisibleProperty );
