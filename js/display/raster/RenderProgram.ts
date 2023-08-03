6e66d3d491937d022693593693c621873f33992e
Jonathan Olson <jonathan.olson@colorado.edu>
2023-08-02 20:04:07 -0600

    Blending and fixes

--- js/display/raster/RenderProgram.ts
+++ js/display/raster/RenderProgram.ts
@@ -13,6 +13,7 @@ import Matrix4 from '../../../../dot/js/Matrix4.js';
 import Matrix3 from '../../../../dot/js/Matrix3.js';
 import Vector4 from '../../../../dot/js/Vector4.js';
 import Utils from '../../../../dot/js/Utils.js';
+import Vector3 from '../../../../dot/js/Vector3.js';
 
 export type FillRule = 'nonzero' | 'evenodd';
 
@@ -233,8 +234,8 @@ export class RenderBlendCompose extends RenderProgram {
         break;
     }
 
-    if ( a instanceof RenderColor && b instanceof RenderColor ) {
-      // TODO: blend colors into each other
+    if ( a instanceof RenderColor && b instanceof RenderColor && a.path === null && b.path === null ) {
+      return new RenderColor( null, RenderBlendCompose.blendCompose( a.color, b.color, this.composeType, this.blendType ) );
     }
 
     return new RenderBlendCompose( this.composeType, this.blendType, a, b );
@@ -310,10 +311,109 @@ export class RenderBlendCompose extends RenderProgram {
     const a = this.a.evaluate( point, map );
     const b = this.b.evaluate( point, map );
 
-    if ( this.blendType !== RenderBlendType.Normal ) {
-      throw new Error( 'unimplemented blendType' );
+    return RenderBlendCompose.blendCompose( a, b, this.composeType, this.blendType );
+  }
+
+  public static blendCompose( a: Vector4, b: Vector4, composeType: RenderComposeType, blendType: RenderBlendType ): Vector4 {
+    let blended;
+    if ( blendType === RenderBlendType.Normal ) {
+      blended = a;
     }
-    const blended = a; // TODO blend a and b
+    else {
+      // Need to apply blending when not premultiplied
+      const a3 = RenderColor.unpremultiply( a ).toVector3();
+      const b3 = RenderColor.unpremultiply( b ).toVector3();
+      let c3: Vector3;
+
+      switch( blendType ) {
+        case RenderBlendType.Multiply: {
+          c3 = b3.componentTimes( a3 );
+          break;
+        }
+        case RenderBlendType.Screen: {
+          c3 = RenderBlendCompose.screen( b3, a3 );
+          break;
+        }
+        case RenderBlendType.Overlay: {
+          c3 = RenderBlendCompose.hardLight( a3, b3 );
+          break;
+        }
+        case RenderBlendType.Darken: {
+          c3 = new Vector3(
+            Math.min( b3.x, a3.x ),
+            Math.min( b3.y, a3.y ),
+            Math.min( b3.z, a3.z )
+          );
+          break;
+        }
+        case RenderBlendType.Lighten: {
+          c3 = new Vector3(
+            Math.max( b3.x, a3.x ),
+            Math.max( b3.y, a3.y ),
+            Math.max( b3.z, a3.z )
+          );
+          break;
+        }
+        case RenderBlendType.ColorDodge: {
+          c3 = new Vector3(
+            RenderBlendCompose.colorDodge( b3.x, a3.x ),
+            RenderBlendCompose.colorDodge( b3.y, a3.y ),
+            RenderBlendCompose.colorDodge( b3.z, a3.z )
+          );
+          break;
+        }
+        case RenderBlendType.ColorBurn: {
+          c3 = new Vector3(
+            RenderBlendCompose.colorBurn( b3.x, a3.x ),
+            RenderBlendCompose.colorBurn( b3.y, a3.y ),
+            RenderBlendCompose.colorBurn( b3.z, a3.z )
+          );
+          break;
+        }
+        case RenderBlendType.HardLight: {
+          c3 = RenderBlendCompose.hardLight( b3, a3 );
+          break;
+        }
+        case RenderBlendType.SoftLight: {
+          c3 = RenderBlendCompose.softLight( b3, a3 );
+          break;
+        }
+        case RenderBlendType.Difference: {
+          c3 = new Vector3(
+            Math.abs( b3.x - a3.x ),
+            Math.abs( b3.y - a3.y ),
+            Math.abs( b3.z - a3.z )
+          );
+          break;
+        }
+        case RenderBlendType.Exclusion: {
+          c3 = b3.plus( a3 ).minus( b3.componentTimes( a3 ).timesScalar( 2 ) );
+          break;
+        }
+        case RenderBlendType.Hue: {
+          c3 = RenderBlendCompose.setLum( RenderBlendCompose.setSat( a3, RenderBlendCompose.sat( b3 ) ), RenderBlendCompose.lum( b3 ) );
+          break;
+        }
+        case RenderBlendType.Saturation: {
+          c3 = RenderBlendCompose.setLum( RenderBlendCompose.setSat( b3, RenderBlendCompose.sat( a3 ) ), RenderBlendCompose.lum( b3 ) );
+          break;
+        }
+        case RenderBlendType.Color: {
+          c3 = RenderBlendCompose.setLum( a3, RenderBlendCompose.lum( b3 ) );
+          break;
+        }
+        case RenderBlendType.Luminosity: {
+          c3 = RenderBlendCompose.setLum( b3, RenderBlendCompose.lum( a3 ) );
+          break;
+        }
+        default: {
+          c3 = a3;
+          break;
+        }
+      }
+
+      blended = RenderColor.premultiply( new Vector4( c3.x, c3.y, c3.z, a.w ) );
+    }
 
     let fa;
     let fb;
@@ -324,7 +424,7 @@ export class RenderBlendCompose extends RenderProgram {
     // atop: fa: b,   fb: 1-a   fa*a: ab     fb*b: b(1-a) sum: b
     // xor:  fa: 1-b, fb: 1-a   fa*a: a(1-b) fb*b: b(1-a) sum: a(1-b) + b(1-a)
     // plus: fa: 1,   fb: 1     fa*a: a      fb*b: b      sum: a + b
-    switch( this.composeType ) {
+    switch( composeType ) {
       case RenderComposeType.Over:
         fa = 1;
         fb = 1 - a.w;
@@ -367,6 +467,143 @@ export class RenderBlendCompose extends RenderProgram {
       Math.min( fa * blended.z + fb * b.z, 1 ),
       Math.min( a.w * fa + b.w * fb, 1 )
     );
+  }
+
+  public static screen( cb: Vector3, cs: Vector3 ): Vector3 {
+    return cb.plus( cs ).minus( cb.componentTimes( cs ) );
+  }
+
+  public static colorDodge( cb: number, cs: number ): number {
+    if ( cb === 0 ) {
+      return 0;
+    }
+    else if ( cs === 1 ) {
+      return 1;
+    }
+    else {
+      return Math.min( 1, cb / ( 1 - cs ) );
+    }
+  }
+
+  public static colorBurn( cb: number, cs: number ): number {
+    if ( cb === 1 ) {
+      return 1;
+    }
+    else if ( cs === 0 ) {
+      return 0;
+    }
+    else {
+      return 1 - Math.min( 1, ( 1 - cb ) / cs );
+    }
+  }
+
+  // TODO: check examples, tests
+  public static hardLight( cb: Vector3, cs: Vector3 ): Vector3 {
+    const falseOption = RenderBlendCompose.screen( cb, cs.timesScalar( 2 ).minusScalar( 1 ) );
+    const trueOption = cb.timesScalar( 2 ).componentTimes( cs );
+
+    return new Vector3(
+      cs.x <= 0.5 ? trueOption.x : falseOption.x,
+      cs.y <= 0.5 ? trueOption.y : falseOption.y,
+      cs.z <= 0.5 ? trueOption.z : falseOption.z
+    );
+  }
+
+  // TODO: check examples, tests
+  public static softLight( cb: Vector3, cs: Vector3 ): Vector3 {
+    const d = new Vector3(
+      cb.x <= 0.25 ? ( 16 * cb.x - 12 ) * cb.x + 4 : Math.sqrt( cb.x ),
+      cb.y <= 0.25 ? ( 16 * cb.y - 12 ) * cb.y + 4 : Math.sqrt( cb.y ),
+      cb.z <= 0.25 ? ( 16 * cb.z - 12 ) * cb.z + 4 : Math.sqrt( cb.z )
+    );
+    return new Vector3(
+      cs.x > 0.5 ? cb.x + ( 2 * cs.x - 1 ) * ( d.x - cb.x ) : cb.x - ( 1 - 2 * cs.x ) * cb.x * ( 1 - cb.x ),
+      cs.y > 0.5 ? cb.y + ( 2 * cs.y - 1 ) * ( d.y - cb.y ) : cb.y - ( 1 - 2 * cs.y ) * cb.y * ( 1 - cb.y ),
+      cs.z > 0.5 ? cb.z + ( 2 * cs.z - 1 ) * ( d.z - cb.z ) : cb.z - ( 1 - 2 * cs.z ) * cb.z * ( 1 - cb.z )
+    );
+  }
+
+  // TODO: check examples, tests
+  public static sat( c: Vector3 ): number {
+    return Math.max( c.x, Math.max( c.y, c.z ) ) - Math.min( c.x, Math.min( c.y, c.z ) );
+  }
+
+  // TODO: check examples, tests
+  public static lum( c: Vector3 ): number {
+    return 0.3 * c.x + 0.59 * c.y + 0.11 * c.z;
+  }
+
+  // TODO: check examples, tests
+  public static clipColor( c: Vector3 ): Vector3 {
+    const lScalar = RenderBlendCompose.lum( c );
+    const l = new Vector3( lScalar, lScalar, lScalar );
+    const n = Math.min( c.x, c.y, c.z );
+    const x = Math.max( c.x, c.y, c.z );
+    if ( n < 0 ) {
+      c = l.plus( c.minus( l ).componentTimes( l ).dividedScalar( lScalar - n ) );
+    }
+    if ( x > 1 ) {
+      c = l.plus( c.minus( l ).timesScalar( ( 1 - lScalar ) / ( x - lScalar ) ) );
+    }
+    return c;
+  }
+
+  public static setLum( c: Vector3, l: number ): Vector3 {
+    return RenderBlendCompose.clipColor( c.plusScalar( l - RenderBlendCompose.lum( c ) ) );
+  }
+
+  public static setSatInner( c: Vector3, s: number ): void {
+    if ( c.z > c.x ) {
+      c.y = ( ( c.y - c.x ) * s ) / ( c.z - c.x );
+      c.z = s;
+    }
+    else {
+      c.y = 0;
+      c.z = 0;
+    }
+    c.x = 0;
+  }
+
+  public static setSat( c: Vector3, s: number ): Vector3 {
+    // TODO: this is horribly inefficient and wonky
+
+    if ( c.x <= c.y ) {
+      if ( c.y <= c.z ) {
+        RenderBlendCompose.setSatInner( c, s );
+        return c;
+      }
+      else {
+        if ( c.x <= c.z ) {
+          const v = new Vector3( c.x, c.z, c.y );
+          RenderBlendCompose.setSatInner( v, s );
+          return new Vector3( v.x, v.z, v.y );
+        }
+        else {
+          const v = new Vector3( c.z, c.x, c.y );
+          RenderBlendCompose.setSatInner( v, s );
+          return new Vector3( v.y, v.z, v.x );
+        }
+      }
+    }
+    else {
+      if ( c.x <= c.z ) {
+        const v = new Vector3( c.y, c.x, c.z );
+        RenderBlendCompose.setSatInner( v, s );
+        return new Vector3( v.y, v.x, v.z );
+      }
+      else {
+        if ( c.y <= c.z ) {
+          const v = new Vector3( c.y, c.z, c.x );
+          RenderBlendCompose.setSatInner( v, s );
+          return new Vector3( v.z, v.x, v.y );
+        }
+        else {
+          const v = new Vector3( c.z, c.y, c.x );
+          RenderBlendCompose.setSatInner( v, s );
+          return new Vector3( v.z, v.y, v.x );
+        }
+      }
+    }
   }
 }
 scenery.register( 'RenderBlendCompose', RenderBlendCompose );
@@ -489,8 +726,7 @@ export class RenderAlpha extends RenderPathProgram {
 
     // Now we're "inside" our path
     if ( program instanceof RenderColor ) {
-      // NOT using premultiplied alpha
-      return new RenderColor( null, program.color.withAlpha( program.color.alpha * this.alpha ) );
+      return new RenderColor( program.path, program.color.timesScalar( this.alpha ) );
     }
     else {
       return new RenderAlpha( null, program, this.alpha );
@@ -514,16 +750,16 @@ scenery.register( 'RenderAlpha', RenderAlpha );
 // TODO: consider transforms as a node itself? Meh probably excessive?
 
 export class RenderColor extends RenderPathProgram {
-  public constructor( path: RenderPath | null, public color: Color ) {
+  public constructor( path: RenderPath | null, public color: Vector4 ) {
     super( path );
   }
 
   public override isFullyTransparent(): boolean {
-    return this.color.alpha === 0;
+    return this.color.w === 0;
   }
 
   public override isFullyOpaque(): boolean {
-    return this.path === null && this.color.alpha === 1;
+    return this.path === null && this.color.w === 1;
   }
 
   public override simplify( map?: Map<RenderPath, boolean> ): RenderProgram {
@@ -545,7 +781,11 @@ export class RenderColor extends RenderPathProgram {
       return Vector4.ZERO;
     }
 
-    return RenderColor.colorToPremultipliedLinear( this.color );
+    return this.color;
+  }
+
+  public static fromColor( path: RenderPath | null, color: Color ): RenderColor {
+    return new RenderColor( path, RenderColor.colorToPremultipliedLinear( color ) );
   }
 
   public static colorToPremultipliedLinear( color: Color ): Vector4 {
@@ -565,37 +805,11 @@ export class RenderColor extends RenderPathProgram {
       color.alpha
     );
 
-    const linear = new Vector4(
-      sRGB.x <= 0.0404482362771082 ? sRGB.x / 12.92 : Math.pow( ( sRGB.x + 0.055 ) / 1.055, 2.4 ),
-      sRGB.y <= 0.0404482362771082 ? sRGB.y / 12.92 : Math.pow( ( sRGB.y + 0.055 ) / 1.055, 2.4 ),
-      sRGB.z <= 0.0404482362771082 ? sRGB.z / 12.92 : Math.pow( ( sRGB.z + 0.055 ) / 1.055, 2.4 ),
-      sRGB.w
-    );
-
-    const premultiplied = new Vector4(
-      linear.x * linear.w,
-      linear.y * linear.w,
-      linear.z * linear.w,
-      linear.w
-    );
-
-    return premultiplied;
+    return RenderColor.premultiply( RenderColor.sRGBToLinear( sRGB ) );
   }
 
   public static premultipliedLinearToColor( premultiplied: Vector4 ): Color {
-    const linear = premultiplied.w === 0 ? Vector4.ZERO : new Vector4(
-      premultiplied.x / premultiplied.w,
-      premultiplied.y / premultiplied.w,
-      premultiplied.z / premultiplied.w,
-      premultiplied.w
-    );
-
-    const sRGB = new Vector4(
-      linear.x <= 0.00313066844250063 ? linear.x * 12.92 : 1.055 * Math.pow( linear.x, 1 / 2.4 ) - 0.055,
-      linear.y <= 0.00313066844250063 ? linear.y * 12.92 : 1.055 * Math.pow( linear.y, 1 / 2.4 ) - 0.055,
-      linear.z <= 0.00313066844250063 ? linear.z * 12.92 : 1.055 * Math.pow( linear.z, 1 / 2.4 ) - 0.055,
-      linear.w
-    );
+    const sRGB = RenderColor.linearToSRGB( RenderColor.unpremultiply( premultiplied ) );
 
     return new Color(
       sRGB.x * 255,
@@ -605,7 +819,43 @@ export class RenderColor extends RenderPathProgram {
     );
   }
 
-  public static readonly TRANSPARENT = new RenderColor( null, Color.TRANSPARENT );
+  public static sRGBToLinear( sRGB: Vector4 ): Vector4 {
+    return new Vector4(
+      sRGB.x <= 0.0404482362771082 ? sRGB.x / 12.92 : Math.pow( ( sRGB.x + 0.055 ) / 1.055, 2.4 ),
+      sRGB.y <= 0.0404482362771082 ? sRGB.y / 12.92 : Math.pow( ( sRGB.y + 0.055 ) / 1.055, 2.4 ),
+      sRGB.z <= 0.0404482362771082 ? sRGB.z / 12.92 : Math.pow( ( sRGB.z + 0.055 ) / 1.055, 2.4 ),
+      sRGB.w
+    );
+  }
+
+  public static linearToSRGB( linear: Vector4 ): Vector4 {
+    return new Vector4(
+      linear.x <= 0.00313066844250063 ? linear.x * 12.92 : 1.055 * Math.pow( linear.x, 1 / 2.4 ) - 0.055,
+      linear.y <= 0.00313066844250063 ? linear.y * 12.92 : 1.055 * Math.pow( linear.y, 1 / 2.4 ) - 0.055,
+      linear.z <= 0.00313066844250063 ? linear.z * 12.92 : 1.055 * Math.pow( linear.z, 1 / 2.4 ) - 0.055,
+      linear.w
+    );
+  }
+
+  public static premultiply( color: Vector4 ): Vector4 {
+    return new Vector4(
+      color.x * color.w,
+      color.y * color.w,
+      color.z * color.w,
+      color.w
+    );
+  }
+
+  public static unpremultiply( color: Vector4 ): Vector4 {
+    return color.w === 0 ? Vector4.ZERO : new Vector4(
+      color.x / color.w,
+      color.y / color.w,
+      color.z / color.w,
+      color.w
+    );
+  }
+
+  public static readonly TRANSPARENT = new RenderColor( null, Vector4.ZERO );
 }
 scenery.register( 'RenderColor', RenderColor );
 
@@ -685,7 +935,7 @@ export class RenderLinearGradient extends RenderPathProgram {
     public readonly transform: Matrix3,
     public readonly start: Vector2,
     public readonly end: Vector2,
-    public readonly stops: RenderGradientStop[],
+    public readonly stops: RenderGradientStop[], // should be sorted!!
     public readonly extend: RenderExtend
   ) {
     super( path );
@@ -736,7 +986,6 @@ export class RenderLinearGradient extends RenderPathProgram {
     const t = gradDelta.magnitude > 0 ? localDelta.dot( gradDelta ) / gradDelta.dot( gradDelta ) : 0;
     const mappedT = RenderImage.extend( this.extend, t );
 
-    // TODO: make sure stops are sorted
     let i = -1;
     while ( i < this.stops.length - 1 && this.stops[ i + 1 ].ratio < mappedT ) {
       i++;
@@ -765,7 +1014,7 @@ export class RenderRadialGradient extends RenderPathProgram {
     public readonly startRadius: number,
     public readonly end: Vector2,
     public readonly endRadius: number,
-    public readonly stops: RenderGradientStop[],
+    public readonly stops: RenderGradientStop[], // should be sorted!!
     public readonly extend: RenderExtend
   ) {
     super( path );
