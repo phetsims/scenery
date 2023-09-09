c5eb5034b3e55fce7c1d026e9be650552e93f79f
phet-dev <phetadmin@gmail.com>
2023-09-09 03:18:44 -0600

    update copyright dates from daily grunt work

--- js/accessibility/Focus.ts
+++ js/accessibility/Focus.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * A scenery-internal type for tracking what currently has focus in Display. If a focused Node is shared between
