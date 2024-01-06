4a6b422259ce3d01b00ba3aaaca55e92b54e96ff
phet-dev <phetadmin@gmail.com>
2024-01-06 03:19:06 -0700

    update copyright dates from daily grunt work

--- js/accessibility/EnglishStringKeyUtils.ts
+++ js/accessibility/EnglishStringKeyUtils.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * A set of utility constants and functions for working with the english keys PhET has defined in
--- js/scenery-main.js
+++ js/scenery-main.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, University of Colorado Boulder
+// Copyright 2016-2024, University of Colorado Boulder
 
 /**
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
