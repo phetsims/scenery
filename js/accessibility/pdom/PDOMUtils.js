b17aec4f2bcd482f6276e6d646209e4a626affd9
phet-dev <phetadmin@gmail.com>
2024-01-23 03:17:52 -0700

    update copyright dates from daily grunt work

--- js/accessibility/pdom/PDOMUtils.js
+++ js/accessibility/pdom/PDOMUtils.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2023, University of Colorado Boulder
+// Copyright 2017-2024, University of Colorado Boulder
 
 /**
  * Utility functions for scenery that are specifically useful for ParallelDOM.
