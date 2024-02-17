0ae3ce350033b9aec70bb744994e4b86a634584d
phet-dev <phetadmin@gmail.com>
2024-02-17 03:22:42 -0700

    update copyright dates from daily grunt work

--- js/util/Gradient.ts
+++ js/util/Gradient.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2023, University of Colorado Boulder
+// Copyright 2013-2024, University of Colorado Boulder
 
 /**
  * Abstract base type for LinearGradient and RadialGradient.
--- js/util/sceneryDeserialize.ts
+++ js/util/sceneryDeserialize.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Deserializes a generalized object
