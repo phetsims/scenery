97dc9c582215d6c4fb6ce6759263dcd4ad17df51
phet-dev <phetadmin@gmail.com>
2024-02-24 03:22:25 -0700

    update copyright dates from daily grunt work

--- js/util/createGatedVisibleProperty.ts
+++ js/util/createGatedVisibleProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * The `createGatedVisibleProperty` function abstracts the process of creating a "gated" visibility Property
