cfe7c2c8c9eac3aea59c81961653e8f7d68f3191
Jonathan Olson <jonathan.olson@colorado.edu>
2024-07-10 10:35:23 -0600

    Adding zero-width space to Firefox SVG Text, see https://github.com/phetsims/scenery/issues/1643

--- js/nodes/Text.js
+++ js/nodes/Text.js
@@ -1063,38 +1063,8 @@ define( function( require ) {
         var string = this.node.renderedText;
 
         // Workaround for Firefox handling of RTL embedding marks, https://github.com/phetsims/scenery/issues/1643
-        // We strip off outer containing embedding marks, and adjust the direction and text-anchor accordingly to match
-        // our normal behavior.
         if ( platform.firefox ) {
-          var direction = 'ltr';
-
-          // While our string is long enough AND the last character is a POP embedding mark
-          while ( string.length > 2 && string[ string.length - 1 ] === '\u202c' ) {
-            var isLTR = string[ 0 ] === '\u202a';
-            var isRTL = string[ 0 ] === '\u202b';
-
-            if ( isLTR ) {
-              direction = 'ltr';
-            }
-            else if ( isRTL ) {
-              direction = 'rtl';
-            }
-            else {
-              break;
-            }
-
-            // Strip off the outer embedding marks
-            string = string.slice( 1, -1 );
-          }
-
-          if ( this.direction !== direction ) {
-            this.direction = direction;
-
-            text.setAttribute( 'direction', direction );
-
-            // To maintain the same positioning, we need to adjust the text-anchor
-            text.setAttribute( 'text-anchor', direction === 'rtl' ? 'end' : 'start' );
-          }
+          string = '\u200b' + string;
         }
 
         text.lastChild.nodeValue = string;
