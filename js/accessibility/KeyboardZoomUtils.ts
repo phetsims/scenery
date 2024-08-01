50539bf7dae19276996a3ddfb87ff9d48c22d202
phet-dev <phetadmin@gmail.com>
2024-08-01 04:14:33 -0600

    update copyright dates from daily grunt work

--- js/accessibility/KeyboardZoomUtils.ts
+++ js/accessibility/KeyboardZoomUtils.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * Utilities specific to the keyboard for handling zoom/pan control.
