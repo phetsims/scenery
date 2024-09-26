12a86697522ea87b7671e53f6e51daf328c87f39
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:46 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for scenery.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import parent from '../chipper/eslint/phet-library.eslint.config.mjs';
+
+export default [
+  ...parent,
+  {
+    rules: {
+      'no-bitwise': 'off'
+    },
+    ignores: [
+      'js/display/swash/pkg',
+      'js/display/guillotiere/pkg'
+    ],
+    languageOptions: {
+      globals: {
+        himalaya: 'readonly',
+        LineBreaker: 'readonly',
+        sceneryLog: 'readonly',
+        he: 'readonly'
+      }
+    }
+  }
+];
