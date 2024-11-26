1098f46a245882fc608ccad5984a41d5bc957bcd
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-26 15:40:15 -0700

    Move GatedVisibleProperty to axon, https://github.com/phetsims/scenery/issues/1660

--- js/imports.ts
+++ js/imports.ts
@@ -74,8 +74,6 @@ export { default as PaintDef } from './util/PaintDef.js';
 export type { default as TColor } from './util/TColor.js';
 export type { default as TPaint } from './util/TPaint.js';
 
-export { default as GatedVisibleProperty } from './util/GatedVisibleProperty.js';
-
 // Filters
 export { default as ColorMatrixFilter } from './filters/ColorMatrixFilter.js';
 export { default as Brightness } from './filters/Brightness.js';
