5928a90daa64c952078e8c88a9dc992eee1f2090
phet-dev <phetadmin@gmail.com>
2025-01-08 03:47:14 -0700

    update copyright dates from daily grunt work

--- js/accessibility/KeyboardFuzzer.ts
+++ js/accessibility/KeyboardFuzzer.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2023, University of Colorado Boulder
+// Copyright 2018-2025, University of Colorado Boulder
 
 /**
  * ?fuzzBoard keyboard fuzzer
--- js/imports.ts
+++ js/imports.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * Ordered imports that should be loaded IN THIS ORDER, so we can get around circular dependencies for type checking.
