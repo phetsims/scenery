3935175ee8851bbcf7114183f83af7d02690d272
phet-dev <phetadmin@gmail.com>
2025-01-24 04:05:23 -0700

    update copyright dates from daily grunt work

--- js/accessibility/voicing/ReadingBlock.ts
+++ js/accessibility/voicing/ReadingBlock.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * A trait that extends Voicing, adding support for "Reading Blocks" of the voicing feature. "Reading Blocks" are
--- js/accessibility/voicing/nodes/VoicingText.ts
+++ js/accessibility/voicing/nodes/VoicingText.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * Text that mixes ReadingBlock, supporting features of Voicing and adding listeners that speak the text string
