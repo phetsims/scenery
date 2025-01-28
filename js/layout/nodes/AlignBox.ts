ec9c2f79de803fa3685e9990a57f3cc83f03344b
phet-dev <phetadmin@gmail.com>
2025-01-28 03:45:57 -0700

    update copyright dates from daily grunt work

--- js/layout/nodes/AlignBox.ts
+++ js/layout/nodes/AlignBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2024, University of Colorado Boulder
+// Copyright 2016-2025, University of Colorado Boulder
 
 /**
  * A Node that will align child (content) Node within a specific bounding box.
