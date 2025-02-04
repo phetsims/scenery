38a278cf7bbd4e3b9ccbc0cab29c47426365f142
Jonathan Olson <jonathan.olson@colorado.edu>
2025-02-03 18:06:41 -0700

    WebGLNodeDrawable refactor to reduce circular references, see https://github.com/scenerystack/scenerystack/issues/8 and https://github.com/phetsims/chipper/issues/1550

--- js/nodes/WebGLNode.ts
+++ js/nodes/WebGLNode.ts
@@ -215,9 +215,13 @@ export default abstract class WebGLNode extends Node {
   }
 
   // Return code from painter.paint() when nothing was painted to the WebGL context.
+  // NOTE: these constants are duplicated in WebGLNodeDrawable to reduce circular references
+  //       If changing, please change them there also!
   public static readonly PAINTED_NOTHING = 0 as const;
 
   // Return code from painter.paint() when something was painted to the WebGL context.
+  // NOTE: these constants are duplicated in WebGLNodeDrawable to reduce circular references
+  //       If changing, please change them there also!
   public static readonly PAINTED_SOMETHING = 1 as const;
 }
 
