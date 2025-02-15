6a12d91180f0cfc60155645eed6c6496fd654429
phet-dev <phetadmin@gmail.com>
2025-02-15 03:33:34 -0700

    update copyright dates from daily grunt work

--- js/util/SceneryStyle.ts
+++ js/util/SceneryStyle.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2025, University of Colorado Boulder
+// Copyright 2025, University of Colorado Boulder
 
 /**
  * Creates and references a stylesheet that can be built up while Scenery is loading.
