610b8775c3a5cd865bbfe46724f833338057e657
Sam Reid <reids@colorado.edu>
2025-05-12 18:25:10 -0600

    Convert to TypeScript, see https://github.com/phetsims/membrane-transport/issues/163

--- js/accessibility/pdom/PDOMUtils.ts
+++ js/accessibility/pdom/PDOMUtils.ts
@@ -7,14 +7,16 @@
  * For the most part this file's methods are public in a scenery-internal context. Some exceptions apply. Please
  * consult @jessegreenberg and/or @zepumph before using this outside of scenery.
  *
- * @author Jesse Greenberg
+ * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import { isTReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
+import TReadOnlyProperty, { isTReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import validate from '../../../../axon/js/validate.js';
 import Validation from '../../../../axon/js/Validation.js';
+import Random from '../../../../dot/js/Random.js';
 import merge from '../../../../phet-core/js/merge.js';
 import stripEmbeddingMarks from '../../../../phet-core/js/stripEmbeddingMarks.js';
+import IntentionalAny from '../../../../phet-core/js/types/IntentionalAny.js';
 import PDOMSiblingStyle from '../../accessibility/pdom/PDOMSiblingStyle.js';
 import scenery from '../../scenery.js';
 
@@ -120,10 +122,9 @@ const ASSOCIATION_ATTRIBUTES = [ ARIA_LABELLEDBY, ARIA_DESCRIBEDBY, ARIA_ACTIVE_
  * Get all 'element' nodes off the parent element, placing them in an array for easy traversal.  Note that this
  * includes all elements, even those that are 'hidden' or purely for structure.
  *
- * @param  {HTMLElement} domElement - parent whose children will be linearized
- * @returns {HTMLElement[]}
+ * @param domElement - parent whose children will be linearized
  */
-function getLinearDOMElements( domElement ) {
+function getLinearDOMElements( domElement: HTMLElement ): Element[] {
 
   // gets ALL descendant children for the element
   const children = domElement.getElementsByTagName( '*' );
@@ -142,11 +143,8 @@ function getLinearDOMElements( domElement ) {
 /**
  * Determine if an element is hidden.  An element is considered 'hidden' if it (or any of its ancestors) has the
  * 'hidden' attribute.
- *
- * @param {HTMLElement} domElement
- * @returns {Boolean}
  */
-function isElementHidden( domElement ) {
+function isElementHidden( domElement: HTMLElement ): boolean {
   if ( domElement.hidden ) {
     return true;
   }
@@ -154,7 +152,7 @@ function isElementHidden( domElement ) {
     return false;
   }
   else {
-    return isElementHidden( domElement.parentElement );
+    return isElementHidden( domElement.parentElement! );
   }
 }
 
@@ -164,18 +162,17 @@ function isElementHidden( domElement ) {
  * when focus changes. If no next or previous focusable is found, it returns the currently focused element.
  * This function should not be used directly, use getNextFocusable() or getPreviousFocusable() instead.
  *
- * @param {string} direction - direction of traversal, one of 'NEXT' | 'PREVIOUS'
- * @param {HTMLElement} [parentElement] - optional, search will be limited to children of this element
- * @returns {HTMLElement}
+ * @param direction - direction of traversal, one of 'NEXT' | 'PREVIOUS'
+ * @param [parentElement] - optional, search will be limited to children of this element
  */
-function getNextPreviousFocusable( direction, parentElement ) {
+function getNextPreviousFocusable( direction: 'NEXT' | 'PREVIOUS', parentElement: HTMLElement | null ): HTMLElement {
 
   // linearize the document [or the desired parent] for traversal
   const parent = parentElement || document.body;
   const linearDOM = getLinearDOMElements( parent );
 
   const activeElement = document.activeElement;
-  const activeIndex = linearDOM.indexOf( activeElement );
+  const activeIndex = linearDOM.indexOf( activeElement! );
   const delta = direction === NEXT ? +1 : -1;
 
   // find the next focusable element in the DOM
@@ -184,22 +181,24 @@ function getNextPreviousFocusable( direction, parentElement ) {
     const nextElement = linearDOM[ nextIndex ];
     nextIndex += delta;
 
+    // @ts-expect-error
     if ( PDOMUtils.isElementFocusable( nextElement ) ) {
+
+      // @ts-expect-error
       return nextElement;
     }
   }
 
   // if no next focusable is found, return the active DOM element
-  return activeElement;
+  // @ts-expect-error
+  return activeElement!;
 }
 
 /**
  * Trims the white space from the left of the string.
  * Solution from https://stackoverflow.com/questions/1593859/left-trim-in-javascript
- * @param  {string} string
- * @returns {string}
  */
-function trimLeft( string ) {
+function trimLeft( string: string ): string {
 
   // ^ - from the beginning of the string
   // \s - whitespace character
@@ -209,25 +208,20 @@ function trimLeft( string ) {
 
 
 /**
- * Returns whether or not the tagName supports innerHTML or textContent in PhET.
- * @private
- * @param {string} tagName
- * @returns {boolean}
+ * Returns whether the tagName supports innerHTML or textContent in PhET.
  */
-function tagNameSupportsContent( tagName ) {
+function tagNameSupportsContent( tagName: string ): boolean {
   return !_.includes( ELEMENTS_WITHOUT_CLOSING_TAG, tagName.toUpperCase() );
 }
 
 const PDOMUtils = {
 
   /**
-   * Given a Property or string, return the Property value if it is a property. Otherwise just return the string.
+   * Given a Property or string, return the Property value if it is a property. Otherwise, just return the string.
    * Useful for forwarding the string to DOM content, but allowing the API to take a StringProperty. Eventually
    * PDOM may support dynamic strings.
-   * @param valueOrProperty
-   * @returns {string|Property}
    */
-  unwrapStringProperty( valueOrProperty ) {
+  unwrapStringProperty( valueOrProperty: string | TReadOnlyProperty<string> | null ): string | null {
     const result = valueOrProperty === null ? null : ( typeof valueOrProperty === 'string' ? valueOrProperty : valueOrProperty.value );
 
     assert && assert( result === null || typeof result === 'string' );
@@ -239,12 +233,10 @@ const PDOMUtils = {
    * Get the next focusable element relative to the currently focused element and under the parentElement.
    * Can be useful if you want to emulate the 'Tab' key behavior or just transition focus to the next element
    * in the document. If no next focusable can be found, it will return the currently focused element.
-   * @public
    *
-   * @param {HTMLElement} [parentElement] - optional, search will be limited to elements under this element
-   * @returns {HTMLElement}
+   * @param [parentElement] - optional, search will be limited to elements under this element
    */
-  getNextFocusable( parentElement ) {
+  getNextFocusable( parentElement: HTMLElement | null = null ): HTMLElement {
     return getNextPreviousFocusable( NEXT, parentElement );
   },
 
@@ -252,12 +244,10 @@ const PDOMUtils = {
    * Get the previous focusable element relative to the currently focused element under the parentElement. Can be
    * useful if you want to emulate 'Shift+Tab' behavior. If no next focusable can be found, it will return the
    * currently focused element.
-   * @public
    *
-   * @param {HTMLElement} [parentElement] - optional, search will be limited to elements under this parent
-   * @returns {HTMLElement}
+   * @param [parentElement] - optional, search will be limited to elements under this parent
    */
-  getPreviousFocusable( parentElement ) {
+  getPreviousFocusable( parentElement: HTMLElement | null = null ): HTMLElement {
     return getNextPreviousFocusable( PREVIOUS, parentElement );
   },
 
@@ -265,46 +255,46 @@ const PDOMUtils = {
    * Get the first focusable element under the parentElement. If no element is available, the document.body is
    * returned.
    *
-   * @param {HTMLElement} [parentElement] - optionally restrict the search to elements under this parent
-   * @returns {HTMLElement}
+   * @param [parentElement] - optionally restrict the search to elements under this parent
    */
-  getFirstFocusable( parentElement ) {
+  getFirstFocusable( parentElement: HTMLElement | null = null ): HTMLElement {
     const parent = parentElement || document.body;
     const linearDOM = getLinearDOMElements( parent );
 
     // return the document.body if no element is found
-    let firstFocusable = document.body;
+    let firstFocusable: Element = document.body;
 
     let nextIndex = 0;
     while ( nextIndex < linearDOM.length ) {
       const nextElement = linearDOM[ nextIndex ];
       nextIndex++;
 
+      // @ts-expect-error
       if ( PDOMUtils.isElementFocusable( nextElement ) ) {
         firstFocusable = nextElement;
         break;
       }
     }
 
+    // @ts-expect-error
     return firstFocusable;
   },
 
   /**
    * Return a random focusable element in the document. Particularly useful for fuzz testing.
-   * @public
-   *
-   * @parma {Random} random
-   * @returns {HTMLElement}
    */
-  getRandomFocusable( random ) {
+  getRandomFocusable( random: Random ): HTMLElement {
     assert && assert( random, 'Random expected' );
 
     const linearDOM = getLinearDOMElements( document.body );
     const focusableElements = [];
     for ( let i = 0; i < linearDOM.length; i++ ) {
+
+      // @ts-expect-error
       PDOMUtils.isElementFocusable( linearDOM[ i ] ) && focusableElements.push( linearDOM[ i ] );
     }
 
+    // @ts-expect-error
     return focusableElements[ random.nextInt( focusableElements.length ) ];
   },
 
@@ -312,28 +302,24 @@ const PDOMUtils = {
    * ParallelDOM trait values may be in a Property to support dynamic locales. This function
    * returns the Property value in that case. The value may be a string, boolean, or number -
    * all of which are valid values for native HTML attributes.
-   *
-   * @param {string | boolean | number | TReadOnlyProperty<string|boolean|number>} valueOrProperty
-   * @returns {string|boolean|number}
    */
-  unwrapProperty( valueOrProperty ) {
+  unwrapProperty( valueOrProperty: string | boolean | number | TReadOnlyProperty<string | boolean | number> ): string | boolean | number {
     return isTReadOnlyProperty( valueOrProperty ) ? valueOrProperty.value : valueOrProperty;
   },
 
   /**
    * If the textContent has any tags that are not formatting tags, return false. Only checking for
    * tags that are not in the allowed FORMATTING_TAGS. If there are no tags at all, return false.
-   * @public
-   *
-   * @param {string} textContent
-   * @returns {boolean}
    */
-  containsFormattingTags( textContent ) {
+  containsFormattingTags( textContent: string ): boolean {
 
     // no-op for null case
     if ( textContent === null ) {
       return false;
     }
+
+    // Continue testing for JavaScript usages
+    // eslint-disable-next-line phet/no-simple-type-checking-assertions
     assert && assert( typeof textContent === 'string', 'unsupported type for textContent.' );
 
     let i = 0;
@@ -390,13 +376,17 @@ const PDOMUtils = {
    * If the text content uses formatting tags, set the content as innerHTML. Otherwise, set as textContent.
    * In general, textContent is more secure and much faster because it doesn't trigger DOM styling and
    * element insertions.
-   * @public
    *
-   * @param {Element} domElement
-   * @param {string|number|null} textContent - domElement is cleared of content if null, could have acceptable HTML
+   * @param domElement
+   * @param textContent - domElement is cleared of content if null, could have acceptable HTML
    *                                    "formatting" tags in it
+   *
+   * TODO: textContent probably wants to support type number, see https://github.com/phetsims/membrane-transport/issues/163
    */
-  setTextContent( domElement, textContent ) {
+  setTextContent( domElement: Element, textContent: string | number | null ): void {
+
+    // Continue testing for JavaScript usages
+    // eslint-disable-next-line phet/no-simple-type-checking-assertions
     assert && assert( domElement instanceof Element ); // parent to HTMLElement, to support other namespaces
     assert && assert( textContent === null || typeof textContent === 'string' );
 
@@ -407,6 +397,7 @@ const PDOMUtils = {
 
       // XHTML requires <br/> instead of <br>, but <br/> is still valid in HTML. See
       // https://github.com/phetsims/scenery/issues/1309
+      // @ts-expect-error - this will probably crash with a number
       const textWithoutBreaks = textContent.replaceAll( '<br>', '<br/>' );
 
       // TODO: this line must be removed to support i18n Interactive Description, see https://github.com/phetsims/chipper/issues/798
@@ -432,26 +423,19 @@ const PDOMUtils = {
    * Given a tagName, test if the element will be focusable by default by the browser.
    * Different from isElementFocusable, because this only looks at tags that the browser will automatically put
    * a >=0 tab index on.
-   * @public
    *
    * NOTE: Uses a set of browser types as the definition of default focusable elements,
    * see https://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus
-   *
-   * @param tagName
-   * @returns {boolean}
    */
-  tagIsDefaultFocusable( tagName ) {
+  tagIsDefaultFocusable( tagName: string ): boolean {
     return _.includes( DEFAULT_FOCUSABLE_TAGS, tagName.toUpperCase() );
   },
 
   /**
    * Returns true if the element is focusable. Assumes that all focusable  elements have tabIndex >= 0, which
    * is only true for elements of the Parallel DOM.
-   *
-   * @param {HTMLElement} domElement
-   * @returns {boolean}
    */
-  isElementFocusable( domElement ) {
+  isElementFocusable( domElement: HTMLElement ): boolean {
 
     if ( !document.body.contains( domElement ) ) {
       return false;
@@ -471,23 +455,16 @@ const PDOMUtils = {
   },
 
   /**
-   * @public
-   *
-   * @param {string} tagName
-   * @returns {boolean} - true if the tag does support inner content
+   * @returns true if the tag does support inner content
    */
-  tagNameSupportsContent( tagName ) {
+  tagNameSupportsContent( tagName: string ): boolean {
     return tagNameSupportsContent( tagName );
   },
 
   /**
    * Helper function to remove multiple HTMLElements from another HTMLElement
-   * @public
-   *
-   * @param {HTMLElement} element
-   * @param {Array.<HTMLElement>} childrenToRemove
    */
-  removeElements( element, childrenToRemove ) {
+  removeElements( element: HTMLElement, childrenToRemove: HTMLElement[] ): void {
 
     for ( let i = 0; i < childrenToRemove.length; i++ ) {
       const childToRemove = childrenToRemove[ i ];
@@ -501,18 +478,17 @@ const PDOMUtils = {
 
   /**
    * Helper function to add multiple elements as children to a parent
-   * @public
    *
-   * @param {HTMLElement} element - to add children to
-   * @param {Array.<HTMLElement>} childrenToAdd
-   * @param {HTMLElement} [beforeThisElement] - if not supplied, the insertBefore call will just use 'null'
+   * @param element - to add children to
+   * @param childrenToAdd
+   * @param [beforeThisElement] - if not supplied, the insertBefore call will just use 'null'
    */
-  insertElements( element, childrenToAdd, beforeThisElement ) {
+  insertElements( element: HTMLElement, childrenToAdd: HTMLElement[], beforeThisElement: HTMLElement | null = null ): void {
     assert && assert( element instanceof window.Element );
     assert && assert( Array.isArray( childrenToAdd ) );
     for ( let i = 0; i < childrenToAdd.length; i++ ) {
       const childToAdd = childrenToAdd[ i ];
-      element.insertBefore( childToAdd, beforeThisElement || null );
+      element.insertBefore( childToAdd, beforeThisElement );
     }
   },
 
@@ -521,13 +497,13 @@ const PDOMUtils = {
    * tab index. IE gives all elements a tabIndex of 0 and handles tab navigation internally, so this marks
    * which elements should not be in the focus order.
    *
-   * @public
-   * @param  {string} tagName
-   * @param {boolean} focusable - should the element be explicitly added to the focus order?
-   * @param {Object} [options]
-   * @returns {HTMLElement}
+   * @param tagName
+   * @param focusable - should the element be explicitly added to the focus order?
+   * @param [options]
    */
-  createElement( tagName, focusable, options ) {
+  createElement( tagName: string, focusable: boolean, options: IntentionalAny ): HTMLElement {
+
+    // eslint-disable-next-line phet/bad-typescript-text
     options = merge( {
       // {string|null} - If non-null, the element will be created with the specific namespace
       namespace: null,
@@ -561,12 +537,9 @@ const PDOMUtils = {
    *
    * If default behavior and focusable align, the tabindex attribute is removed so that can't interfere with a
    * screen reader.
-   * @public (scenery-internal)
-   *
-   * @param {HTMLElement} element
-   * @param {boolean} focusable
+   * (scenery-internal usage only)
    */
-  overrideFocusWithTabIndex( element, focusable ) {
+  overrideFocusWithTabIndex( element: HTMLElement, focusable: boolean ): void {
     const defaultFocusable = PDOMUtils.tagIsDefaultFocusable( element.tagName );
 
     // only add a tabindex when we are overriding the default focusable behavior of the browser for the tag name
@@ -577,6 +550,7 @@ const PDOMUtils = {
       element.removeAttribute( 'tabindex' );
     }
 
+    // @ts-expect-error
     element.setAttribute( DATA_FOCUSABLE, focusable );
   },
 
