ca2af53fc35d69deebc02868fe8ab05cf0f1817f
phet-dev <phetadmin@gmail.com>
2026-01-16 04:07:35 -0700

    update copyright dates from daily grunt work

--- js/accessibility/pdom/ParallelDOM.ts
+++ js/accessibility/pdom/ParallelDOM.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2025, University of Colorado Boulder
+// Copyright 2021-2026, University of Colorado Boulder
 
 /**
  * A superclass for Node, adding accessibility by defining content for the Parallel DOM. Please note that Node and
