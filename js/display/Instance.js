1b6aa0dd0458ca750891edadf9da9470fa51d059
phet-dev <phetadmin@gmail.com>
2026-01-17 04:56:58 -0700

    update copyright dates from daily grunt work

--- js/display/Instance.js
+++ js/display/Instance.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2025, University of Colorado Boulder
+// Copyright 2013-2026, University of Colorado Boulder
 
 /**
  * An instance that is specific to the display (not necessarily a global instance, could be in a Canvas cache, etc),
