ec05bd43be3237e9cee4ea057e3f0fb47f57a33b
phet-dev <phetadmin@gmail.com>
2026-01-25 04:27:28 -0700

    update copyright dates from daily grunt work

--- js/accessibility/pdom/globalDescriptionQueue.ts
+++ js/accessibility/pdom/globalDescriptionQueue.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * Global queue for Interactive Description responses that are not tied to a visible `Node`.
